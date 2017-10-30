f6ea36d5dc87b990ad1a7a29551eb3310535a263
j2969719 <30291994+j2969719@users.noreply.github.com>
2017-10-30 20:35:00 +0300

    helloworld

--- plugins/wdx/dfwdx.lua
+++ plugins/wdx/dfwdx.lua
@@ -4,7 +4,24 @@ local fields = {
     {"Mountpoint", 6, "target | tail -1"}, 
     {"Target", 8, "target | tail -1"}, 
     {"Source", 8, "source | tail -1"}, 
-    {"FS Type", 8, "fstype | tail -1"} 
+    {"FS Type", 8, "fstype | tail -1"}, 
+    {"Size", 2, "size | tail -1"}, 
+    {"Size (M)", 2, "size -BM | tail -1"}, 
+    {"Size (G)", 2, "size -BG | tail -1"}, 
+    {"Size (T)", 2, "size -BT | tail -1"}, 
+    {"Used", 2, "used | tail -1"}, 
+    {"Used (M)", 2, "used -BM | tail -1"}, 
+    {"Used (G)", 2, "used -BG | tail -1"}, 
+    {"Used (T)", 2, "used -BT | tail -1"}, 
+    {"Available", 2, "avail | tail -1"}, 
+    {"Available (M)", 2, "avail -BM | tail -1"}, 
+    {"Available (G)", 2, "avail -BG | tail -1"}, 
+    {"Available (T)", 2, "avail -BT | tail -1"}, 
+    {"% Used", 2, "pcent | tail -1"}, 
+    {"Inodes", 2, "itotal | tail -1"}, 
+    {"IUsed", 2, "iused | tail -1"}, 
+    {"IFree", 2, "iavail | tail -1"}, 
+    {"% IUsed", 2, "ipcent | tail -1"}
 }
 
 function ContentGetSupportedField(Index)
@@ -23,12 +40,17 @@ function ContentGetDetectString()
 end
 
 function ContentGetValue(FileName, FieldIndex, UnitIndex, flags)
+    if not (SysUtils.DirectoryExists(FileName)) then
+        return nil;
+    end
     if (fields[FieldIndex + 1][3] ~= nil) then
         local handle = io.popen('df "'..FileName..'" --output=' .. fields[FieldIndex + 1][3]);
         local result = handle:read("*a");
         handle:close();
         result = result:sub(1, - 2);
-        if (fields[FieldIndex + 1][1] ~= "Mountpoint") then
+        if (FieldIndex > 3) then
+            return result:match("%d+");
+        elseif (fields[FieldIndex + 1][1] ~= "Mountpoint") then
             return result;
         elseif (result == FileName) then
             return true;
