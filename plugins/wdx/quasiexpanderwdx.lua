43b46a0f7f030021fa480781de69f8e16ecf3368
j2969719 <30291994+j2969719@users.noreply.github.com>
2017-11-02 01:09:00 +0300

    split

--- plugins/wdx/quasiexpanderwdx.lua
+++ plugins/wdx/quasiexpanderwdx.lua
@@ -3,9 +3,10 @@
 local parts = 5
 
 local separator = {
-    {"([^%s]+)", "space"}, 
-    {"([^-]+)", "-"}, 
-    {"([^/]+)", "/"}
+    {"%s", "space"}, 
+    {"%-", "-"}, 
+    {"%s%-%s", " - "}, 
+    {"/", "/"}    
 }
 
 function ContentGetSupportedField(Index)
@@ -73,12 +74,37 @@ function getPath(str)
     return str:match("(.*[/\\])");
 end
 
-function split(str, pat)
-    local i = 1;
-    local t = {};
-    for str in string.gmatch(str, pat) do
-        t[i] = str;
-        i = i + 1;
+-- https://stackoverflow.com/a/43582076
+
+local function gsplit(text, pattern, plain)
+    local splitStart, length = 1, #text
+    return function ()
+        if splitStart then
+            local sepStart, sepEnd = string.find(text, pattern, splitStart, plain)
+            local ret
+            if not sepStart then
+                ret = string.sub(text, splitStart)
+                splitStart = nil
+            elseif sepEnd < sepStart then
+                ret = string.sub(text, splitStart, sepStart)
+                if sepStart < length then
+                    splitStart = sepStart + 1
+                else
+                    splitStart = nil
+                end
+            else
+                ret = sepStart > splitStart and string.sub(text, splitStart, sepStart - 1) or ''
+                splitStart = sepEnd + 1
+            end
+            return ret
+        end
     end
-    return t;
+end
+
+function split(text, pattern, plain)
+    local ret = {}
+    for match in gsplit(text, pattern, plain) do
+        table.insert(ret, match)
+    end
+    return ret
 end
