a445caebe4e19111c5031bb198631b5cd038b591
j2969719 <j172933@mvrht.net>
2017-11-21 20:48:22 +0300

    helloworld

--- plugins/wlx/wlxwebkit/wlxwebkit.c
+++ plugins/wlx/wlxwebkit/wlxwebkit.c
@@ -1,11 +1,14 @@
 #include <gtk/gtk.h>
 #include <webkit/webkit.h>
+#include <string.h>
 #include "wlxplugin.h"
+
+#define _detectstring "(EXT=\"HTML\")|(EXT=\"HTM\")|(EXT=\"XHTM\")"
 
 
 GtkWidget* find_child(GtkWidget* parent, const gchar* name)
 {
-	if (g_strcasecmp(gtk_widget_get_name((GtkWidget*)parent), (gchar*)name) == 0) 
+	if (g_ascii_strcasecmp(gtk_widget_get_name((GtkWidget*)parent), (gchar*)name) == 0) 
 	{
 		return parent;
 	}
@@ -35,18 +38,19 @@ GtkWidget* find_child(GtkWidget* parent, const gchar* name)
 
 HWND DCPCALL ListLoad (HWND ParentWin, char* FileToLoad, int ShowFlags)
 {
-	WebKitWebView* webView;
 	GtkWidget *gFix;
+	GtkWidget* webView;
+
 	gFix = gtk_scrolled_window_new(NULL, NULL);
 	gtk_container_add(GTK_CONTAINER((GtkWidget*)(ParentWin)), gFix);
-	webView = WEBKIT_WEB_VIEW(webkit_web_view_new());
+	webView = webkit_web_view_new();
 	gtk_widget_set_name (webView, "webkitfrm");
 	gchar* fileUri = g_filename_to_uri(FileToLoad, NULL, NULL);
-	webkit_web_view_load_uri(webView, fileUri);
-	gtk_widget_grab_focus(GTK_WIDGET(webView));
-	gtk_container_add (GTK_CONTAINER (gFix), GTK_WIDGET(webView));
+	webkit_web_view_load_uri(WEBKIT_WEB_VIEW(webView), fileUri);
+	gtk_widget_grab_focus(webView);
+	gtk_container_add (GTK_CONTAINER (gFix), webView);
 
-	gtk_widget_show_all (gFix);
+	gtk_widget_show_all (gFix); 
 	return gFix;
 }
 
@@ -57,7 +61,7 @@ void ListCloseWindow(HWND ListWin)
 
 void DCPCALL ListGetDetectString(char* DetectString,int maxlen)
 {
-	strncpy(DetectString, "(EXT=\"HTML\")|(EXT=\"HTM\")|(EXT=\"XHTM\")", maxlen);
+	strncpy(DetectString, _detectstring, maxlen);
 }
 
 int DCPCALL ListSearchText(HWND ListWin,char* SearchString,int SearchParameter)
@@ -68,7 +72,7 @@ int DCPCALL ListSearchText(HWND ListWin,char* SearchString,int SearchParameter)
 		ss_case = TRUE;
 	if (SearchParameter & lcs_backwards)
 		ss_forward = FALSE;
-	webkit_web_view_search_text (find_child(GTK_CONTAINER((GtkWidget*)(ListWin)), "webkitfrm"), SearchString, ss_case, ss_forward, TRUE);
+	webkit_web_view_search_text (WEBKIT_WEB_VIEW(find_child(ListWin, "webkitfrm")), SearchString, ss_case, ss_forward, TRUE);
 
 }
 
@@ -77,12 +81,13 @@ int DCPCALL ListSendCommand(HWND ListWin,int Command,int Parameter)
 	switch(Command) 
 	{
 		case lc_copy :
-			webkit_web_view_copy_clipboard (find_child(GTK_CONTAINER((GtkWidget*)(ListWin)), "webkitfrm"));
+			webkit_web_view_copy_clipboard (WEBKIT_WEB_VIEW(find_child(ListWin, "webkitfrm")));
 			break;
 		case lc_selectall :
-			webkit_web_view_select_all (find_child(GTK_CONTAINER((GtkWidget*)(ListWin)), "webkitfrm"));
+			webkit_web_view_select_all (WEBKIT_WEB_VIEW(find_child(ListWin, "webkitfrm")));
 			break;
 		default :
 			printf("Command = %d\n", Command);
 	}
 }
+
