e056f31ccbfc15c756630d094bbf91466dedc0f0
j2969719 <30291994+j2969719@users.noreply.github.com>
2017-11-22 13:42:22 +0300

    helloworld

--- plugins/wdx/scripts/filenamematchwdx.lua
+++ plugins/wdx/scripts/filenamematchwdx.lua
@@ -1,18 +1,23 @@
 -- filenamematchwdx.lua
 
 local pattern = {
-    {"(%d+)%.", 1},
-    {"%-%s+(%d+)%.", 1},
-    {"^(.-)%s+%-", 8},
-    {"%-%s+(.+)", 8},
-    {".+%-%s(.+)$", 8},
-    {"%-%s+%d+%.(.+)$", 8},
-    {"%-%s(.-)%s+%-", 8},
+    {"(%d+)%.", 1, "(number)."}, -- pattern, type, name 
+    {"%-%s+(%d+)%.", 1, " (number)."}, 
+    {"^(.-)%s+%-", 8}, 
+    {"%-%s+(.+)", 8}, 
+    {".+%-%s(.+)$", 8}, 
+    {"%-%s+%d+%.(.+)$", 8}, 
+    {"%-%s(.-)%s+%-", 8}, 
+    {"%.tar%.(.-)$", 8, "tar.(xyz)"}, 
 }
 
 function ContentGetSupportedField(Index)
     if (pattern[Index + 1] ~= nil ) then
-        return pattern[Index + 1][1], "", pattern[Index + 1][2];
+        if (pattern[Index + 1][3] ~= nil) then
+            return pattern[Index + 1][3], "Filename|Filename.Ext|Path|Path with Filename.Ext", pattern[Index + 1][2];
+        else
+            return pattern[Index + 1][1], "Filename|Filename.Ext|Path|Path with Filename.Ext", pattern[Index + 1][2];
+        end
     else
         return '', '', 0; -- ft_nomorefields
     end
@@ -27,18 +32,31 @@ function ContentGetDetectString()
 end
 
 function ContentGetValue(FileName, FieldIndex, UnitIndex, flags)
-    local target = '';
     local tname = FileName;
     if (string.find(FileName, "[/\\]%.%.$")) then
         tname = string.sub(FileName, 1, - 3);
     end
-    target = tname:match("^.*[/\\](.+[^/\\])$");
+    if (UnitIndex == 0) then
+        tname = getFilename(tname);
+    elseif (UnitIndex == 1) then
+        tname = string.match(tname, "^.*[/\\](.+[^/\\])$");
+    elseif (UnitIndex == 2) then
+        tname = string.match(tname, "(.*[/\\])");
+    end
+    if (tname ~= nil) then
+        return tname:match(pattern[FieldIndex + 1][1]);
+    end
+    return nil;
+end
+
+function getFilename(str)
+    local target = string.match(str, "^.*[/\\](.+[^/\\])$");
     if (target ~= nil) then
         if (string.match(target, "(.+)%..+")) then
             target = string.match(target, "(.+)%..+");
         end
     else
-        target = '';
+        target = nil;
     end
-    return target:match(pattern[FieldIndex + 1][1]);
+    return target;
 end
