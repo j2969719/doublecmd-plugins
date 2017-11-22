d7d316270202fc30464bbd011745be6a0ca35dad
j2969719 <j172933@mvrht.net>
2017-11-22 07:55:40 +0300

    ...

--- plugins/wlx/dcpoppler/wlxPView.c
+++ plugins/wlx/dcpoppler/wlxPView.c
@@ -1,3 +1,26 @@
+/**
+*      @brief Too simple PDF Viewer
+*
+*      Copyright 2009 Yasser Nour <yassernour.wordpress.com>
+*
+*      This program is free software; you can redistribute it and/or modify
+*      it under the terms of the GNU General Public License as published by
+*      the Free Software Foundation; either version 2 of the License, or
+*      (at your option) any later version.
+*
+*      This program is distributed in the hope that it will be useful,
+*      but WITHOUT ANY WARRANTY; without even the implied warranty of
+*      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*      GNU General Public License for more details.
+*
+*      You should have received a copy of the GNU General Public License
+*      along with this program; if not, write to the Free Software
+*      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+*      MA 02110-1301, USA.
+*
+*      https://yassernour.wordpress.com/2010/04/04/how-hard-to-build-a-pdf-viewer/
+*/
+
 #include <gtk/gtk.h>
 #include <cairo.h>
 #include <poppler.h>
