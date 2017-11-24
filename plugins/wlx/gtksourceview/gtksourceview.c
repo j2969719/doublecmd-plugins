0c81d5907c945350e7909d084a35d253c250347e
j2969719 <j172933@mvrht.net>
2017-11-25 00:20:40 +0300

    helloworld

--- plugins/wlx/gtksourceview/gtksourceview.c
+++ plugins/wlx/gtksourceview/gtksourceview.c
@@ -182,9 +182,34 @@ void DCPCALL ListGetDetectString(char* DetectString,int maxlen)
 	strncpy(DetectString, _detectstring, maxlen);
 }
 
-int DCPCALL ListSearchDialog(HWND ListWin,int FindNext)
+int DCPCALL ListSearchText(HWND ListWin, char* SearchString,int SearchParameter)
 {
-	return LISTPLUGIN_OK;
+	GtkSourceBuffer *sBuf;
+	GtkTextMark *last_pos;
+	GtkTextIter iter, mstart, mend;
+	gboolean found;
+	
+	sBuf = g_object_get_data (G_OBJECT (ListWin), "srcbuf");
+	last_pos = gtk_text_buffer_get_mark (GTK_TEXT_BUFFER(sBuf), "last_pos");
+
+	gtk_text_buffer_get_start_iter(GTK_TEXT_BUFFER(sBuf), &iter);
+	
+	if (SearchParameter & lcs_findfirst)
+		gtk_text_buffer_get_start_iter(GTK_TEXT_BUFFER(sBuf), &iter);
+	else 
+		gtk_text_buffer_get_iter_at_mark (GTK_TEXT_BUFFER(sBuf), &iter, last_pos);
+
+	if (SearchParameter & lcs_backwards)
+		found = gtk_text_iter_backward_search (&iter, SearchString, GTK_TEXT_SEARCH_TEXT_ONLY, &mend, &mstart, NULL);
+	else
+		found = gtk_text_iter_forward_search (&iter, SearchString, GTK_TEXT_SEARCH_TEXT_ONLY, &mstart, &mend, NULL);
+
+	if (found)
+	{
+		gtk_text_buffer_select_range (GTK_TEXT_BUFFER(sBuf), &mstart, &mend);
+		gtk_text_buffer_create_mark (GTK_TEXT_BUFFER(sBuf), "last_pos", &mend, FALSE);
+	}
+	
 }
 
 int DCPCALL ListSendCommand(HWND ListWin,int Command,int Parameter)
