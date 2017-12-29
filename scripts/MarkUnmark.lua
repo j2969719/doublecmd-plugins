0ff5071a49512149ddeaefafa728bf7d28681b9a
j2969719 <j172933@mvrht.net>
2017-12-29 06:40:10 +0300

    helloworld

---
+++ scripts/MarkUnmark.lua
@@ -0,0 +1,21 @@
+--[[
+DOUBLECMD#TOOLBAR#XMLDATA<?xml version="1.0" encoding="UTF-8"?>
+<doublecmd>
+  <Command>
+    <ID>{58430566-6032-4BCC-986E-FE206ADA40FF}</ID>
+    <Icon>cm_markmarkall</Icon>
+    <Hint>MarkUnmark</Hint>
+    <Command>cm_ExecuteScript</Command>
+    <Param>%COMMANDER_PATH%/scripts/MarkUnmark.lua</Param>
+    <Param>%ps2</Param>
+  </Command>
+</doublecmd>
+]]
+
+local params = {...}
+
+if (params[1] == '') then    
+    DC.ExecuteCommand("cm_MarkMarkAll");
+else
+    DC.ExecuteCommand("cm_MarkUnmarkAll");
+end
