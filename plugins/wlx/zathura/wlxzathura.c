80adaef25d508a129c75f418b0f2fbb93129a8d6
j2969719 <j172933@mvrht.net>
2017-12-31 09:41:17 +0300

    helloworld

--- plugins/wlx/zathura/wlxzathura.c
+++ plugins/wlx/zathura/wlxzathura.c
@@ -1,4 +1,8 @@
+#define _GNU_SOURCE
 #include <gtk/gtk.h>
+#include <dlfcn.h>
+#include <limits.h>
+#include <string.h>
 #include "wlxplugin.h"
 
 #define _detectstring  "(EXT=\"PDF\")|(EXT=\"DJVU\")|(EXT=\"DJV\")|(EXT=\"PS\")|(EXT=\"CBR\")|(EXT=\"CBZ\")"
@@ -6,17 +10,58 @@
 
 HWND DCPCALL ListLoad(HWND ParentWin, char* FileToLoad, int ShowFlags)
 {
-
+	Dl_info dlinfo;
+	static char plg_path[PATH_MAX], cfg_path[PATH_MAX];
 	GtkWidget *gFix;
 	GtkWidget *zathura;
+	GKeyFile *cfg;
+	GError *err = NULL;
+	gchar *param, *command;
+	gboolean bval;
 
+	memset(&dlinfo, 0, sizeof(dlinfo));
+	if (dladdr(cfg_path, &dlinfo) != 0)
+	{
+		g_strlcpy(cfg_path, dlinfo.dli_fname, PATH_MAX);
+		char *pos = strrchr(dlinfo.dli_fname, '/');
+		if (pos)
+			g_strlcpy(plg_path, dlinfo.dli_fname, pos-dlinfo.dli_fname+1);
+		pos = strrchr(cfg_path, '/');
+		if (pos)
+			strcpy(pos + 1, "settings.ini");
+	}
+	cfg = g_key_file_new();
+	if (!g_key_file_load_from_file(cfg, cfg_path, G_KEY_FILE_KEEP_COMMENTS, &err))
+	{
+		g_print("zathura.wlx (%s): %s\n", cfg_path, (err)->message);
+		param = " ";
+		bval = FALSE;
+	}
+	else
+	{
+		param = g_key_file_get_string(cfg, "Settings", "Params", NULL);
+		if (!param)
+			param = " ";
+		bval = g_key_file_get_boolean(cfg, "Settings", "ExternalCfg", &err);
+		if (!bval && !err)
+			bval = FALSE;
+		else
+			bval = TRUE;
+	}
+	g_key_file_free(cfg);
+	if (err)
+		g_error_free(err);
 	gFix = gtk_vbox_new(FALSE , 5);
 	gtk_container_add(GTK_CONTAINER (GTK_WIDGET(ParentWin)), gFix);
 
 	zathura = gtk_socket_new();
 	gtk_container_add(GTK_CONTAINER(gFix), zathura);
 	GdkNativeWindow id = gtk_socket_get_id(GTK_SOCKET(zathura));
-	gchar *command = g_strdup_printf("zathura --reparent=%d \"%s\"", id, FileToLoad);
+	if (bval)
+		command = g_strdup_printf("zathura --reparent=%d \"%s\" %s --config-dir=\"%s\" --data-dir=\"%s\"", id, FileToLoad, param, plg_path, plg_path);
+	else
+		command = g_strdup_printf("zathura --reparent=%d \"%s\" %s", id, FileToLoad, param);
+	//g_print(command);
 	g_spawn_command_line_async(command, NULL);
 	g_free(command);
 
