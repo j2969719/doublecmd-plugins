611a1ca5badec6652db32e1685697e45026d549e
j2969719 <j172933@mvrht.net>
2018-01-01 17:06:44 +0300

    helloworld

--- plugins/wlx/mpv/wlxmpv.c
+++ plugins/wlx/mpv/wlxmpv.c
@@ -5,7 +5,7 @@
 #define _detectstring  "(EXT=\"AVI\")|(EXT=\"MKV\")|(EXT=\"FLV\")|(EXT=\"MPG\")|\
 (EXT=\"MPEG\")|(EXT=\"MP4\")|(EXT=\"3GP\")|(EXT=\"MP3\")|(EXT=\"OGG\")|(EXT=\"WMA\")|\
 (EXT=\"BIK\")|(EXT=\"VOC\")|(EXT=\"WAV\")|(EXT=\"WEBM\")|(EXT=\"VOB\")|(EXT=\"ROQ\")|\
-(EXT=\"IVF\")|(EXT=\"MOV\")|(EXT=\"FLAC\")"
+(EXT=\"IVF\")|(EXT=\"MOV\")|(EXT=\"MID\")"
 
 
 HWND DCPCALL ListLoad(HWND ParentWin, char* FileToLoad, int ShowFlags)
@@ -13,22 +13,22 @@ HWND DCPCALL ListLoad(HWND ParentWin, char* FileToLoad, int ShowFlags)
 
 	GtkWidget *gFix;
 	GtkWidget *mpv;
-	GtkWidget *label;
 
 	gFix = gtk_vbox_new(FALSE , 5);
 	gtk_container_add(GTK_CONTAINER (GTK_WIDGET(ParentWin)), gFix);
 
 	mpv = gtk_drawing_area_new();
 	gtk_container_add(GTK_CONTAINER(gFix), mpv);
+	gtk_widget_realize(mpv);
 	GdkNativeWindow id = GDK_WINDOW_XID(gtk_widget_get_window(mpv));
 	gchar *command = g_strdup_printf("mpv --force-window=yes --loop --wid=%d \"%s\"", id, FileToLoad);
 	if (id!=0)
 		g_spawn_command_line_async(command, NULL);
 	else
 	{
-		gtk_widget_destroy(mpv);
-		label = gtk_label_new("no luck:(\nmove the cursor up or down...");
-		gtk_container_add(GTK_CONTAINER(gFix), label);
+		gtk_widget_destroy(gFix);
+		g_print("no luck:( window xid = 0\n");
+		return NULL;
 	}
 	g_free(command);
 
