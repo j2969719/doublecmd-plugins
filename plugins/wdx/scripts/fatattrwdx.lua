8cdcff45353196c25ab705a9ac62e4f2a4dcc15b
j2969719 <j172933@mvrht.net>
2018-01-08 23:16:07 +0300

    helloworld

---
+++ plugins/wdx/scripts/fatattrwdx.lua
@@ -0,0 +1,86 @@
+local cmd = "/usr/bin/fatattr"
+local output = ''
+local dosattr = ''
+local filename = ''
+
+function ContentGetSupportedField(Index)
+    if (Index == 0) then
+        return 'archive', '', 6; 
+    elseif (Index == 1) then
+        return 'hidden', '', 6;
+    elseif (Index == 2) then
+        return 'readonly', '', 6;
+    elseif (Index == 3) then
+        return 'system', '', 6;
+    elseif (Index == 4) then
+        return 'attr string', '', 8;
+    end
+    return '', '', 0; -- ft_nomorefields
+end
+
+function ContentGetDetectString()
+    return nil; -- return detect string
+end
+
+function ContentGetValue(FileName, FieldIndex, UnitIndex, flags)
+    if (string.find(FileName, "[^" .. SysUtils.PathDelim .. "]%.%.$")) then
+        return nil;
+    end
+    if (filename ~= FileName) or (FieldIndex == 4) then        
+        local attr = SysUtils.FileGetAttr(FileName);
+        if (attr > 0) then
+            if (math.floor(attr / 0x00000004) % 2 ~= 0)  then
+                return nil; 
+            end
+        end        
+        local handle = io.popen(cmd .. ' "' .. FileName .. '"');
+        output = handle:read("*a");
+        handle:close();
+        if (output:sub(1, 9) ~= FileName:sub(1, 9)) then
+            dosattr = output:sub(1, 9);
+        end
+        filename = FileName;
+    end
+    if (FieldIndex == 0) then
+        return chechattr(dosattr, 'a');
+    elseif (FieldIndex == 1) then
+        return chechattr(dosattr, 'h');
+    elseif (FieldIndex == 2) then
+        return chechattr(dosattr, 'r');
+    elseif (FieldIndex == 3) then
+        return chechattr(dosattr, 's');
+    elseif (FieldIndex == 4) then
+        local str = '';       
+        if (chechattr(dosattr, 'r')) then
+            str = str .. 'r';
+        else
+            str = str .. '-';
+        end
+        if (chechattr(dosattr, 'a')) then
+            str = str .. 'a';
+        else
+            str = str .. '-';
+        end
+        if (chechattr(dosattr, 'h')) then
+            str = str .. 'h';
+        else
+            str = str .. '-';
+        end
+        if (chechattr(dosattr, 's')) then
+            str = str .. 's';
+        else
+            str = str .. '-';
+        end
+        return str;
+    end
+    return nil; -- invalid
+end
+
+function chechattr(vattr, val)
+    if (vattr ~= nil) then
+        if (string.find(vattr, val)) then
+            return true;
+        end
+    end
+    return false;   
+end
---
+++ plugins/wdx/scripts/getfattrwdx.lua
@@ -0,0 +1,90 @@
+local cmd = "/usr/bin/getfattr"
+local output = ''
+local dosattr = ''
+local filename = ''
+
+function ContentGetSupportedField(Index)
+    if (Index == 0) then
+        return 'archive', '', 6; 
+    elseif (Index == 1) then
+        return 'compressed', '', 6;
+    elseif (Index == 2) then
+        return 'encrypted', '', 6;
+    elseif (Index == 3) then
+        return 'hidden', '', 6;
+    elseif (Index == 4) then
+        return 'readonly', '', 6;
+    elseif (Index == 5) then
+        return 'system', '', 6;
+    elseif (Index == 6) then
+        return 'attr string', '', 8;
+    end
+    return '', '', 0; -- ft_nomorefields
+end
+
+function ContentGetDetectString()
+    return nil; -- return detect string
+end
+
+function ContentGetValue(FileName, FieldIndex, UnitIndex, flags)
+    if (string.find(FileName, "[^" .. SysUtils.PathDelim .. "]%.%.$")) then
+        return nil;
+    end
+    if (filename ~= FileName) or (FieldIndex == 6) then
+        local attr = SysUtils.FileGetAttr(FileName);      
+        if (chechattr(attr, 0x00000004))  then
+            return nil; 
+        end
+        local handle = io.popen(cmd .. ' --absolute-names -d "' .. FileName .. '"');
+        output = handle:read("*a");
+        handle:close();
+        dosattr = output:match('%.DOSATTRIB="([^"]+)');
+        filename = FileName;
+    end
+    if (FieldIndex == 0) then
+        return chechattr(dosattr, 0x20);
+    elseif (FieldIndex == 1) then
+        return chechattr(dosattr, 0x800);
+    elseif (FieldIndex == 2) then
+        return chechattr(dosattr, 0x4000);
+    elseif (FieldIndex == 3) then
+        return chechattr(dosattr, 0x2);
+    elseif (FieldIndex == 4) then
+        return chechattr(dosattr, 0x1);
+    elseif (FieldIndex == 5) then
+        return chechattr(dosattr, 0x4);
+    elseif (FieldIndex == 6) then
+        local str = '';       
+        if (chechattr(dosattr, 0x1)) then
+            str = str .. 'r';
+        else
+            str = str .. '-';
+        end
+        if (chechattr(dosattr, 0x20)) then
+            str = str .. 'a';
+        else
+            str = str .. '-';
+        end
+        if (chechattr(dosattr, 0x2)) then
+            str = str .. 'h';
+        else
+            str = str .. '-';
+        end
+        if (chechattr(dosattr, 0x4)) then
+            str = str .. 's';
+        else
+            str = str .. '-';
+        end
+        return str;
+    end
+    return nil; -- invalid
+end
+
+function chechattr(vattr, val)
+    if (vattr ~= nil) then
+        if (math.floor(vattr / val) % 2 ~= 0)  then
+            return true;        
+        end
+    end
+    return false; 
+end
