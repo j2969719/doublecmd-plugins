371669856cf81d60078b1329ab10d426501910a4
j2969719 <j172933@mvrht.net>
2018-01-09 10:36:55 +0300

    helloworld

---
+++ plugins/wlx/wlxdisplay/wlxdisplay.c
@@ -0,0 +1,42 @@
+#include <gtk/gtk.h>
+#include <string.h>
+#include "wlxplugin.h"
+
+#define _detectstring  "(EXT=\"TTF\")|(EXT=\"OTF\")|(EXT=\"PCX\")"
+#define _cmdstring "display -backdrop -window %d -window-group %d \"%s\""
+
+HWND DCPCALL ListLoad(HWND ParentWin, char* FileToLoad, int ShowFlags)
+{
+
+	GtkWidget *gFix;
+	GtkWidget *socket;
+
+	gFix = gtk_vbox_new(FALSE , 5);
+	gtk_container_add(GTK_CONTAINER (GTK_WIDGET(ParentWin)), gFix);
+
+	socket = gtk_socket_new();
+	gtk_container_add(GTK_CONTAINER(gFix), socket);
+	GdkNativeWindow id = gtk_socket_get_id(GTK_SOCKET(socket));
+	gchar *command = g_strdup_printf(_cmdstring, id, id, FileToLoad);
+	g_spawn_command_line_async(command, NULL);
+	g_free(command);
+
+	gtk_widget_show_all(gFix);
+
+	return gFix;
+}
+
+void DCPCALL ListCloseWindow(HWND ListWin)
+{
+	gtk_widget_destroy(GTK_WIDGET(ListWin));
+}
+
+void DCPCALL ListGetDetectString(char* DetectString,int maxlen)
+{
+	strncpy(DetectString, _detectstring, maxlen);
+}
+
+int DCPCALL ListSearchDialog(HWND ListWin,int FindNext)
+{
+	return LISTPLUGIN_OK;
+}
