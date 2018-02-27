9ed0876fd44e992efc29324621cd4cbb161adc9a
j2969719 <j172933@mvrht.net>
2018-02-27 11:41:45 +0300

    helloworld

--- plugins/wlx/dcpoppler/wlxPView.c
+++ plugins/wlx/dcpoppler/wlxPView.c
@@ -24,29 +24,9 @@
 #include <gtk/gtk.h>
 #include <cairo.h>
 #include <poppler.h>
-#include <math.h>
-#include <string.h>
 #include "wlxplugin.h"
 
 #define _detectstring "EXT=\"PDF\""
-
-GtkWidget *label;
-GtkWidget  *canvas;
-
-PopplerPage *ppage;
-PopplerDocument *document;
-cairo_surface_t *surface;
-int current_page = 0;
-int total_pages = 0;
-
-void update_numpages()
-{
-	gchar *str;
-	str = g_strdup_printf("%d / %d", current_page + 1, total_pages);
-
-	gtk_label_set_text(GTK_LABEL(label), str);
-	g_free(str);
-}
 
 void reset_scroll(GtkScrolledWindow *scrolled_window)
 {
@@ -58,73 +38,66 @@ void reset_scroll(GtkScrolledWindow *scrolled_window)
 	gtk_scrolled_window_set_hadjustment(scrolled_window, tmp);
 }
 
-static void canvas_expose_event(GtkWidget *widget, GdkEventExpose *event)
+static void canvas_expose_event(GtkWidget *widget)
 {
 	cairo_t *cr;
-
-	gdk_window_clear(canvas->window);
-	cr = gdk_cairo_create(canvas->window);
-
-	cairo_set_source_surface(cr, surface, 0, 0);
-	cairo_paint(cr);
-	cairo_destroy(cr);
-}
-
-static void redraw_callback(void *data)
-{
-	gtk_widget_queue_draw(canvas);
-}
-
-static void view_set_page(int page)
-{
-	int err;
-	int w, h;
 	double width, height;
-	cairo_t *cr;
 
-	ppage = poppler_document_get_page(document, page);
+	gdk_window_clear(widget->window);
+	cr = gdk_cairo_create(widget->window);
+
+	PopplerPage *ppage = g_object_get_data(G_OBJECT(widget), "page");
 	poppler_page_get_size(ppage, &width, &height);
-	w = (int) ceil(width);
-	h = (int) ceil(height);
-	cairo_surface_destroy(surface);
-	surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, w, h);
-	cr = cairo_create(surface);
+	gtk_widget_set_size_request(widget, (guint)width, (guint)height);
 	poppler_page_render(ppage, cr);
-	cairo_destroy(cr);
-	gtk_widget_set_size_request(canvas, w, h);
-	gtk_widget_queue_draw(canvas);
-	update_numpages();
+
+	guint current_page = GPOINTER_TO_UINT(g_object_get_data(G_OBJECT(widget), "cpage"));
+	guint total_pages = GPOINTER_TO_UINT(g_object_get_data(G_OBJECT(widget), "tpages"));
+	gchar *pstr = g_strdup_printf("%d / %d", current_page + 1, total_pages);
+	GtkWidget *label = g_object_get_data(G_OBJECT(widget), "plabel");
+	gtk_label_set_text(GTK_LABEL(label), pstr);
+	g_free(pstr);
 }
 
-static void tb_back_clicked(GtkToolItem *tooleditcut, GtkWindow *parentWindow)
+static void view_set_page(GtkWidget *canvas, guint page)
 {
+	PopplerDocument *document =  g_object_get_data(G_OBJECT(canvas), "doc");
+	PopplerPage *ppage = poppler_document_get_page(document, page);
+	g_object_set_data_full(G_OBJECT(canvas), "page", ppage, (GDestroyNotify)g_object_unref);
+	g_object_set_data(G_OBJECT(canvas), "cpage", GUINT_TO_POINTER(page));
+	canvas_expose_event(canvas);
+	reset_scroll(GTK_SCROLLED_WINDOW(g_object_get_data(G_OBJECT(canvas), "pscroll")));
+}
+
+static void tb_back_clicked(GtkToolItem *tooleditcopy, GtkWidget *canvas)
+{
+	guint current_page = GPOINTER_TO_UINT(g_object_get_data(G_OBJECT(canvas), "cpage"));
+	guint total_pages = GPOINTER_TO_UINT(g_object_get_data(G_OBJECT(canvas), "tpages"));
+
 	if (current_page != 0)
 		current_page--;
 
-	view_set_page(current_page);
-
-	reset_scroll(GTK_SCROLLED_WINDOW(parentWindow));
+	view_set_page(canvas, current_page);
 }
 
-static void tb_forward_clicked(GtkToolItem *tooleditcopy, GtkWindow *parentWindow)
+static void tb_forward_clicked(GtkToolItem *tooleditcopy, GtkWidget *canvas)
 {
+	guint current_page = GPOINTER_TO_UINT(g_object_get_data(G_OBJECT(canvas), "cpage"));
+	guint total_pages = GPOINTER_TO_UINT(g_object_get_data(G_OBJECT(canvas), "tpages"));
+
 	if (current_page != (total_pages - 1))
 		current_page++;
 
-	view_set_page(current_page);
-
-	reset_scroll(GTK_SCROLLED_WINDOW(parentWindow));
+	view_set_page(canvas, current_page);
 }
 
-static void tb_home_clicked(GtkToolItem *tooleditpaste, GtkWindow *parentWindow)
+static void tb_home_clicked(GtkToolItem *tooleditcopy, GtkWidget *canvas)
 {
-	current_page = 0;
-	view_set_page(0);
-
-	reset_scroll(GTK_SCROLLED_WINDOW(parentWindow));
+	guint current_page = 0;
+	view_set_page(canvas, current_page);
 }
 
-static void tb_info_clicked(GtkToolItem *tooleditpaste, GtkWindow *parentWindow)
+static void tb_info_clicked(GtkToolItem *tooleditpaste, GtkWidget *canvas)
 {
 	GtkWidget *dialog;
 	GtkWidget *scroll;
@@ -132,6 +105,7 @@ static void tb_info_clicked(GtkToolItem *tooleditpaste, GtkWindow *parentWindow)
 	GtkWidget *mlabel;
 	gchar *mtext, *itext;
 
+	PopplerDocument *document =  g_object_get_data(G_OBJECT(canvas), "doc");
 	dialog = gtk_dialog_new();
 	gtk_window_set_title(GTK_WINDOW(dialog), "Info");
 	gtk_window_set_modal(GTK_WINDOW(dialog), TRUE);
@@ -167,20 +141,23 @@ static void tb_info_clicked(GtkToolItem *tooleditpaste, GtkWindow *parentWindow)
 
 HWND DCPCALL ListLoad(HWND ParentWin, char* FileToLoad, int ShowFlags)
 {
-
-	GtkWidget  *vscroll;
-	GtkWidget  *gFix;
-	GdkColor  color;
-	GtkWidget  *tb1;
-	GtkToolItem  *tb_back;
-	GtkToolItem  *tb_forward;
-	GtkToolItem  *tb_home;
-	GtkToolItem  *tb_separator;
-	GtkToolItem  *tb_pages;
-	GtkToolItem  *tb_info;
+	GtkWidget *gFix;
+	GtkWidget *vscroll;
+	GtkWidget *canvas;
+	GtkWidget *label;
+	GtkWidget *tb1;
+	GtkToolItem *tb_back;
+	GtkToolItem *tb_forward;
+	GtkToolItem *tb_home;
+	GtkToolItem *tb_separator;
+	GtkToolItem *tb_pages;
+	GtkToolItem *tb_info;
+	GdkColor color;
+	guint current_page = 0;
+	guint total_pages;
 
 	gchar* fileUri = g_filename_to_uri(FileToLoad, NULL, NULL);
-	document = poppler_document_new_from_file(fileUri, NULL, NULL);
+	PopplerDocument *document = poppler_document_new_from_file(fileUri, NULL, NULL);
 
 	if (fileUri)
 		g_free(fileUri);
@@ -188,35 +165,42 @@ HWND DCPCALL ListLoad(HWND ParentWin, char* FileToLoad, int ShowFlags)
 	if (document == NULL)
 		return NULL;
 
-	current_page = 0;
 	gFix = gtk_vbox_new(FALSE, 0);
 	gtk_container_add(GTK_CONTAINER((GtkWidget*)(ParentWin)), gFix);
 
-	vscroll = gtk_scrolled_window_new(NULL, NULL);
 	tb1 = gtk_toolbar_new();
 	gtk_toolbar_set_show_arrow(GTK_TOOLBAR(tb1), TRUE);
 	gtk_toolbar_set_style(GTK_TOOLBAR(tb1), GTK_TOOLBAR_ICONS);
 	gtk_box_pack_start(GTK_BOX(gFix), tb1, FALSE, FALSE, 2);
 
+	vscroll = gtk_scrolled_window_new(NULL, NULL);
+	gtk_box_pack_start(GTK_BOX(gFix), vscroll, TRUE, TRUE, 2);
+	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(vscroll), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+	canvas = gtk_drawing_area_new();
+	gdk_color_parse("white", &color);
+	gtk_widget_modify_bg(canvas, GTK_STATE_NORMAL, &color);
+	gtk_scrolled_window_add_with_viewport(GTK_SCROLLED_WINDOW(vscroll), canvas);
+	g_signal_connect(G_OBJECT(canvas), "expose_event", G_CALLBACK(canvas_expose_event), NULL);
+
 	tb_back = gtk_tool_button_new_from_stock(GTK_STOCK_GO_BACK);
 	gtk_toolbar_insert(GTK_TOOLBAR(tb1), tb_back, 0);
 	gtk_widget_set_tooltip_text(GTK_WIDGET(tb_back), "Previous Page");
-	g_signal_connect(G_OBJECT(tb_back), "clicked", G_CALLBACK(tb_back_clicked), (gpointer)(GtkWidget*)(vscroll));
+	g_signal_connect(G_OBJECT(tb_back), "clicked", G_CALLBACK(tb_back_clicked), (gpointer)canvas);
 
 	tb_forward = gtk_tool_button_new_from_stock(GTK_STOCK_GO_FORWARD);
 	gtk_toolbar_insert(GTK_TOOLBAR(tb1), tb_forward, 1);
 	gtk_widget_set_tooltip_text(GTK_WIDGET(tb_forward), "Next page");
-	g_signal_connect(G_OBJECT(tb_forward), "clicked", G_CALLBACK(tb_forward_clicked), (gpointer)(GtkWidget*)(vscroll));
+	g_signal_connect(G_OBJECT(tb_forward), "clicked", G_CALLBACK(tb_forward_clicked), (gpointer)canvas);
 
 	tb_home = gtk_tool_button_new_from_stock(GTK_STOCK_HOME);
 	gtk_toolbar_insert(GTK_TOOLBAR(tb1), tb_home, 2);
 	gtk_widget_set_tooltip_text(GTK_WIDGET(tb_home), "First page");
-	g_signal_connect(G_OBJECT(tb_home), "clicked", G_CALLBACK(tb_home_clicked), (gpointer)(GtkWidget*)(vscroll));
+	g_signal_connect(G_OBJECT(tb_home), "clicked", G_CALLBACK(tb_home_clicked), (gpointer)canvas);
 
 	tb_info = gtk_tool_button_new_from_stock(GTK_STOCK_INFO);
 	gtk_toolbar_insert(GTK_TOOLBAR(tb1), tb_info, 3);
 	gtk_widget_set_tooltip_text(GTK_WIDGET(tb_info), "Info");
-	g_signal_connect(G_OBJECT(tb_info), "clicked", G_CALLBACK(tb_info_clicked), (gpointer)(GtkWidget*)(vscroll));
+	g_signal_connect(G_OBJECT(tb_info), "clicked", G_CALLBACK(tb_info_clicked), (gpointer)canvas);
 
 
 	tb_separator = gtk_separator_tool_item_new();
@@ -228,16 +212,14 @@ HWND DCPCALL ListLoad(HWND ParentWin, char* FileToLoad, int ShowFlags)
 	gtk_toolbar_insert(GTK_TOOLBAR(tb1), tb_pages, 5);
 	gtk_widget_set_tooltip_text(GTK_WIDGET(tb_pages), "Current page");
 
-	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(vscroll), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
-	gtk_box_pack_start(GTK_BOX(gFix), vscroll, TRUE, TRUE, 2);
-	canvas = gtk_drawing_area_new();
-	gdk_color_parse("white", &color);
-	gtk_widget_modify_bg(canvas, GTK_STATE_NORMAL, &color);
-	gtk_scrolled_window_add_with_viewport(GTK_SCROLLED_WINDOW(vscroll), canvas);
-	g_signal_connect(G_OBJECT(canvas), "expose_event", G_CALLBACK(canvas_expose_event), (gpointer)(GtkWidget*)(ParentWin));
-
+	PopplerPage *ppage = poppler_document_get_page(document, current_page);
 	total_pages = poppler_document_get_n_pages(document);
-	view_set_page(0);
+	g_object_set_data_full(G_OBJECT(canvas), "doc", document, (GDestroyNotify)g_object_unref);
+	g_object_set_data(G_OBJECT(canvas), "tpages", GUINT_TO_POINTER(total_pages));
+	g_object_set_data(G_OBJECT(canvas), "cpage", GUINT_TO_POINTER(current_page));
+	g_object_set_data_full(G_OBJECT(canvas), "page", ppage, (GDestroyNotify)g_object_unref);
+	g_object_set_data(G_OBJECT(canvas), "plabel", label);
+	g_object_set_data(G_OBJECT(canvas), "pscroll", vscroll);
 
 	gtk_widget_grab_focus(vscroll);
 	gtk_widget_show_all(gFix);
