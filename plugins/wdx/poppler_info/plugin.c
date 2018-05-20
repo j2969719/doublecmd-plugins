39e0aa0078604f84627ed0e059bce5abac5e8b42
j2969719 <j172933@mvrht.net>
2018-05-20 23:05:21 +0300

    helloworld

--- plugins/wdx/poppler_info/plugin.c
+++ plugins/wdx/poppler_info/plugin.c
@@ -1,8 +1,10 @@
 #include <glib.h>
+#include <gio/gio.h>
 #include <poppler.h>
 #include "wdxplugin.h"
 
 #define _detectstring "EXT=\"PDF\""
+#define _validcontent "application/pdf"
 
 typedef struct _field
 {
@@ -15,15 +17,37 @@ typedef struct _field
 
 FIELD fields[] =
 {
-	{"Title",	ft_string,	""},
-	{"Author",	ft_string,	""},
-	{"Subject",	ft_string,	""},
-	{"Creator",	ft_string,	""},
-	{"Producer",	ft_string,	""},
-	{"Keywords",	ft_string,	""},
-	{"Version",	ft_string,	""},
-	{"Total Pages",	ft_numeric_32,	""},
+	{"Title",			ft_string,	""},
+	{"Author",			ft_string,	""},
+	{"Subject",			ft_string,	""},
+	{"Creator",			ft_string,	""},
+	{"Producer",			ft_string,	""},
+	{"Keywords",			ft_string,	""},
+	{"Version",			ft_string,	""},
+	{"Total Pages",			ft_numeric_32,	""},
+	{"Creation",			ft_datetime,	""},
+	{"Modification",		ft_datetime,	""},
+	{"Attachments",			ft_boolean,	""},
+	{"Linearized",			ft_boolean,	""},
+	{"Password protected",		ft_boolean,	""},
+	{"Damaged",			ft_boolean,	""},
+	{"Can be printer",		ft_boolean,	""},
+	{"Can be modified",		ft_boolean,	""},
+	{"Can be copied",		ft_boolean,	""},
+	{"Ok to add notes",		ft_boolean,	""},
+	{"Ok to fill form",		ft_boolean,	""},
 };
+
+gboolean UnixTimeToFileTime(uint64_t unix_time, LPFILETIME FileTime)
+{
+	if (unix_time == -1)
+		return FALSE;
+
+	unix_time = (unix_time * 10000000) + 0x019DB1DED53E8000;
+	FileTime->dwLowDateTime = (DWORD)unix_time;
+	FileTime->dwHighDateTime = unix_time >> 32;
+	return TRUE;
+}
 
 int DCPCALL ContentGetSupportedField(int FieldIndex, char* FieldName, char* Units, int maxlen)
 {
@@ -44,9 +68,35 @@ int DCPCALL ContentGetDetectString(char* DetectString, int maxlen)
 int DCPCALL ContentGetValue(char* FileName, int FieldIndex, int UnitIndex, void* FieldValue, int maxlen, int flags)
 {
 	char *strvalue;
+	uint64_t timevalue;
 	gboolean vempty = FALSE;
+	gboolean tmpbool = FALSE;
+	GError *err = NULL;
+	gchar* content_type = g_content_type_guess(FileName, NULL, 0, &tmpbool);
 	gchar* fileUri = g_filename_to_uri(FileName, NULL, NULL);
-	PopplerDocument *document = poppler_document_new_from_file(fileUri, NULL, NULL);
+
+	if (!g_content_type_equals(content_type, _validcontent))
+		return ft_fileerror;
+
+	PopplerDocument *document = poppler_document_new_from_file(fileUri, NULL, &err);
+
+	if (err)
+	{
+		if ((FieldIndex == 12) && (err->code == POPPLER_ERROR_ENCRYPTED))
+		{
+			g_error_free(err);
+			*(int*)FieldValue = 1;
+			return fields[FieldIndex].type;
+		}
+		else if ((FieldIndex == 13) && (err->code == POPPLER_ERROR_DAMAGED))
+		{
+			g_error_free(err);
+			*(int*)FieldValue = 1;
+			return fields[FieldIndex].type;
+		}
+
+		g_error_free(err);
+	}
 
 	if (document == NULL)
 		return ft_fileerror;
@@ -125,6 +175,78 @@ int DCPCALL ContentGetValue(char* FileName, int FieldIndex, int UnitIndex, void*
 
 	case 7:
 		*(int*)FieldValue = poppler_document_get_n_pages(document);
+		break;
+
+	case 8:
+		timevalue = poppler_document_get_creation_date(document);
+
+		if (!UnixTimeToFileTime(timevalue, (FILETIME*)FieldValue))
+			vempty = TRUE;
+
+	case 9:
+		timevalue = poppler_document_get_modification_date(document);
+
+		if (!UnixTimeToFileTime(timevalue, (FILETIME*)FieldValue))
+			vempty = TRUE;
+
+	case 10:
+		if (poppler_document_has_attachments(document))
+			*(int*)FieldValue = 1;
+		else
+			*(int*)FieldValue = 0;
+
+		break;
+
+	case 11:
+		if (poppler_document_is_linearized(document))
+			*(int*)FieldValue = 1;
+		else
+			*(int*)FieldValue = 0;
+
+		break;
+
+	case 12:
+		*(int*)FieldValue = 0;
+		break;
+
+	case 14:
+		if (poppler_document_get_permissions(document) & POPPLER_PERMISSIONS_OK_TO_PRINT)
+			*(int*)FieldValue = 1;
+		else
+			*(int*)FieldValue = 0;
+
+		break;
+
+	case 15:
+		if (poppler_document_get_permissions(document) & POPPLER_PERMISSIONS_OK_TO_MODIFY)
+			*(int*)FieldValue = 1;
+		else
+			*(int*)FieldValue = 0;
+
+		break;
+
+	case 16:
+		if (poppler_document_get_permissions(document) & POPPLER_PERMISSIONS_OK_TO_COPY)
+			*(int*)FieldValue = 1;
+		else
+			*(int*)FieldValue = 0;
+
+		break;
+
+	case 17:
+		if (poppler_document_get_permissions(document) & POPPLER_PERMISSIONS_OK_TO_ADD_NOTES)
+			*(int*)FieldValue = 1;
+		else
+			*(int*)FieldValue = 0;
+
+		break;
+
+	case 18:
+		if (poppler_document_get_permissions(document) & POPPLER_PERMISSIONS_OK_TO_FILL_FORM)
+			*(int*)FieldValue = 1;
+		else
+			*(int*)FieldValue = 0;
+
 		break;
 
 	default:
