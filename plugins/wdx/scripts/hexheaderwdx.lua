4b5ef978a7658faae2fa0d9284039baa505dd65e
j2969719 <j172933@mvrht.net>
2018-05-24 10:41:50 +0300

    Alexx2000's fix

--- plugins/wdx/scripts/hexheaderwdx.lua
+++ plugins/wdx/scripts/hexheaderwdx.lua
@@ -1,11 +1,10 @@
 -- hexheaderwdx.lua
 
 local offset = {
-    "0x10", 
-    "0x000020", 
-    "0x20B0"
+    "0x10",
+    "0x000020",
+    "0x20B0",
 }
-local sizeLimit = 1000000
 
 function ContentGetSupportedField(Index)
     local submi = '1 byte';
@@ -15,7 +14,7 @@ function ContentGetSupportedField(Index)
     if (Index == 0) then
         return 'header', submi, 8; -- FieldName,Units,ft_string
     elseif (Index == 1) then
-        return 'all file(<' .. sizeLimit .. 'b)', '', 8;  -- some problems with ft_fulltext
+        return 'all file', '', 9;
     elseif (offset[Index - 1] ~= nil) then
         return 'offset: ' .. offset[Index - 1], submi, 8;
     end
@@ -35,28 +34,45 @@ function ContentGetValue(FileName, FieldIndex, UnitIndex, flags)
         return nil;
     end
     local f = io.open(FileName, "rb");
-    local bytes = nil;
+    local step = 1024;
+    local result = nil;
     if (f ~= nil) then
         if (FieldIndex == 0) then
-            bytes = f:read(UnitIndex + 1);
+            result = toHex(f:read(UnitIndex + 1));
         elseif (FieldIndex == 1) then
-            if (f:seek("end") <= sizeLimit) then
-                f:seek("set", 0);
-                bytes = f:read("*a");
-            end
-        else 
-            f:seek("set", tonumber(offset[FieldIndex - 1]));
-            bytes = f:read(UnitIndex + 1);
+           if (UnitIndex == 0) then
+             result = toHexFull(f:read(step));
+           else
+             -- Переводим UnitIndex в байты
+             -- на каждый байт 3 символа в Hex строке
+             f:seek("set", UnitIndex / 3);
+             result = toHexFull(f:read(step));
+           end
+        else
+           f:seek("set", tonumber(offset[FieldIndex - 1]));
+           result = toHex(f:read(UnitIndex + 1));
         end
         f:close();
     end
+    return result;
+end
+
+-- Результат с завершающим пробелом
+-- В итоге 1 байт = 3 символа в строке
+function toHexFull(bytes)
     if (bytes == nil) then
-        return nil; -- invalid
-    end
-    local t = {}
-    for b in string.gfind(bytes, ".") do
+        return "";
+    else
+      local t = {}
+      for b in string.gfind(bytes, ".") do
         table.insert(t, string.format("%02X ", string.byte(b)));
+      end
+      local result = table.concat(t);
+      return result;
     end
-    local result = table.concat(t);
+end
+
+function toHex(bytes)
+    local result = toHexFull(bytes);
     return result:sub(1, - 2);
 end
