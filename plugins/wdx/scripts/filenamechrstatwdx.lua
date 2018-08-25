67c19dcc4b232a4d84a37a24fb0fa2877d6b8a4b
j2969719 <j172933@mvrht.net>
2018-08-25 19:16:57 +0300

    helloworld

--- plugins/wdx/scripts/filenamechrstatwdx.lua
+++ plugins/wdx/scripts/filenamechrstatwdx.lua
@@ -1,141 +1,156 @@
 -- filenamechrstatwdx.lua
 -- luarocks-5.1 install luautf8
--- the search for duplicate file names is a bit unstable
- 
-local utf8 = require("lua-utf8")
 
-local pattern = {
-    {".", "all chars"}, 
+local utf8 = require("lua-utf8")
+local DelimPattern = SysUtils.PathDelim;
+local UnitsStr = "Filename|Filename.Ext|Ext|Path|Path with Filename.Ext";
+
+local pattern = { -- pattern, discription
+    {".",           "all chars"}, 
     {"%w", "alphanumeric chars"}, 
-    {"%s", "spaces"}, 
-    {"%a", "letters"}, 
+    {"%s",             "spaces"}, 
+    {"%a",            "letters"}, 
     {"%l", "lower case letters"}, 
     {"%u", "upper case letters"}, 
-    {"%d", "digits"}, 
-    {"%p", "punctuation chars"}, 
-    {"%."}, 
-    {"%s%-%s"}, 
+    {"%d",             "digits"}, 
+    {"%p",  "punctuation chars"}, 
+    {"%.",                  nil}, 
+    {"%s%-%s",              nil}, 
 }
-local fnames = {}
 
 function ContentGetSupportedField(Index)
-    local submi = '';
-    for i = 1, #pattern do
-        if (i > 1) then
-            submi = submi .. '|';
-        end
-        if (pattern[i][2] ~= nil) then
-            submi = submi .. pattern[i][2];
+    if (Index > -1) and (Index < #pattern) then
+        local fieldname = "Chars Stat (";
+        if (pattern[Index+1][2] ~= nil) then
+            fieldname = fieldname .. pattern[Index+1][2] .. ')';
         else
-            submi = submi .. 'pattern "' .. pattern[i][1].. '"';
+            fieldname = fieldname .. 'pattern "' .. pattern[Index+1][1] .. '")';
         end
-    end
-    if (Index == 0) then
-        return 'Filename', submi, 1;
-    elseif (Index == 1) then
-        return 'Filename.Ext', submi, 1;
-    elseif (Index == 2) then
-        return 'Ext', submi, 1;
-    elseif (Index == 3) then
-        return 'Path', submi, 1;
-    elseif (Index == 4) then
-        return 'Path with Filename.Ext', submi, 1;
-    elseif (Index == 5) then
-        return 'lower case', 'Filename|Filename.Ext|Ext', 6;
-    elseif (Index == 6) then
-        return 'upper case', 'Filename|Filename.Ext|Ext', 6;
-    elseif (Index == 7) then
-        return '(case) duplicate filename', '', 6;
+        return fieldname, UnitsStr, 1;
+    elseif (Index == #pattern) then
+        return "Lower Case Check", UnitsStr, 6;
+    elseif (Index == #pattern+1) then
+        return "Upper Case Check", UnitsStr, 6;
+    elseif (Index == #pattern+2) then
+        return "Case Duplicates Check", '', 6;
     end
     return '', '', 0; -- ft_nomorefields
 end
 
-function ContentGetDefaultSortOrder(FieldIndex)
-    return 1; --or -1
-end
-
-function ContentGetDetectString()
-    return nil;
-end
-
 function ContentGetValue(FileName, FieldIndex, UnitIndex, flags)
-    local target;
-    local tname = FileName;
-    if (utf8.find(FileName, "[/\\]%.%.$")) then
-        tname = utf8.sub(FileName, 1, - 3);
+    if (DelimPattern == nil) then
+        DelimPattern = "/\\";
     end
-    if (FieldIndex  == 0) then
-        target = getFilename(tname)
-    elseif (FieldIndex == 1) then
-        target = getFullname(tname)
-    elseif (FieldIndex == 2) then
-        target = getExt(tname)
-    elseif (FieldIndex == 3) then
-        target = utf8.match(tname, "(.*[/\\])");
-    elseif (FieldIndex == 4) then
-        target = tname; 
-    elseif (FieldIndex == 5) then 
-        if (UnitIndex == 0) and (getFilename(tname) == utf8.lower(getFilename(tname))) then
-            return  true;
-        elseif (UnitIndex == 1) and (getFullname(tname) == utf8.lower(getFullname(tname))) then
-            return  true; 
-        elseif (UnitIndex == 2) and (getExt(tname) == utf8.lower(getExt(tname))) then
-            return  true; 
-        end 
-        return false; 
-    elseif (FieldIndex == 6) then 
-        if (UnitIndex == 0) and (getFilename(tname) == utf8.upper(getFilename(tname))) then
-            return  true;
-        elseif (UnitIndex == 1) and (getFullname(tname) == utf8.upper(getFullname(tname))) then
-            return  true; 
-        elseif (UnitIndex == 2) and (getExt(tname) == utf8.upper(getExt(tname))) then
-            return  true; 
-        end 
-        return false;           
-    elseif (FieldIndex == 7) then         
-        local fpath = utf8.match(tname, "(.*[/\\])");
-        local fname = getFullname(tname);
-        if (#fnames ~= 0) then
-            for i = 1, #fnames do
-                local tpath = utf8.match(fnames[i], "(.*[/\\])");
-                local tfname = getFullname(fnames[i]);
-                if (fpath == tpath) and (fname ~= tfname) and (utf8.lower(fname) == utf8.lower(tfname)) then
-                    return true;
-                end
-            end
-            fnames[#fnames + 1] = tname;
-            return false;
-        else
-            fnames[1] = tname;
-            return false;        
-        end
+    local targetpath = FileName;
+    if (utf8.find(FileName, "[" .. DelimPattern .. "]%.%.$")) then
+        targetpath = utf8.sub(FileName, 1, -3);
     end
-    if (target ~= nil) and (pattern[UnitIndex + 1][1] ~= nil) then
-        local count = 0;
-        for char in utf8.gmatch(target, pattern[UnitIndex + 1][1]) do 
-            count = count + 1; 
-        end
-        return count;
-    end 
+    local isDir = SysUtils.DirectoryExists(targetpath)
+    local target = getTargetStr(targetpath, isDir, UnitIndex);
+    if (FieldIndex > -1) and (FieldIndex < #pattern) then
+        return calcChr(target, pattern[FieldIndex+1][1]);
+    elseif (FieldIndex == #pattern) then
+        return checkCase(target, false);
+    elseif (FieldIndex == #pattern+1) then
+        return checkCase(target, true);
+    elseif (FieldIndex == #pattern+2) then
+        return checkCaseDupl(targetpath);
+    end
     return nil; -- invalid
 end
 
-function getFilename(str)
-    local target = utf8.match(str, "^.*[/\\](.+[^/\\])$");
-    if (target ~= nil) then
-        if (utf8.match(target, "(.+)%..+")) then
-            target = utf8.match(target, "(.+)%..+");
-        end
-    else
-        target = nil;
+function getPath(Str)
+    if (Str == nil) then
+        return nil;
     end
-    return target;
+    return utf8.match(Str, "(.*[" .. DelimPattern .. "])");
 end
 
-function getFullname(str)
-    return utf8.match(str, "^.*[/\\](.+[^/\\])$");
+function getFullname(Str)
+    if (Str == nil) then
+        return nil;
+    end
+    return utf8.match(Str, "[" .. DelimPattern .. "]([^" .. DelimPattern .. "]+)$");
 end
 
-function getExt(str)
-    return utf8.match(str, ".+%.(.+)$");
+function getFilename(Str, IsDir)
+    local FileName = nil;
+    local FullName = getFullname(Str);
+    if (FullName ~= nil) and (IsDir == false) then
+        FileName = utf8.match(FullName, "(.+)%..+");
+    end
+    if (FileName ~= nil) then
+        return FileName;
+    else
+        return FullName;
+    end
+end
+
+function getExt(Str, IsDir)
+    if (Str == nil) or (IsDir == true) then
+        return nil;
+    end
+    if (getFilename(Str, IsDir) == getFullname(Str)) then
+        return nil;
+    end
+    return utf8.match(Str, ".+%.(.+)$");
+end
+
+function getTargetStr(FullPath, IsDir, UnitIndex)
+    if (UnitIndex == 0) then
+        return getFilename(FullPath, IsDir);
+    elseif (UnitIndex == 1) then
+        return getFullname(FullPath);
+    elseif (UnitIndex == 2) then
+        return getExt(FullPath, IsDir);
+    elseif (UnitIndex == 3) then
+        return utf8.match(FullPath, "(.*[" .. DelimPattern .. "])");
+    elseif (UnitIndex == 4) then
+        return FullPath;
+    end
+    return nil;
+end
+
+function calcChr(Target, Pattern)
+    if (Target ~= nil) and (Pattern ~= nil) then
+        local Count = 0;
+        for Chr in utf8.gmatch(Target, Pattern) do
+            Count = Count + 1;
+        end
+        return Count;
+    end 
+end
+
+function checkCase(Target, UpperCase)
+    if (Target == nil) then
+        return nil;
+    end
+    if (UpperCase == true) then
+        if (Target == utf8.upper(Target)) then
+            return true;
+        end
+    elseif (UpperCase == false) then
+        if (Target == utf8.lower(Target)) then
+            return true;
+        end
+    end
+    return false;
+end
+
+function checkCaseDupl(Str)
+    local IsFound = false;
+    local CurrentPath = getPath(Str);
+    local CurrentName = getFullname(Str);
+    local Handle, FindData = SysUtils.FindFirst(CurrentPath .. "*");
+    if (Handle ~= nil) then
+        repeat
+            if (FindData.Name ~= CurrentName) and (utf8.lower(CurrentName) == utf8.lower(FindData.Name)) then
+                IsFound = true;
+                break;
+            end
+                Result, FindData = SysUtils.FindNext(Handle)
+        until (Result == nil)
+        SysUtils.FindClose(Handle)
+    end
+    return IsFound;
 end
