f59c4e1f26d31a763966f4569ce5393c60e5eab4
j2969719 <j172933@mvrht.net>
2018-09-28 01:01:51 +0300

    helloworld

--- plugins/wdx/scripts/gitinfowdx.lua
+++ plugins/wdx/scripts/gitinfowdx.lua
@@ -1,42 +1,40 @@
 -- gitinfowdx.lua
 
-local fields = {  -- field name, field type, command
-    {"Subject",         8,                                 "git log -1 --pretty=format:%s"},
-    {"Body",            8,                                 "git log -1 --pretty=format:%b"},
-    {"Commit hash",     8,                                 "git log -1 --pretty=format:%H"},
-    {"Committer name",  8,                                "git log -1 --pretty=format:%cn"},
-    {"Committer email", 8,                                "git log -1 --pretty=format:%ce"},
-    {"Сommitter date",  8, "git log -1 --pretty=format:%cd --date=format:'%d.%m.%Y %H:%M'"},
-    {"Author name",     8,                                "git log -1 --pretty=format:%an"},
-    {"Author email",    8,                                "git log -1 --pretty=format:%ae"},
-    {"Author date",     8, "git log -1 --pretty=format:%ad --date=format:'%d.%m.%Y %H:%M'"},
-    {"Branch",          8,                             "git rev-parse --abbrev-ref HEAD #"},
-    {"Origin URL",      8,                                   "git remote get-url origin #"},
-    {"Push URL",        8,                            "git remote get-url origin --push #"},
-    {"Log",             8,                                             "git log --oneline"},
-    {"Untracked",       6,                                               "git ls-files -o"},
-    {"Ignored",         6,                            "git ls-files -i --exclude-standard"},
-    {"Modified",        6,                                               "git ls-files -m"},
-}
+local result_true = "☑️"
+local result_false = "-"
 
-local stripnewline = {  -- strip trailing linebreak from output
-    {"Branch"},  -- field name
-    {"Origin URL"},
-    {"Push URL"},
-    {"Untracked"},
-    {"Ignored"},
-    {"Modified"},
+local fields = {  -- field name, field type, command, strip newline, sort order
+    {"Subject",         8,                                 "git log -1 --pretty=format:%s", false,  1},
+    {"Body",            8,                                 "git log -1 --pretty=format:%b", false,  1},
+    {"Commit hash",     8,                                 "git log -1 --pretty=format:%H", false,  1},
+    {"Committer name",  8,                                "git log -1 --pretty=format:%cn", false,  1},
+    {"Committer email", 8,                                "git log -1 --pretty=format:%ce", false,  1},
+    {"Сommitter date",  8, "git log -1 --pretty=format:%cd --date=format:'%d.%m.%Y %H:%M'", false,  1},
+    {"Author name",     8,                                "git log -1 --pretty=format:%an", false,  1},
+    {"Author email",    8,                                "git log -1 --pretty=format:%ae", false,  1},
+    {"Author date",     8, "git log -1 --pretty=format:%ad --date=format:'%d.%m.%Y %H:%M'", false,  1},
+    {"Branch",          8,                             "git rev-parse --abbrev-ref HEAD #", true,   1},
+    {"Revision",        2,                                   "git rev-list --count HEAD #", true,   1},
+    {"Origin URL",      8,                                   "git remote get-url origin #", true,   1},
+    {"Push URL",        8,                            "git remote get-url origin --push #", true,   1},
+    {"Log",             8,                                             "git log --oneline", false,  1},
+    {"Untracked",       6,                                               "git ls-files -o", false,  1},
+    {"Ignored",         6,                            "git ls-files -i --exclude-standard", true,  -1},
+    {"Modified",        6,                                               "git ls-files -m", true,  -1},
+    {"Untracked (alt)", 7,                                               "git ls-files -o", true,  -1},
+    {"Ignored (alt)",   7,                            "git ls-files -i --exclude-standard", true,  -1},
+    {"Modified (alt)",  7,                                               "git ls-files -m", true,  -1},
 }
 
 function ContentGetSupportedField(FieldIndex)
     if (fields[FieldIndex + 1] ~= nil ) then
-        return fields[FieldIndex + 1][1], "", fields[FieldIndex + 1][2];
+        return fields[FieldIndex + 1][1], '', fields[FieldIndex + 1][2];
     end
     return '', '', 0; -- ft_nomorefields
 end
 
 function ContentGetDefaultSortOrder(FieldIndex)
-    return 1; --or -1
+    return fields[FieldIndex + 1][5];
 end
 
 function ContentGetDetectString()
@@ -61,18 +59,23 @@ function ContentGetValue(FileName, FieldIndex, UnitIndex, flags)
         local result = handle:read("*a");
         handle:close();
         if (result ~= nil) then
-            for i = 1, #stripnewline do
-                if (fields[FieldIndex + 1][1] == stripnewline[i][1]) then
+            if (fields[FieldIndex + 1][4] == true) then
                     result = result:sub(1, -2);
-                    break;
-                end
             end
             if (fields[FieldIndex + 1][2] == 6) then
-                if (result ~= "") then
+                if (result ~= '') then
                     return true;
                 else
                     return false;
                 end
+            elseif (fields[FieldIndex + 1][2] == 7) then
+                if (result ~= '') then
+                    return result_true;
+                else
+                    return result_false;
+                end
+            elseif (fields[FieldIndex + 1][2] == 2) then
+                return tonumber(result);
             else
                 return result;
             end
