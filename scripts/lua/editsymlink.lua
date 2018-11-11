48841a228b160c1923b3921d704445fa33f5f6ad
j2969719 <j172933@mvrht.net>
2018-11-11 11:05:33 +0300

    helloworld

--- scripts/lua/editsymlink.lua
+++ scripts/lua/editsymlink.lua
@@ -18,29 +18,32 @@ local args = {...};
 local path, ret, newpath, msg;
 local params = "-n";
 
-local function getOutput(command)
+local function getOutput(command, stripnewline)
     local handle = io.popen(command, 'r');
     local output = handle:read("*a");
     handle:close();
+    if (stripnewline == true) then
+        return output:sub(1, -2);
+    end
     return output;
 end
 
 if (args[1] ~= nil) then
-    path = getOutput('readlink ' .. params .. ' "' .. args[1] .. '"');
+    path = getOutput('readlink ' .. params .. ' "' .. args[1] .. '"', false);
 end
 
 if (path ~= nil) and (path ~= "") then
-   ret, newpath = Dialogs.InputQuery("Edit Symlink", "Enter new path:", false, path);
-
-   if (ret == true) then
-       if (SysUtils.DirectoryExists(newpath)) then
-           msg = getOutput('ln -sfn "' .. newpath .. '" "' .. args[1] .. '" 2>&1');
-       else
-           msg = getOutput('ln -sf "' .. newpath .. '" "' .. args[1] .. '" 2>&1');
-       end
-       
-       if (msg ~= nil) and (msg ~= '') then
-           Dialogs.MessageBox(msg, "Edit Symlink", 0x0030)
-       end
-   end
+    ret, newpath = Dialogs.InputQuery("Edit Symlink", "Enter new path:", false, path);
+    
+    if (ret == true) then
+        if (SysUtils.DirectoryExists(newpath)) then
+            msg = getOutput('ln -sfn "' .. newpath .. '" "' .. args[1] .. '" 2>&1', true);
+        else
+            msg = getOutput('ln -sf "' .. newpath .. '" "' .. args[1] .. '" 2>&1', true);
+        end
+        
+        if (msg ~= nil) and (msg ~= '') then
+            Dialogs.MessageBox(msg, "Edit Symlink", 0x0030)
+        end
+    end
 end
