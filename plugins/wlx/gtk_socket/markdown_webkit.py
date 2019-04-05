48b804ff2435ee4d21db25af5a926e73de240ffe
j2969719 <j172933@mvrht.net>
2019-04-05 21:30:22 +0300

    helloworld

---
+++ plugins/wlx/gtk_socket/markdown_webkit.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import os
+import sys
+import markdown
+os.environ["GDK_CORE_DEVICE_EVENTS"] = "1"
+
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('WebKit2', '4.0')
+from gi.repository import Gtk, GLib
+from gi.repository import WebKit2
+
+wid = int(sys.argv[1])
+path = sys.argv[2]
+
+plug = Gtk.Plug()
+plug.construct(wid)
+view = WebKit2.WebView()
+plug.add(view)
+
+mdfile = open(path)
+html = markdown.markdown(mdfile.read())
+mdfile.close()
+
+view.load_html(html)
+
+plug.connect("delete-event", Gtk.main_quit)
+plug.show_all()
+Gtk.main()
---
+++ plugins/wlx/gtk_socket/webkit2gtk.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+import os
+import sys
+os.environ["GDK_CORE_DEVICE_EVENTS"] = "1"
+
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('WebKit2', '4.0')
+from gi.repository import Gtk, GLib
+from gi.repository import WebKit2
+
+wid = int(sys.argv[1])
+uri = GLib.filename_to_uri(sys.argv[2])
+plug = Gtk.Plug()
+plug.construct(wid)
+view = WebKit2.WebView()
+plug.add(view)
+
+settings = view.get_settings()
+settings.set_allow_file_access_from_file_urls(True)
+settings.set_allow_universal_access_from_file_urls(True)
+view.set_settings(settings)
+
+view.load_uri(uri)
+
+plug.connect("delete-event", Gtk.main_quit)
+plug.show_all()
+Gtk.main()
