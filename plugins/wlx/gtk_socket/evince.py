273ed3efea5bf73f431f560ddb1518789a68dd50
j2969719 <j172933@mvrht.net>
2019-04-24 23:05:59 +0300

    helloworld

---
+++ plugins/wlx/gtk_socket/evince.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import os
+import sys
+os.environ["GDK_CORE_DEVICE_EVENTS"] = "1"
+
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('EvinceDocument', '3.0')
+gi.require_version('EvinceView', '3.0')
+from gi.repository import Gtk, GLib
+from gi.repository import EvinceDocument
+from gi.repository import EvinceView
+
+wid = int(sys.argv[1])
+uri = GLib.filename_to_uri(sys.argv[2])
+plug = Gtk.Plug()
+plug.construct(wid)
+scroll = Gtk.ScrolledWindow()
+EvinceDocument.init()
+doc = EvinceDocument.Document.factory_get_document(uri)
+view = EvinceView.View()
+model = EvinceView.DocumentModel()
+model.set_document(doc)
+view.set_model(model)
+scroll.add(view)
+plug.add(scroll)
+
+plug.connect("delete-event", Gtk.main_quit)
+plug.show_all()
+Gtk.main()
---
+++ plugins/wlx/gtk_socket/gtksourceview.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python
+
+import os
+import sys
+os.environ["GDK_CORE_DEVICE_EVENTS"] = "1"
+
+import gi
+gi.require_version('Gtk', '3.0')
+gi.require_version('GtkSource', '4')
+from gi.repository import Gtk
+from gi.repository import GtkSource
+
+import locale
+locale.setlocale(locale.LC_ALL, "")
+ENCODING = locale.getpreferredencoding() or "utf_8"
+
+wid = int(sys.argv[1])
+path = sys.argv[2]
+f = open(path, "r", encoding=ENCODING, errors="replace")
+txt = f.read()
+f.close()
+
+plug = Gtk.Plug()
+plug.construct(wid)
+
+lang_manager = GtkSource.LanguageManager()
+lang = lang_manager.guess_language(path)
+style_manager = GtkSource.StyleSchemeManager.get_default()
+style = style_manager.get_scheme("builder")
+
+srcbuf = GtkSource.Buffer()
+srcbuf.set_style_scheme(style)
+srcbuf.set_language(lang)
+srcbuf.set_text(txt)
+
+scroll = Gtk.ScrolledWindow()
+view = GtkSource.View.new_with_buffer(srcbuf)
+
+view.set_show_line_numbers(True)
+view.set_highlight_current_line(True)
+view.get_space_drawer().set_enable_matrix(True)
+view.set_editable(False)
+
+scroll.add(view)
+plug.add(scroll)
+
+plug.connect("delete-event", Gtk.main_quit)
+plug.show_all()
+Gtk.main()
