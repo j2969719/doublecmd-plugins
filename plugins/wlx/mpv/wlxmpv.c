cfe1e116ab4b8ddc5d458f549d106bb9f5aaf935
j2969719 <j172933@mvrht.net>
2019-06-15 23:53:24 +0300

    helloworld

--- plugins/wlx/mpv/wlxmpv.c
+++ plugins/wlx/mpv/wlxmpv.c
@@ -168,7 +168,7 @@ HWND DCPCALL ListLoad(HWND ParentWin, char* FileToLoad, int ShowFlags)
 
 void DCPCALL ListCloseWindow(HWND ListWin)
 {
-	kill(GPOINTER_TO_INT(g_object_get_data(G_OBJECT(ListWin), "pid")), SIGTERM);
+	kill(GPOINTER_TO_INT(g_object_get_data(G_OBJECT(ListWin), "pid")), SIGINT);
 	gtk_widget_destroy(GTK_WIDGET(ListWin));
 }
 
