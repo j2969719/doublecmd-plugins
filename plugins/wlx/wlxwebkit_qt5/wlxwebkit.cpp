ef67c97002e42751a38af74327fefe54ef424fe6
j2969719 <j172933@mvrht.net>
2019-08-22 03:22:35 +0300

    helloworld

---
+++ plugins/wlx/wlxwebkit_qt5/wlxwebkit.cpp
@@ -0,0 +1,66 @@
+#include <QUrl>
+#include <QWebView>
+#include "wlxplugin.h"
+
+#define _detectstring "(EXT=\"HTML\")|(EXT=\"HTM\")|(EXT=\"XHTM\")|(EXT=\"XHTML\")"
+
+HANDLE DCPCALL ListLoad(HANDLE ParentWin, char* FileToLoad, int ShowFlags)
+{
+	QWebView *webView = new QWebView((QWidget*)ParentWin);
+	webView->load(QUrl::fromLocalFile(FileToLoad));
+	webView->show();
+	return webView;
+}
+
+void DCPCALL ListCloseWindow(HANDLE ListWin)
+{
+	delete (QWebView*)ListWin;
+}
+
+int DCPCALL ListLoadNext(HWND ParentWin, HWND PluginWin, char* FileToLoad, int ShowFlags)
+{
+	QWebView *webView = (QWebView *)PluginWin;
+	webView->load(QUrl::fromLocalFile(FileToLoad));
+	return LISTPLUGIN_OK;
+}
+
+void DCPCALL ListGetDetectString(char* DetectString, int maxlen)
+{
+	strncpy(DetectString, _detectstring, maxlen - 1);
+}
+
+int DCPCALL ListSearchText(HWND ListWin, char* SearchString, int SearchParameter)
+{
+	QWebPage::FindFlags sflags;
+
+	if (SearchParameter & lcs_matchcase)
+		sflags |= QWebPage::FindCaseSensitively;
+
+	if (SearchParameter & lcs_backwards)
+		sflags |= QWebPage::FindBackward;
+
+	QWebView *webView = (QWebView *)ListWin;
+	webView->findText(SearchString, sflags);
+	return LISTPLUGIN_OK;
+}
+
+int DCPCALL ListSendCommand(HWND ListWin, int Command, int Parameter)
+{
+	QWebView *webView = (QWebView *)ListWin;
+
+	switch (Command)
+	{
+	case lc_copy :
+		webView->triggerPageAction(QWebPage::Copy);
+		break;
+
+	case lc_selectall :
+		webView->triggerPageAction(QWebPage::SelectAll);
+		break;
+
+	default :
+		return LISTPLUGIN_ERROR;
+	}
+
+	return LISTPLUGIN_OK;
+}
