a9fa455d1b90f1c15da70e40a1cce7dfb9d150a4
j2969719 <j172933@mvrht.net>
2019-09-01 16:44:06 +0300

    helloworld

--- plugins/dsx/duplicates_crap/plugin.c
+++ plugins/dsx/duplicates_crap/plugin.c
@@ -1,15 +1,15 @@
-#define _GNU_SOURCE
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <linux/limits.h>
-#include <string.h>
+#include <glib.h>
+#include <signal.h>
 #include "dsxplugin.h"
+
+#define cmdf "/bin/sh -c \"find '%s' -type f -name '%s' -exec sha1sum '{}' ';' | sort | uniq --all-repeated=separate -w 40 | cut -c 43-\""
+
 
 tSAddFileProc gAddFileProc;
 tSUpdateStatusProc gUpdateStatus;
 
-#define cmdf "find '%s' -type f -name '%s' -exec sha1sum '{}' ';' | sort | uniq --all-repeated=separate -w 40 | cut -c 43-"
+gboolean stop_search;
+GPid pid;
 
 int DCPCALL Init(tDsxDefaultParamStruct* dsp, tSAddFileProc pAddFileProc, tSUpdateStatusProc pUpdateStatus)
 {
@@ -20,21 +20,41 @@ int DCPCALL Init(tDsxDefaultParamStruct* dsp, tSAddFileProc pAddFileProc, tSUpda
 
 void DCPCALL StartSearch(int PluginNr, tDsxSearchRecord* pSearchRec)
 {
-	FILE *fp;
-	size_t i = 1, len = 0;
-	char *str, *line = NULL;
+	gchar **argv;
+	gchar *line, *command;
+	gsize len, term, i = 1;
 
-	asprintf(&str, cmdf, pSearchRec->StartPath, pSearchRec->FileMask);
-	gUpdateStatus(PluginNr, str, 0);
+	gint fp;
+	GSpawnFlags flags;
+	GError *err = NULL;
 
-	if ((fp = popen(str, "r")) == NULL)
+	stop_search = FALSE;
+	command = g_strdup_printf(cmdf, pSearchRec->StartPath, pSearchRec->FileMask);
+	gUpdateStatus(PluginNr, command, 0);
+
+	if (!g_shell_parse_argv(command, NULL, &argv, &err))
+	{
+		gUpdateStatus(PluginNr, g_strdup(err->message), 0);
 		gAddFileProc(PluginNr, "");
+	}
 
-	while(getline(&line, &len, fp) != -1)
-		if (line && line != "")
+	g_clear_error(&err);
+
+	if (!g_spawn_async_with_pipes(NULL, argv, NULL, 0, NULL, NULL, &pid, NULL, &fp, NULL, &err))
+	{
+		gUpdateStatus(PluginNr, g_strdup(err->message), 0);
+		gAddFileProc(PluginNr, "");
+	}
+	else
+	{
+
+		GIOChannel *stdout = g_io_channel_unix_new(fp);
+
+		while (!stop_search && (G_IO_STATUS_NORMAL == g_io_channel_read_line(stdout, &line, &len, &term, NULL)))
 		{
-			line[strlen(line)-1] = '\0';
-			if (strcmp(line, "") == 0)
+			line[term] = '\0';
+
+			if (line[0] == '\0')
 				gAddFileProc(PluginNr, "-----------------------------------------");
 			else
 			{
@@ -43,14 +63,21 @@ void DCPCALL StartSearch(int PluginNr, tDsxSearchRecord* pSearchRec)
 			}
 		}
 
-	pclose(fp);
-	free(str);
+		kill(pid, SIGTERM);
+		g_spawn_close_pid(pid);
+		g_io_channel_shutdown(stdout, TRUE, NULL);
+		g_io_channel_unref(stdout);
+	}
+
+	if (err)
+		g_error_free(err);
+
 	gAddFileProc(PluginNr, "");
 }
 
 void DCPCALL StopSearch(int PluginNr)
 {
-
+	stop_search = TRUE;
 }
 
 void DCPCALL Finalize(int PluginNr)
