32d970757b59b34d818d78d576d9a72ed3e3aff1
j2969719 <j172933@mvrht.net>
2019-10-04 13:47:06 +0300

    helloworld

--- plugins/wlx/zathura/wlxzathura.c
+++ plugins/wlx/zathura/wlxzathura.c
@@ -7,6 +7,9 @@
 
 #define _detectstring  "(EXT=\"PDF\")|(EXT=\"DJVU\")|(EXT=\"DJV\")|(EXT=\"PS\")|(EXT=\"CBR\")|(EXT=\"CBZ\")"
 
+static char cfg_path[PATH_MAX];
+static char plug_path[PATH_MAX];
+const char* cfg_file = "settings.ini";
 
 static void plug_added(GtkWidget *widget, gpointer data)
 {
@@ -15,61 +18,58 @@ static void plug_added(GtkWidget *widget, gpointer data)
 	gtk_widget_show(widget);
 }
 
-HWND DCPCALL ListLoad(HWND ParentWin, char* FileToLoad, int ShowFlags)
+static gchar* get_command_string(GdkNativeWindow id, char* FileToLoad)
 {
-	Dl_info dlinfo;
-	static char plg_path[PATH_MAX], cfg_path[PATH_MAX];
-	GtkWidget *gFix;
-	GtkWidget *wspin;
-	GtkWidget *zathura;
 	GKeyFile *cfg;
 	GError *err = NULL;
-	gchar *param, *command;
-	gboolean bval;
-
-	memset(&dlinfo, 0, sizeof(dlinfo));
-
-	if (dladdr(cfg_path, &dlinfo) != 0)
-	{
-		g_strlcpy(cfg_path, dlinfo.dli_fname, PATH_MAX);
-		char *pos = strrchr(dlinfo.dli_fname, '/');
-
-		if (pos)
-			g_strlcpy(plg_path, dlinfo.dli_fname, pos - dlinfo.dli_fname + 1);
-
-		pos = strrchr(cfg_path, '/');
-
-		if (pos)
-			strcpy(pos + 1, "settings.ini");
-	}
+	gchar *params, *command;
+	gboolean extcfg;
 
 	cfg = g_key_file_new();
 
 	if (!g_key_file_load_from_file(cfg, cfg_path, G_KEY_FILE_KEEP_COMMENTS, &err))
 	{
 		g_print("zathura.wlx (%s): %s\n", cfg_path, (err)->message);
-		param = " ";
-		bval = FALSE;
+		params = " ";
+		extcfg = FALSE;
 	}
 	else
 	{
-		param = g_key_file_get_string(cfg, "Settings", "Params", NULL);
+		g_clear_error(&err);
 
-		if (!param)
-			param = " ";
+		params = g_key_file_get_string(cfg, "Settings", "Params", NULL);
 
-		bval = g_key_file_get_boolean(cfg, "Settings", "ExternalCfg", &err);
+		if (!params)
+			params = " ";
 
-		if (!bval && !err)
-			bval = FALSE;
+		extcfg = g_key_file_get_boolean(cfg, "Settings", "ExternalCfg", &err);
+
+		if (!extcfg || err)
+			extcfg = FALSE;
 		else
-			bval = TRUE;
+			extcfg = TRUE;
 	}
 
 	g_key_file_free(cfg);
 
 	if (err)
 		g_error_free(err);
+
+	if (extcfg)
+		command = g_strdup_printf("zathura --reparent=%d %s %s --config-dir=%s --data-dir=%s",
+		                          id, g_shell_quote(FileToLoad), params, g_shell_quote(plug_path), g_shell_quote(plug_path));
+	else
+		command = g_strdup_printf("zathura --reparent=%d %s %s", id, g_shell_quote(FileToLoad), params);
+
+	return command;
+}
+
+HWND DCPCALL ListLoad(HWND ParentWin, char* FileToLoad, int ShowFlags)
+{
+	GtkWidget *gFix;
+	GtkWidget *wspin;
+	GtkWidget *zathura;
+	gchar **argv, **envp;
 
 	gFix = gtk_vbox_new(FALSE, 5);
 	gtk_container_add(GTK_CONTAINER(GTK_WIDGET(ParentWin)), gFix);
@@ -82,19 +82,32 @@ HWND DCPCALL ListLoad(HWND ParentWin, char* FileToLoad, int ShowFlags)
 	gtk_container_add(GTK_CONTAINER(gFix), zathura);
 	GdkNativeWindow id = gtk_socket_get_id(GTK_SOCKET(zathura));
 
-	if (bval)
-		command = g_strdup_printf("zathura --reparent=%d \"%s\" %s --config-dir=\"%s\" --data-dir=\"%s\"", id, FileToLoad, param, plg_path, plg_path);
-	else
-		command = g_strdup_printf("zathura --reparent=%d \"%s\" %s", id, FileToLoad, param);
+	g_shell_parse_argv(get_command_string(id, FileToLoad), NULL, &argv, NULL);
 
-	if (!g_spawn_command_line_async(command, NULL))
+	if (!argv)
 	{
-		g_free(command);
 		gtk_widget_destroy(gFix);
 		return NULL;
 	}
 
-	g_free(command);
+	envp = g_environ_setenv(g_get_environ(), "GDK_CORE_DEVICE_EVENTS", "1", TRUE);
+
+	if (!g_spawn_async(NULL, argv, envp, G_SPAWN_SEARCH_PATH, NULL, NULL, NULL, NULL))
+	{
+		g_strfreev(argv);
+
+		if (envp)
+			g_strfreev(envp);
+
+		gtk_widget_destroy(gFix);
+		return NULL;
+	}
+
+	g_strfreev(argv);
+
+	if (envp)
+		g_strfreev(envp);
+
 	g_signal_connect(zathura, "plug-added", G_CALLBACK(plug_added), (gpointer)wspin);
 
 	if (g_strcmp0(gtk_window_get_title(GTK_WINDOW(gtk_widget_get_toplevel(GTK_WIDGET(ParentWin)))), FileToLoad) != 0)
@@ -112,7 +125,7 @@ void DCPCALL ListCloseWindow(HWND ListWin)
 
 void DCPCALL ListGetDetectString(char* DetectString, int maxlen)
 {
-	g_strlcpy(DetectString, _detectstring, maxlen-1);
+	g_strlcpy(DetectString, _detectstring, maxlen - 1);
 }
 
 int DCPCALL ListSearchDialog(HWND ListWin, int FindNext)
@@ -129,4 +142,21 @@ int DCPCALL ListSendCommand(HWND ListWin, int Command, int Parameter)
 		return LISTPLUGIN_ERROR;
 
 	return LISTPLUGIN_OK;
+}
+
+void DCPCALL ListSetDefaultParams(ListDefaultParamStruct* dps)
+{
+	Dl_info dlinfo;
+
+	memset(&dlinfo, 0, sizeof(dlinfo));
+
+	if (dladdr(cfg_path, &dlinfo) != 0)
+	{
+		strncpy(cfg_path, dlinfo.dli_fname, PATH_MAX);
+		strncpy(plug_path, g_path_get_dirname(cfg_path), PATH_MAX);
+		char *pos = strrchr(cfg_path, '/');
+
+		if (pos)
+			strcpy(pos + 1, cfg_file);
+	}
 }
