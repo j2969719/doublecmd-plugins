806956f064ddce247c5a4e8b993e7a1cf75626b7
j2969719 <j172933@mvrht.net>
2019-10-06 02:25:12 +0300

    helloworld

---
+++ plugins/wlx/syntax-highlighting_qt/plugin.cpp
@@ -0,0 +1,119 @@
+#include <QFile>
+#include <QDebug>
+#include <QVariant>
+#include <QPlainTextEdit>
+
+#include <SyntaxHighlighter>
+#include <Repository>
+#include <Definition>
+#include <Theme>
+
+#include "wlxplugin.h"
+
+HANDLE DCPCALL ListLoad(HANDLE ParentWin, char* FileToLoad, int ShowFlags)
+{
+	KSyntaxHighlighting::Repository *repo = new KSyntaxHighlighting::Repository();
+	KSyntaxHighlighting::Definition definition = repo->definitionForFileName(FileToLoad);
+
+	if (!definition.isValid())
+		return NULL;
+
+	QFile file(FileToLoad);
+
+	if (!file.open(QFile::ReadOnly | QFile::Text))
+		return NULL;
+
+	QPlainTextEdit *view = new QPlainTextEdit((QWidget*)ParentWin);
+	view->setPlainText(file.readAll());
+	file.close();
+	view->setReadOnly(true);
+
+
+	KSyntaxHighlighting::SyntaxHighlighter *highlighter = new KSyntaxHighlighting::SyntaxHighlighter();
+	highlighter->setDefinition(definition);
+	highlighter->setTheme(repo->defaultTheme(KSyntaxHighlighting::Repository::DarkTheme));
+	//highlighter->setTheme(repo->defaultTheme(KSyntaxHighlighting::Repository::LightTheme));
+	highlighter->setDocument(view->document());
+	view->show();
+
+	return view;
+}
+/*
+int DCPCALL ListLoadNext(HWND ParentWin, HWND PluginWin, char* FileToLoad, int ShowFlags)
+{
+        KSyntaxHighlighting::Repository *repo = new KSyntaxHighlighting::Repository();
+	KSyntaxHighlighting::Definition definition = repo->definitionForFileName(FileToLoad);
+
+	if (!definition.isValid())
+              return LISTPLUGIN_ERROR;
+
+	QFile file(FileToLoad);
+
+	if (!file.open(QFile::ReadOnly | QFile::Text))
+		return LISTPLUGIN_ERROR;
+
+	QPlainTextEdit *view = (QPlainTextEdit*)PluginWin;
+
+	view->setPlainText(file.readAll());
+	file.close();
+
+	KSyntaxHighlighting::SyntaxHighlighter *highlighter = new KSyntaxHighlighting::SyntaxHighlighter();
+	highlighter->setDefinition(definition);
+	highlighter->setTheme(repo->defaultTheme(KSyntaxHighlighting::Repository::DarkTheme));
+	//highlighter->setTheme(repo->defaultTheme(KSyntaxHighlighting::Repository::LightTheme));
+	highlighter->setDocument(view->document());
+
+	return LISTPLUGIN_OK;
+}
+*/
+void DCPCALL ListCloseWindow(HANDLE ListWin)
+{
+	delete (QPlainTextEdit*)ListWin;
+}
+
+int DCPCALL ListSearchText(HWND ListWin, char* SearchString, int SearchParameter)
+{
+	QTextDocument::FindFlags sflags;
+
+	if (SearchParameter & lcs_matchcase)
+		sflags |= QTextDocument::FindBackward;
+
+	if (SearchParameter & lcs_backwards)
+		sflags |= QTextDocument::FindBackward;
+
+	QPlainTextEdit *view = (QPlainTextEdit*)ListWin;
+
+	if (view->find(SearchString, sflags))
+		return LISTPLUGIN_OK;
+	else
+		return LISTPLUGIN_ERROR;
+}
+
+int DCPCALL ListSendCommand(HWND ListWin, int Command, int Parameter)
+{
+	QPlainTextEdit *view = (QPlainTextEdit*)ListWin;
+
+	switch (Command)
+	{
+	case lc_copy :
+		view->copy();
+		break;
+
+	case lc_selectall :
+		view->selectAll();
+		break;
+
+	case lc_newparams :
+		if (Parameter & lcp_wraptext)
+			view->setLineWrapMode(QPlainTextEdit::WidgetWidth);
+		else
+			view->setLineWrapMode(QPlainTextEdit::NoWrap);
+
+		break;
+
+	default :
+		return LISTPLUGIN_ERROR;
+	}
+
+	return LISTPLUGIN_OK;
+}
