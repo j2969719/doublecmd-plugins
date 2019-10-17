2fd1a5411c2c930982f3586367645aec3a88b3b8
j2969719 <j172933@mvrht.net>
2019-10-17 13:45:33 +0300

    helloworld

---
+++ plugins/wfx/avfs_crap/plugin.c
@@ -0,0 +1,342 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <linux/limits.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <virtual.h>
+#include <string.h>
+#include <time.h>
+#include "wfxplugin.h"
+#include "extension.h"
+
+#define Int32x32To64(a,b) ((int64_t)(a)*(int64_t)(b))
+
+#define ANSWER_TO_THE_ULTIMATE_QUESTION_OF_LIFE_THE_UNIVERSE_AND_EVERYTHING 42
+
+typedef struct sAVFSDirData
+{
+	DIR *cur;
+	char path[PATH_MAX];
+} tAVFSDirData;
+
+int gPluginNr;
+tProgressProc gProgressProc;
+tLogProc gLogProc;
+tRequestProc gRequestProc;
+tExtensionStartupInfo* gStartupInfo;
+
+//static char gAVFSPath[PATH_MAX] = "/home/user/tst.diff#patchfs";
+//static char gAVFSPath[PATH_MAX] = "/home/user/tst.zip#";
+static char gAVFSPath[PATH_MAX] = "/#ftp:ftp.funet.fi/pub/Linux";
+
+char* strlcpy(char* p, const char* p2, int maxlen)
+{
+	if ((int)strlen(p2) >= maxlen)
+	{
+		strncpy(p, p2, maxlen);
+		p[maxlen] = 0;
+	}
+	else
+		strcpy(p, p2);
+
+	return p;
+}
+
+void UnixTimeToFileTime(time_t t, LPFILETIME pft)
+{
+	int64_t ll = Int32x32To64(t, 10000000) + 116444736000000000;
+	pft->dwLowDateTime = (DWORD)ll;
+	pft->dwHighDateTime = ll >> 32;
+}
+
+bool SetFindData(DIR *cur, char *path, WIN32_FIND_DATAA *FindData)
+{
+	struct dirent *ent;
+	struct stat buf;
+	char lpath[PATH_MAX];
+
+	ent = virt_readdir(cur);
+
+	if (ent != NULL)
+	{
+		memset(FindData, 0, sizeof(WIN32_FIND_DATAA));
+
+/*
+		if (ent->d_type == DT_DIR)
+		{
+			printf("\tSetFindData: ent->d_type == DT_DIR\n");
+			FindData->dwFileAttributes = FILE_ATTRIBUTE_DIRECTORY;
+		}
+*/
+
+		snprintf(lpath, sizeof(lpath), "%s/%s", path, ent->d_name);
+
+		if (virt_stat(lpath, &buf) == 0)
+		{
+			if (S_ISDIR(buf.st_mode))
+				FindData->dwFileAttributes = FILE_ATTRIBUTE_DIRECTORY;
+
+			FindData->nFileSizeHigh = (buf.st_size & 0xFFFFFFFF00000000) >> 32;
+			FindData->nFileSizeLow = buf.st_size & 0x00000000FFFFFFFF;
+
+			UnixTimeToFileTime(buf.st_mtime, &FindData->ftLastWriteTime);
+
+			FindData->dwFileAttributes |= FILE_ATTRIBUTE_UNIX_MODE;
+			FindData->dwReserved0 = buf.st_mode;
+		}
+		else
+			printf("\tSetFindData: virt_stat failed for %s\n", ent->d_name);
+
+		strlcpy(FindData->cFileName, ent->d_name, PATH_MAX - 1);
+
+		printf("\tSetFindData for %s\n", FindData->cFileName);
+
+		return true;
+	}
+
+	printf("\tSetFindData = false\n");
+
+	return false;
+}
+
+int DCPCALL FsInit(int PluginNr, tProgressProc pProgressProc, tLogProc pLogProc, tRequestProc pRequestProc)
+{
+	gPluginNr = PluginNr;
+	gProgressProc = pProgressProc;
+	gLogProc = pLogProc;
+	gRequestProc = pRequestProc;
+	return 0;
+}
+
+HANDLE DCPCALL FsFindFirst(char* Path, WIN32_FIND_DATAA *FindData)
+{
+	tAVFSDirData *dirdata;
+	dirdata = malloc(sizeof(tAVFSDirData));
+	memset(dirdata, 0, sizeof(tAVFSDirData));
+
+	snprintf(dirdata->path, sizeof(dirdata->path), "%s%s", gAVFSPath, Path);
+	dirdata->cur = virt_opendir(dirdata->path);
+
+	printf("===================\nFsFindFirst in %s (%s)\n", dirdata->path, gAVFSPath);
+
+	if (dirdata->cur != NULL && SetFindData(dirdata->cur, dirdata->path, FindData) == true)
+		return (HANDLE)dirdata;
+
+	return (HANDLE)(-1);
+}
+
+BOOL DCPCALL FsFindNext(HANDLE Hdl, WIN32_FIND_DATAA *FindData)
+{
+	tAVFSDirData *dirdata = (tAVFSDirData*)Hdl;
+
+	printf("FsFindNext in %s (%s)\n", dirdata->path, gAVFSPath);
+
+	return SetFindData(dirdata->cur, dirdata->path, FindData);
+}
+
+int DCPCALL FsFindClose(HANDLE Hdl)
+{
+	tAVFSDirData *dirdata = (tAVFSDirData*)Hdl;
+
+	printf("\nFsFindClose in %s (%s)\n===================\n", dirdata->path, gAVFSPath);
+
+	if (dirdata->cur != NULL)
+		virt_closedir(dirdata->cur);
+
+	return 0;
+}
+
+int DCPCALL FsGetFile(char* RemoteName, char* LocalName, int CopyFlags, RemoteInfoStruct* ri)
+{
+	int ifd, ofd, done;
+	ssize_t len, total = 0;
+	char rpath[PATH_MAX];
+	char buff[8192];
+	int result = FS_FILE_OK;
+
+	if ((CopyFlags == 0) && (access(LocalName, F_OK) == 0))
+		return FS_FILE_EXISTS;
+
+	snprintf(rpath, sizeof(rpath), "%s%s", gAVFSPath, RemoteName);
+
+	if (gProgressProc(gPluginNr, rpath, LocalName, 0) == 1)
+		return FS_FILE_USERABORT;
+
+	ifd = virt_open(rpath, O_RDONLY, 0);
+
+	if (ifd == -1)
+		return FS_FILE_READERROR;
+
+	ofd = open(LocalName, O_WRONLY | O_CREAT | O_TRUNC, S_IRWXU);
+
+	if (ofd > -1)
+	{
+		size_t rsize = ((int64_t)ri->SizeHigh << 32) | ri->SizeLow;
+
+		while ((len = virt_read(ifd, buff, sizeof(buff))) > 0)
+		{
+			if (write(ofd, buff, len) == -1)
+				result = FS_FILE_WRITEERROR;
+
+			total += len;
+
+			if (rsize > 0)
+				done = total * 100 / rsize;
+			else
+				done = 0;
+
+			if (done > 100)
+				done = 100;
+
+			if (gProgressProc(gPluginNr, rpath, LocalName, done) == 1)
+			{
+				result = FS_FILE_USERABORT;
+				break;
+			}
+		}
+
+		close(ofd);
+	}
+	else
+		result = FS_FILE_WRITEERROR;
+
+	if (ri->Attr > 0)
+		chmod(LocalName, ri->Attr);
+
+	virt_close(ifd);
+
+	return result;
+}
+
+int DCPCALL FsExecuteFile(HWND MainWin, char* RemoteName, char* Verb)
+{
+	if (strcmp(Verb, "open") == 0)
+		return FS_EXEC_YOURSELF;
+
+	return FS_EXEC_OK;
+}
+
+void DCPCALL FsStatusInfo(char* RemoteDir, int InfoStartEnd, int InfoOperation)
+{
+	char *msg = malloc(sizeof(char) * ANSWER_TO_THE_ULTIMATE_QUESTION_OF_LIFE_THE_UNIVERSE_AND_EVERYTHING);
+	memset(msg, 0, sizeof(msg));
+
+	switch (InfoOperation)
+	{
+	case FS_STATUS_OP_LIST:
+		strcat(msg, "\n>>> FS_STATUS_OP_LIST");
+		break;
+
+	case FS_STATUS_OP_GET_SINGLE:
+		strcat(msg, "\n>>> FS_STATUS_OP_GET_SINGLE");
+		break;
+
+	case FS_STATUS_OP_GET_MULTI:
+		strcat(msg, "\n>>> FS_STATUS_OP_GET_MULTI");
+		break;
+
+	case FS_STATUS_OP_PUT_SINGLE:
+		strcat(msg, "\n>>> FS_STATUS_OP_PUT_SINGLE");
+		break;
+
+	case FS_STATUS_OP_PUT_MULTI:
+		strcat(msg, "\n>>> FS_STATUS_OP_PUT_MULTI");
+		break;
+
+	case FS_STATUS_OP_RENMOV_SINGLE:
+		strcat(msg, "\n>>> FS_STATUS_OP_RENMOV_SINGLE");
+		break;
+
+	case FS_STATUS_OP_RENMOV_MULTI:
+		strcat(msg, "\n>>> FS_STATUS_OP_RENMOV_MULTI");
+		break;
+
+	case FS_STATUS_OP_DELETE:
+		strcat(msg, "\n FS_STATUS_OP_DELETE");
+		break;
+
+	case FS_STATUS_OP_ATTRIB:
+		strcat(msg, "\n>>> FS_STATUS_OP_ATTRIB");
+		break;
+
+	case FS_STATUS_OP_MKDIR:
+		strcat(msg, "\n>>> FS_STATUS_OP_MKDIR");
+		break;
+
+	case FS_STATUS_OP_EXEC:
+		strcat(msg, "\n>>> FS_STATUS_OP_EXEC");
+		break;
+
+	case FS_STATUS_OP_CALCSIZE:
+		strcat(msg, "\n>>> FS_STATUS_OP_CALCSIZE");
+		break;
+
+	case FS_STATUS_OP_SEARCH:
+		strcat(msg, "\n>>> FS_STATUS_OP_SEARCH");
+		break;
+
+	case FS_STATUS_OP_SEARCH_TEXT:
+		strcat(msg, "\n>>> FS_STATUS_OP_SEARCH_TEXT");
+		break;
+
+	case FS_STATUS_OP_SYNC_SEARCH:
+		strcat(msg, "\n>>> FS_STATUS_OP_SYNC_SEARCH");
+		break;
+
+	case FS_STATUS_OP_SYNC_GET:
+		strcat(msg, "\n>>> FS_STATUS_OP_SYNC_GET");
+		break;
+
+	case FS_STATUS_OP_SYNC_PUT:
+		strcat(msg, "\n>>> FS_STATUS_OP_SYNC_PUT");
+		break;
+
+	case FS_STATUS_OP_GET_MULTI_THREAD:
+		strcat(msg, "\n>>> FS_STATUS_OP_GET_MULTI_THREAD");
+		break;
+
+	case FS_STATUS_OP_PUT_MULTI_THREAD:
+		strcat(msg, "\n>>> FS_STATUS_OP_PUT_MULTI_THREAD");
+		break;
+	}
+
+	if (InfoStartEnd == FS_STATUS_START)
+		strcat(msg, " Start\n");
+	else if (InfoStartEnd == FS_STATUS_END)
+		strcat(msg, " End\n");
+
+	printf(msg);
+	free(msg);
+
+	if (strcmp(RemoteDir, "/") == 0)
+	{
+		if (InfoStartEnd == FS_STATUS_START && InfoOperation == FS_STATUS_OP_LIST)
+			gRequestProc(gPluginNr, RT_TargetDir, "AVFS", "Enter AVFS path:", gAVFSPath, sizeof(gAVFSPath) - 1);
+	}
+}
+
+void DCPCALL FsSetDefaultParams(FsDefaultParamStruct* dps)
+{
+
+}
+
+void DCPCALL FsGetDefRootName(char* DefRootName, int maxlen)
+{
+	strlcpy(DefRootName, "avfs-tst", maxlen - 1);
+}
+
+/*
+void DCPCALL ExtensionInitialize(tExtensionStartupInfo* StartupInfo)
+{
+	gStartupInfo = malloc(sizeof(tExtensionStartupInfo));
+	memcpy(gStartupInfo, StartupInfo, sizeof(tExtensionStartupInfo));
+}
+
+void DCPCALL ExtensionFinalize(void* Reserved)
+{
+	free(gStartupInfo);
+}
+*/
