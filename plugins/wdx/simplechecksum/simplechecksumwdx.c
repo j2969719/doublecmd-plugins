1a758c11a10af5abec41dae21aa0bccc962ffbcc
j2969719 <j172933@mvrht.net>
2019-12-04 17:44:31 +0300

    helloworld

--- plugins/wdx/simplechecksum/simplechecksumwdx.c
+++ plugins/wdx/simplechecksum/simplechecksumwdx.c
@@ -1,30 +1,56 @@
 #include <stdio.h>
-#include <openssl/md5.h>
-#include <openssl/sha.h>
-#include <zlib.h>
+#include <gcrypt.h>
 #include <linux/limits.h>
 #include <sys/stat.h>
 #include <string.h>
 #include "wdxplugin.h"
 
-#define _detectstring "EXT=\"*\""
+#define BUFF_SIZE 1024
 
 typedef struct _field
 {
 	char *name;
 	int type;
-	char *unit;
+	int algo;
 } FIELD;
 
 #define fieldcount (sizeof(fields)/sizeof(FIELD))
 
 FIELD fields[] =
 {
-	{"CRC32",		ft_string,		""},
-	{"MD5",			ft_string,		""},
-	{"SHA1",		ft_string,		""},
-	{"SHA256",		ft_string,		""},
-	{"SHA512",		ft_string,		""},
+	{"CRC32",		ft_string,		GCRY_MD_CRC32},
+	{"MD5",			ft_string,		  GCRY_MD_MD5},
+	{"SHA1",		ft_string,		 GCRY_MD_SHA1},
+	{"SHA256",		ft_string,	       GCRY_MD_SHA256},
+	{"SHA512",		ft_string,	       GCRY_MD_SHA512},
+	{"BLAKE2B 256",		ft_string,	  GCRY_MD_BLAKE2B_256},
+	{"BLAKE2B 512",		ft_string,	  GCRY_MD_BLAKE2B_512},
+	{"BLAKE2B 384",		ft_string,	  GCRY_MD_BLAKE2B_384},
+	{"BLAKE2B 160",		ft_string,	  GCRY_MD_BLAKE2B_160},
+	{"BLAKE2S 256",		ft_string,	  GCRY_MD_BLAKE2S_256},
+	{"BLAKE2S 224",		ft_string,	  GCRY_MD_BLAKE2S_224},
+	{"BLAKE2S 160",		ft_string,	  GCRY_MD_BLAKE2S_160},
+	{"BLAKE2S 128",		ft_string,	  GCRY_MD_BLAKE2S_128},
+	{"SHA384",		ft_string,	       GCRY_MD_SHA384},
+	{"SHA224",		ft_string,	       GCRY_MD_SHA224},
+	{"RMD160",		ft_string,	       GCRY_MD_RMD160},
+	{"MD2",			ft_string,		  GCRY_MD_MD2},
+	{"MD4",			ft_string,		  GCRY_MD_MD4},
+	{"TIGER",		ft_string,		GCRY_MD_TIGER},
+	{"HAVAL",		ft_string,		GCRY_MD_HAVAL},
+	{"CRC32 RFC1510",	ft_string,	GCRY_MD_CRC32_RFC1510},
+	{"CRC24 RFC2440",	ft_string,	GCRY_MD_CRC24_RFC2440},
+	{"TIGER1",		ft_string,	       GCRY_MD_TIGER1},
+	{"TIGER2",		ft_string,	       GCRY_MD_TIGER2},
+	{"STRIBOG256",		ft_string,	   GCRY_MD_STRIBOG256},
+	{"STRIBOG512",		ft_string,	   GCRY_MD_STRIBOG512},
+	{"GOSTR3411_CP",	ft_string,	 GCRY_MD_GOSTR3411_CP},
+	{"GOSTR3411_94 ",	ft_string,	 GCRY_MD_GOSTR3411_94},
+	{"WHIRLPOOL",		ft_string,	    GCRY_MD_WHIRLPOOL},
+	{"SHA3 224",		ft_string,	     GCRY_MD_SHA3_224},
+	{"SHA3 256",		ft_string,	     GCRY_MD_SHA3_256},
+	{"SHA3 384",		ft_string,	     GCRY_MD_SHA3_384},
+	{"SHA3 512",		ft_string,	     GCRY_MD_SHA3_512},
 };
 
 char* strlcpy(char* p, const char* p2, int maxlen)
@@ -46,14 +72,8 @@ int DCPCALL ContentGetSupportedField(int FieldIndex, char* FieldName, char* Unit
 		return ft_nomorefields;
 
 	strlcpy(FieldName, fields[FieldIndex].name, maxlen - 1);
-	strlcpy(Units, fields[FieldIndex].unit, maxlen - 1);
+	strlcpy(Units, "", maxlen - 1);
 	return fields[FieldIndex].type;
-}
-
-int DCPCALL ContentGetDetectString(char* DetectString, int maxlen)
-{
-	strlcpy(DetectString, _detectstring, maxlen - 1);
-	return 0;
 }
 
 int DCPCALL ContentGetValue(char* FileName, int FieldIndex, int UnitIndex, void* FieldValue, int maxlen, int flags)
@@ -61,10 +81,17 @@ int DCPCALL ContentGetValue(char* FileName, int FieldIndex, int UnitIndex, void*
 	if (flags && CONTENT_DELAYIFSLOW)
 		return ft_delayed;
 
-	int i, bytes;
-	unsigned char data[1024];
-	char result[128];
+	if (FieldIndex < 0 || FieldIndex >= fieldcount)
+		return ft_nosuchfield;
+
+	size_t i, bytes;
+	unsigned char data[BUFF_SIZE];
+	char* result = NULL;
+	unsigned char* digest;
+	unsigned int digestlen;
 	struct stat buf;
+	gcry_md_hd_t h;
+	gcry_error_t err;
 
 	if (stat(FileName, &buf))
 		return ft_fileerror;
@@ -77,75 +104,41 @@ int DCPCALL ContentGetValue(char* FileName, int FieldIndex, int UnitIndex, void*
 	if (inFile == NULL)
 		return ft_fileerror;
 
-	if (FieldIndex == 0)
+	err = gcry_md_open(&h, fields[FieldIndex].algo, 0);
+
+	if (gcry_err_code(err))
 	{
-		uLong c = crc32(0L, Z_NULL, 0);
-
-		while ((bytes = fread(data, 1, 1024, inFile)) != 0)
-			c = crc32(c, data, bytes);
-
-		sprintf(result, "%02lx", c);
+		fclose(inFile);
+		return ft_fieldempty;
 	}
-	else if (FieldIndex == 1)
+
+	while ((bytes = fread(data, 1, BUFF_SIZE, inFile)) != 0)
+		gcry_md_write(h, data, bytes);
+
+	digest = gcry_md_read(h, fields[FieldIndex].algo);
+	digestlen = gcry_md_get_algo_dlen(fields[FieldIndex].algo);
+
+	size_t res_size = (size_t)digestlen * sizeof(char) * 2 + 1;
+	result = (char*)malloc(res_size);
+
+	if (result != NULL)
 	{
-		unsigned char c[SHA256_DIGEST_LENGTH];
-		MD5_CTX md5;
-		MD5_Init(&md5);
+		memset(result, 0, res_size);
 
-		while ((bytes = fread(data, 1, 1024, inFile)) != 0)
-			MD5_Update(&md5, data, bytes);
-
-		MD5_Final(c, &md5);
-
-		for (i = 0; i < MD5_DIGEST_LENGTH; i++)
-			sprintf(&result[i * 2], "%02x", c[i]);
-	}
-	else if (FieldIndex == 2)
-	{
-		unsigned char c[SHA_DIGEST_LENGTH];
-		SHA_CTX sha1;
-		SHA1_Init(&sha1);
-
-		while ((bytes = fread(data, 1, 1024, inFile)) != 0)
-			SHA1_Update(&sha1, data, bytes);
-
-		SHA1_Final(c, &sha1);
-
-		for (i = 0; i < SHA_DIGEST_LENGTH; i++)
-			sprintf(&result[i * 2], "%02x", c[i]);
-	}
-	else if (FieldIndex == 3)
-	{
-		unsigned char c[SHA256_DIGEST_LENGTH];
-		SHA256_CTX sha256;
-		SHA256_Init(&sha256);
-
-		while ((bytes = fread(data, 1, 1024, inFile)) != 0)
-			SHA256_Update(&sha256, data, bytes);
-
-		SHA256_Final(c, &sha256);
-
-		for (i = 0; i < SHA256_DIGEST_LENGTH; i++)
-			sprintf(&result[i * 2], "%02x", c[i]);
-	}
-	else if (FieldIndex == 4)
-	{
-		unsigned char c[SHA512_DIGEST_LENGTH];
-		SHA512_CTX sha512;
-		SHA512_Init(&sha512);
-
-		while ((bytes = fread(data, 1, 1024, inFile)) != 0)
-			SHA512_Update(&sha512, data, bytes);
-
-		SHA512_Final(c, &sha512);
-
-		for (i = 0; i < SHA512_DIGEST_LENGTH; i++)
-			sprintf(&result[i * 2], "%02x", c[i]);
+		for (i = 0; i < digestlen; i++)
+			sprintf(&result[i * 2], "%02x", digest[i]);
 	}
 
 	fclose(inFile);
-	strlcpy((char*)FieldValue, result, maxlen - 1);
+	gcry_md_close(h);
+
+	if (result != NULL)
+	{
+		strlcpy((char*)FieldValue, result, maxlen - 1);
+		free(result);
+	}
+	else
+		return ft_fieldempty;
 
 	return fields[FieldIndex].type;
-
 }
