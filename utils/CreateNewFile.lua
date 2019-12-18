a25834847eb5d9bc79c2060e3bfb12b9b4e4a18d
Skif-off <Skif-off@users.noreply.github.com>
2019-12-18 07:34:47 +0400

    UPD: CreateNewFile.lua: save entered name, code style

--- utils/CreateNewFile.lua
+++ utils/CreateNewFile.lua
@@ -1,6 +1,7 @@
--- Create new file (cross-platform)
--- 2018.12.01
+-- CreateNewFile.lua (cross-platform)
+-- 2019.12.18
 --
+-- Create new file using the template(s)
 -- Params:
 --   %"0%D
 --   %dir-with-templates%
@@ -23,45 +24,46 @@ DOUBLECMD#TOOLBAR#XMLDATA<?xml version="1.0" encoding="UTF-8"?>
 
 local params = {...}
 local items = {}
-local hd, fd, rt, st, i, ba, sn, tf, bp, fl, ct
+local hd, fd, rt, st, i, ba, tf, bp, fl, ct
+local sn = 'new'
 
-if (#params ~= 2) then
-  Dialogs.MessageBox('Check the number of parameters!\nRequires only two.', 'Create new file', 0x0030);
-  return;
+if #params ~= 2 then
+  Dialogs.MessageBox('Check the number of parameters!\nRequires only two.', 'Create new file', 0x0030)
+  return
 end
 -- Get list of new files
-hd, fd = SysUtils.FindFirst(params[2] .. SysUtils.PathDelim .. '*');
-if (hd ~= nil) then
+hd, fd = SysUtils.FindFirst(params[2] .. SysUtils.PathDelim .. '*')
+if hd ~= nil then
   repeat
-    if (fd.Name ~= '.') and (fd.Name ~= '..') then table.insert(items, fd.Name) end;
-    rt, fd = SysUtils.FindNext(hd);
+    if (fd.Name ~= '.') and (fd.Name ~= '..') then table.insert(items, fd.Name) end
+    rt, fd = SysUtils.FindNext(hd)
   until rt == nil
-  SysUtils.FindClose(hd);
+  SysUtils.FindClose(hd)
 end
 -- Get new file
-if (#items >= 1) then
-  table.sort(items);
-  st = Dialogs.InputListBox('Create new file', 'Select type:', items, 1);
-  if (st == nil) then return end;
+if #items >= 1 then
+  table.sort(items)
+  st = Dialogs.InputListBox('Create new file', 'Select type:', items, 1)
+  if st == nil then return end
   -- Get new filename
   while i ~= 1 do
-    ba, sn = Dialogs.InputQuery('Type: ' .. st, 'Enter filename:', false, 'new');
-    if (ba == false) then return end;
-    tf = params[1] .. SysUtils.PathDelim .. string.gsub(st, '(.+)(%.[^%.]+)$', sn .. '%2');
-    if (SysUtils.FileExists(tf) == false) then break end;
-    bp = Dialogs.MessageBox('File "' .. tf .. '" already exists!\n\nOverwrite?', 'Create new file', 0x0003 + 0x0030 + 0x0100);
-    if (bp == 0x0006) then
-      break;
+    ba, sn = Dialogs.InputQuery('Type: ' .. st, 'Enter filename:', false, sn)
+    if ba == false then return end
+    tf = params[1] .. SysUtils.PathDelim .. string.gsub(st, '(.+)(%.[^%.]+)$', sn .. '%2')
+    if SysUtils.FileExists(tf) == false then break end
+    bp = Dialogs.MessageBox('File "' .. tf .. '" already exists!\n\nOverwrite?', 'Create new file', 0x0003 + 0x0030 + 0x0100)
+    if bp == 0x0006 then
+      break
     else
-      if (bp ~= 0x0007) then return end;
+      if bp ~= 0x0007 then return end
     end
   end
   -- Read-copy
-  fl = io.open(params[2] .. SysUtils.PathDelim .. st, 'rb');
-  ct = fl:read('*a');
-  fl:close();
+  fl = io.open(params[2] .. SysUtils.PathDelim .. st, 'rb')
+  ct = fl:read('*a')
+  fl:close()
   -- Write-paste
-  fl = io.open(tf, 'wb');
-  fl:write(ct);
-  fl:close();
+  fl = io.open(tf, 'wb')
+  fl:write(ct)
+  fl:close()
 end
