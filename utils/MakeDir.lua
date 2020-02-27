8118615c1930b457cde42321a2297b73042736f7
Skif-off <Skif-off@users.noreply.github.com>
2020-02-27 08:23:34 +0400

    UPD: MakeDir.lua: Fix multiple folder creation with counter; Upd desc. and some internal changes

--- utils/MakeDir.lua
+++ utils/MakeDir.lua
@@ -1,63 +1,37 @@
 -- MakeDir.lua (cross-platform)
--- 2020.02.18
+-- 2020.02.27
 --
 -- Creating directories with additional features.
-
--- 1. You can use absolute name (/path/to/newdir), relative to current path (to/newdir) or only name (newdir).
--- 2. Script will also create all directories leading up to the entered name(s) that do not exist already.
--- 3. If you want more one dir at once then use vertical bar:
---    newdir1|newdir2|newdir3
--- 4. Also you can use some variables:
---   - <dt>: current date and time: YYYYMMDD-hhmmss;
---   - <d> or <t>: current date (YYYYMMDD) or time (hhmmss);
---   - <y>: current year, 4 digits;
---   - <p>: parent folder from current directory;
---   - <x-y>: multiple folder creation with counter. Wight depends on y.
---     For example:
---     "name<1-15>" means 15 folders: "name01", "name02", ... "name15".
---
+--[[
+1. You can use absolute name (/path/to/newdir), relative to current path (to/newdir) or only name (newdir).
+  Script will also create all directories leading up to the entered name(s) that do not exist already.
+2. If you want more one dir at once then use vertical bar:
+  newdir1|newdir2|newdir3
+3. All space characters at the beginning or end of the name(s) will be deleted.
+4. Also you can use some variables:
+    <dt> - current date and time: YYYYMMDD-hhmmss;
+    <d> or <t> - current date (YYYYMMDD) or time (hhmmss);
+    <y> - current year, 4 digits;
+    <p> - parent folder from current directory;
+    <x-y> - multiple folder creation with counter. Wight depends on y.
+    For example: "name<1-15>" means 15 folders: "name01", "name02", ... "name15".
+  Note: You can use more then one variable in each name.
+]]
 -- Params:
 --   %"0%D
 --   %permission_mode%
 --
 -- %permission_mode% is optional parameter for Linux only, use octal mode (for example, 755 or 777)
---
--- Button example:
---[[
-DOUBLECMD#TOOLBAR#XMLDATA<?xml version="1.0" encoding="UTF-8"?>
-<doublecmd>
-  <Command>
-    <ID>{78AE07D0-42A0-4238-92BB-BE802AFB6F49}</ID>
-    <Icon>cm_executescript</Icon>
-    <Hint>Make dir(s)</Hint>
-    <Command>cm_ExecuteScript</Command>
-    <Param>%COMMANDER_PATH%/utils/MakeDir.lua</Param>
-    <Param>%"0%D</Param>
-  </Command>
-</doublecmd>
-]]
 
 local params = {...}
 
-local function PathIsAbsolute(s, ds)
-  if ds == '\\' then
+local function PathIsAbsolute(s)
+  if SysUtils.PathDelim == '\\' then
     if string.match(s, '^[A-Za-z]:\\.*') ~= nil then return true end
   else
     if string.sub(s, 1, 1) == '/' then return true end
   end
   return false
-end
-
-local function MakeList(s, p, ds)
-  local a = {}
-  for l in string.gmatch(s, '[^|]+') do
-    if PathIsAbsolute(l, ds) then
-      table.insert(a, l)
-    else
-      table.insert(a, p .. ds .. l)
-    end
-  end
-  return a
 end
 
 local function GetOutput(cmd)
@@ -67,13 +41,9 @@ local function GetOutput(cmd)
   return string.sub(out, 1, -2)
 end
 
-local function MakeDir(a, ds)
+local function MakeDir(a)
   local m
-  if ds == '/' then
-    local cmd = 'mkdir -p'
-    if #params == 2 then cmd = 'mkdir -m ' .. params[2] .. ' -p' end
-    m = GetOutput(cmd .. ' "' .. table.concat(a, '" "') .. '" 2>&1')
-  else
+  if SysUtils.PathDelim == '\\' then
     m = ''
     local c = 0
     for i = 1, #a do
@@ -83,6 +53,10 @@ local function MakeDir(a, ds)
       end
     end
     if c > 0 then m = 'Error:\n' .. m end
+  else
+    local cmd = 'mkdir -p'
+    if #params == 2 then cmd = 'mkdir -m ' .. params[2] .. ' -p' end
+    m = GetOutput(cmd .. ' "' .. table.concat(a, '" "') .. '" 2>&1')
   end
   return m
 end
@@ -92,10 +66,10 @@ if (#params == 0) or (#params > 2) then
   return
 end
 
-local msg, tmp, lst
+local msg = ''
 local msgl = [[
 Variables:
-<dt>: current date and time: YYYYMMDD-hhmmss;
+<dt>: current date and time: YYYYMMDDhhmmss;
 <d> or <t>: current date (YYYYMMDD) or time (hhmmss);
 <y>: current year, 4 digits;
 <p>: parent folder from current directory;
@@ -103,23 +77,29 @@ Variables:
 ]]
 local ba, dn = Dialogs.InputQuery('Make dir(s)', msgl .. '\nEnter name:', false, 'new')
 if ba == false then return end
-local ds = SysUtils.PathDelim
-local cdt = os.date('%Y%m%d-%H%M%S', os.time())
+local cdt = os.date('%Y%m%d%H%M%S', os.time())
 local pf = SysUtils.ExtractFileName(params[1])
 -- delete trailing space(s)
 dn = string.gsub(dn, '^%s+', '')
 dn = string.gsub(dn, '%s+$', '')
 dn = string.gsub(dn, '(%s+)([\\/|])', '%2')
 dn = string.gsub(dn, '([\\/|])(%s+)', '%1')
-lst = MakeList(dn, params[1], ds)
-local n1, n2, c1, c2, t
-local n3, c3 = 1, 1
-local r = {}
+local lst = {}
 local lst2 = {}
+local r = {}
+local n1, n2, c1, c2, t, tmp
+local n3, c3 = 1, 1
+for l in string.gmatch(dn, '[^|]+') do
+  if PathIsAbsolute(l, SysUtils.PathDelim) then
+    table.insert(lst, l)
+  else
+    table.insert(lst, params[1] .. SysUtils.PathDelim .. l)
+  end
+end
 for i = 1, #lst do
   lst[i] = string.gsub(lst[i], '<dt>', cdt)
   lst[i] = string.gsub(lst[i], '<d>', string.sub(cdt, 1, 8))
-  lst[i] = string.gsub(lst[i], '<t>', string.sub(cdt, 10, -1))
+  lst[i] = string.gsub(lst[i], '<t>', string.sub(cdt, 9, 14))
   lst[i] = string.gsub(lst[i], '<y>', string.sub(cdt, 1, 4))
   lst[i] = string.gsub(lst[i], '<p>', pf)
   while true do
@@ -142,13 +122,14 @@ for i = 1, #lst do
     end
     n3 = n2
   end
+  n3 = 1
 end
 -- <x-y>
-for i = 1, #r do table.remove(lst, r[i]) end
+for i = #r, 1, -1 do table.remove(lst, r[i]) end
 
-msg = MakeDir(lst, ds)
+if #lst > 0 then msg = MakeDir(lst) end
 if #lst2 > 0 then
-  tmp = MakeDir(lst2, ds)
+  tmp = MakeDir(lst2)
   if (tmp ~= '') and (tmp ~= nil) then
     if (msg ~= '') and (msg ~= nil) then
       msg = msg .. string.sub(tmp, 7, -1)
@@ -158,7 +139,7 @@ if #lst2 > 0 then
   end
 end
 
-msg = string.gsub(msg, '\n\n+', '\n')
-if string.len(msg) > 8 then
+if (msg ~= '') and (msg ~= nil) then
+  msg = string.gsub(msg, '\n\n+', '\n')
   Dialogs.MessageBox(msg, 'Make dir(s)', 0x0030)
 end
