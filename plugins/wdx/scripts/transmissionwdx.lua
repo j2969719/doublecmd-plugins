fdf484d27aa0bf4538ca5d0b943512ad24ac51b7
j2969719 <j172933@mvrht.net>
2020-03-30 02:49:56 +0300

    helloworld

--- plugins/wdx/scripts/transmissionwdx.lua
+++ plugins/wdx/scripts/transmissionwdx.lua
@@ -6,17 +6,45 @@ local output = ''
 local filename = ''
 
 local fields = {
-    {"Name",        8,         "%s%sName:%s([^\n]+)"},  -- name, field type, pattern
-    {"Hash",        8,         "%s%sHash:%s([^\n]+)"}, 
-    {"Created by",  8,  "%s%sCreated%sby:%s([^\n]+)"}, 
-    {"Created on",  8,  "%s%sCreated%son:%s([^\n]+)"}, 
-    {"Comment",     8,      "%s%sComment:%s([^\n]+)"}, 
-    {"Piece Count", 2, "%s%sPiece%sCount:%s([^\n]+)"}, 
-    {"Piece Size",  8,  "%s%sPiece%sSize:%s([^\n]+)"}, 
-    {"Total Size",  8,  "%s%sTotal%sSize:%s([^\n]+)"}, 
-    {"Privacy",     8,      "%s%sPrivacy:%s([^\n]+)"}, 
+    {"Name",                8,         "%s%sName:%s([^\n]+)"},      -- name, field type, pattern
+    {"Hash",                8,         "%s%sHash:%s([^\n]+)"}, 
+    {"Created by",          8,  "%s%sCreated%sby:%s([^\n]+)"}, 
+    {"Created on",          8,  "%s%sCreated%son:%s([^\n]+)"}, 
+    {"Comment",             8,      "%s%sComment:%s([^\n]+)"}, 
+    {"Piece Count",         2, "%s%sPiece%sCount:%s([^\n]+)"}, 
+    {"Piece Size",          8,  "%s%sPiece%sSize:%s([^\n]+)"}, 
+    {"Total Size",          8,  "%s%sTotal%sSize:%s([^\n]+)"}, 
+    {"Privacy",             8,      "%s%sPrivacy:%s([^\n]+)"}, 
+    {"Piece Size (bytes)",  8,  "%s%sPiece%sSize:%s([^\n]+)"}, 
+    {"Total Size (bytes)",  2,  "%s%sTotal%sSize:%s([^\n]+)"}, 
 }
 
+local mults = {
+    ["kB"]  =  math.pow(10, 3),  
+    ["KB"]  =  math.pow(10, 3),  
+    ["MB"]  =  math.pow(10, 6),  
+    ["GB"]  =  math.pow(10, 9),  
+    ["TB"]  = math.pow(10, 12),  
+    ["KiB"] =  math.pow(2, 10),  
+    ["MiB"] =  math.pow(2, 20),  
+    ["GiB"] =  math.pow(2, 30),  
+    ["TiB"] =  math.pow(2, 40),  
+}
+
+local months = {
+    ["Jan"] = "01", 
+    ["Feb"] = "02", 
+    ["Mar"] = "03", 
+    ["Apr"] = "04", 
+    ["May"] = "05", 
+    ["Jun"] = "06", 
+    ["Jul"] = "07", 
+    ["Aug"] = "08", 
+    ["Sep"] = "09", 
+    ["Oct"] = "10", 
+    ["Nov"] = "11", 
+    ["Dec"] = "12", 
+}
 
 function ContentGetSupportedField(FieldIndex)
     if (fields[FieldIndex + 1] ~= nil) then
@@ -40,8 +68,28 @@ function ContentGetValue(FileName, FieldIndex, UnitIndex, flags)
         handle:close();
         filename = FileName;
     end
+    local result = nil;
     if (output ~= nil) and (fields[FieldIndex + 1][3] ~= nil) then
-        return output:match(fields[FieldIndex + 1][3]);
-    end 
-    return nil;
+        result = output:match(fields[FieldIndex + 1][3]);
+        if (result ~= nil) then
+            if (FieldIndex > 8) then
+                mult = result:match("%w+$");
+                size = result:gsub("[^%d%.]", '');
+                size = tonumber(size)
+                if (mults[mult] ~= nil) then
+                    size = size * mults[mult];  
+                end
+                result = size;
+            elseif (FieldIndex == 3) then
+                month, day, dtime, year = result:match("%w+%s+(%w+)%s+(%d+)%s+([%d:]+)%s+(%d+)");
+                if (tonumber(day) < 10) then
+                    day = '0' .. day
+                end
+                if (months[month] ~= nil) and (year ~= nil)  and (day ~= nil) and (dtime ~= nil) then
+                    result = year .. '-' .. months[month] .. '-' .. day .. ' ' .. dtime;
+                end
+            end
+        end
+    end
+    return result;
 end
