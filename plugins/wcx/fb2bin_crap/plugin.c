c1a06bcfa3e31b4a8b1a93a9c3d9252acc512854
j2969719 <j172933@mvrht.net>
2020-04-10 22:38:13 +0300

    helloworld

---
+++ plugins/wcx/fb2bin_crap/plugin.c
@@ -0,0 +1,182 @@
+#include <glib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <libxml/parser.h>
+#include <libxml/xpath.h>
+#include "wcxplugin.h"
+#include "extension.h"
+
+typedef struct sArcData
+{
+	char arcname[PATH_MAX + 1];
+	char filename[PATH_MAX + 1];
+	xmlDoc *document;
+	xmlNode *node;
+	gsize out_len;
+	guchar *data;
+	time_t filetime;
+	tProcessDataProc ProcessDataProc;
+} tArcData;
+
+typedef tArcData* ArcData;
+typedef void *HINSTANCE;
+
+tProcessDataProc gProcessDataProc = NULL;
+
+HANDLE DCPCALL OpenArchive(tOpenArchiveData *ArchiveData)
+{
+	struct stat buf;
+	tArcData * handle;
+	handle = malloc(sizeof(tArcData));
+	memset(handle, 0, sizeof(tArcData));
+	strncpy(handle->arcname, ArchiveData->ArcName, PATH_MAX);
+	handle->document = xmlReadFile(ArchiveData->ArcName, NULL, 0);
+
+	if (handle->document == NULL)
+	{
+		free(handle);
+		ArchiveData->OpenResult = E_UNKNOWN_FORMAT;
+		return E_SUCCESS;
+	}
+
+	xmlNode *root = xmlDocGetRootElement(handle->document);
+
+	if (strcmp(root->name, "FictionBook") != 0)
+	{
+		xmlFreeDoc(handle->document);
+		free(handle);
+		ArchiveData->OpenResult = E_UNKNOWN_FORMAT;
+		return E_SUCCESS;
+	}
+
+	if (stat(ArchiveData->ArcName, &buf) == 0)
+		handle->filetime = buf.st_mtime;
+
+	handle->node = root->children;
+	return (HANDLE)handle;
+
+}
+
+int DCPCALL ReadHeader(HANDLE hArcData, tHeaderData *HeaderData)
+{
+	memset(HeaderData, 0, sizeof(HeaderData));
+	ArcData handle = (ArcData)hArcData;
+
+	xmlNode *node = handle->node;
+	handle->data = NULL;
+	memset(handle->filename, 0, sizeof(handle->filename));
+
+	if (node)
+	{
+		if (strcmp(node->name, "binary") == 0)
+		{
+			xmlChar *bin_id, *bin_content;
+			bin_id = xmlGetProp(node, "id");
+
+			char *pdot = strchr((char*)bin_id, '.');
+
+			if (pdot != NULL)
+			{
+				snprintf(handle->filename, PATH_MAX, "%s", bin_id);
+			}
+			else
+			{
+				bin_content = xmlGetProp(node, "content-type");
+				char *p = strchr((char*)bin_content, '/');
+
+				if (p != NULL)
+					snprintf(handle->filename, PATH_MAX, "%s.%s", bin_id, p + 1);
+
+				xmlFree(bin_content);
+			}
+
+			xmlFree(bin_id);
+
+			if (handle->filename[0] != '\0')
+			{
+				strncpy(HeaderData->FileName, handle->filename, sizeof(HeaderData->FileName) - 1);
+				HeaderData->FileTime = handle->filetime;
+				handle->data = g_base64_decode((char*)node->children->content, &handle->out_len);
+				HeaderData->UnpSize = handle->out_len;
+				HeaderData->PackSize = handle->out_len;
+				HeaderData->FileAttr = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+			}
+		}
+
+		return E_SUCCESS;
+	}
+
+	return E_END_ARCHIVE;
+}
+
+int DCPCALL ProcessFile(HANDLE hArcData, int Operation, char *DestPath, char *DestName)
+{
+	ArcData handle = (ArcData)hArcData;
+	xmlNode *node = handle->node;
+
+	if (Operation == PK_EXTRACT)
+	{
+		FILE *fp = fopen(DestName, "w");
+
+		if (fp)
+		{
+			for (gsize i = 0; i < handle->out_len; i++)
+			{
+				fputc(handle->data[i], fp);
+
+				if (handle->ProcessDataProc)
+					handle->ProcessDataProc(handle->filename, 1);
+			}
+
+			fclose(fp);
+		}
+	}
+
+	if (handle->data != NULL)
+		g_free(handle->data);
+
+	handle->node = node->next;
+
+	return E_SUCCESS;
+}
+
+int DCPCALL CloseArchive(HANDLE hArcData)
+{
+	ArcData handle = (ArcData)hArcData;
+	xmlFreeDoc(handle->document);
+	free(handle);
+	return E_SUCCESS;
+}
+
+int DCPCALL GetPackerCaps(void)
+{
+	return PK_CAPS_HIDE;
+}
+
+void DCPCALL SetProcessDataProc(HANDLE hArcData, tProcessDataProc pProcessDataProc)
+{
+	ArcData handle = (ArcData)hArcData;
+
+	if ((int)(long)hArcData == -1 || !handle)
+		gProcessDataProc = pProcessDataProc;
+	else
+		handle->ProcessDataProc = pProcessDataProc;
+}
+
+void DCPCALL SetChangeVolProc(HANDLE hArcData, tChangeVolProc pChangeVolProc1)
+{
+	return;
+}
+
+BOOL DCPCALL CanYouHandleThisFile(char *FileName)
+{
+	char *pdot = strchr(FileName, '.');
+
+	if (pdot != NULL)
+	{
+		if (strcmp(pdot, ".fb2") == 0)
+			return TRUE;
+	}
+
+	return FALSE;
+}
