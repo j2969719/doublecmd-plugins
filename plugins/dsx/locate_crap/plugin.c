5071a1dd28f184dba82456798ddabfedb091fec8
j2969719 <j172933@mvrht.net>
2020-06-26 14:03:40 +0300

    helloworld

--- plugins/dsx/locate_crap/plugin.c
+++ plugins/dsx/locate_crap/plugin.c
@@ -15,6 +15,8 @@ tSAddFileProc gAddFileProc;
 tSUpdateStatusProc gUpdateStatus;
 
 gboolean gStop;
+GKeyFile *gCfg = NULL;
+static gchar gCfgPath[PATH_MAX];
 
 int DCPCALL Init(tDsxDefaultParamStruct* dsp, tSAddFileProc pAddFileProc, tSUpdateStatusProc pUpdateStatus)
 {
@@ -24,12 +26,13 @@ int DCPCALL Init(tDsxDefaultParamStruct* dsp, tSAddFileProc pAddFileProc, tSUpda
 	Dl_info dlinfo;
 	static char plg_path[PATH_MAX];
 	const char* loc_dir = "langs";
+	GError *err = NULL;
 
 	memset(&dlinfo, 0, sizeof(dlinfo));
 
 	if (dladdr(plg_path, &dlinfo) != 0)
 	{
-		strncpy(plg_path, dlinfo.dli_fname, PATH_MAX);
+		g_strlcpy(plg_path, dlinfo.dli_fname, PATH_MAX);
 		char *pos = strrchr(plg_path, '/');
 
 		if (pos)
@@ -38,6 +41,14 @@ int DCPCALL Init(tDsxDefaultParamStruct* dsp, tSAddFileProc pAddFileProc, tSUpda
 		setlocale(LC_ALL, "");
 		bindtextdomain(GETTEXT_PACKAGE, plg_path);
 		textdomain(GETTEXT_PACKAGE);
+
+		g_strlcpy(gCfgPath, dlinfo.dli_fname, PATH_MAX);
+		pos = strrchr(gCfgPath, '/');
+
+		if (pos)
+			strcpy(pos + 1, "settings.ini");
+
+		gCfg = g_key_file_new();
 	}
 
 	return 0;
@@ -45,85 +56,114 @@ int DCPCALL Init(tDsxDefaultParamStruct* dsp, tSAddFileProc pAddFileProc, tSUpda
 
 void DCPCALL StartSearch(int PluginNr, tDsxSearchRecord* pSearchRec)
 {
-	gchar **argv = NULL;
-	gchar *line = NULL, *command = NULL;
-	gsize len, term, i = 1;
+	gchar *argv[7];
+	gchar *line = NULL, *dppath = NULL;
+	gchar locate_opts[7] = "-q";
+	gsize len, term, i;
 	GPid pid;
 	gint fp;
 	GSpawnFlags flags = G_SPAWN_SEARCH_PATH;
 	GError *err = NULL;
 	gboolean dirsonly = FALSE, exludedirs = FALSE;
+	gboolean startpath;
 
-	gStop = FALSE;
+	i = strlen(locate_opts);
 
-	if (system("locate -V") == 0)
+	if (!g_key_file_load_from_file(gCfg, gCfgPath, G_KEY_FILE_KEEP_COMMENTS, &err))
+		g_print("%s: %s\n", gCfgPath, (err)->message);
+
+	if (g_key_file_get_boolean(gCfg, "Search", "Basename", NULL) == TRUE)
+		locate_opts[i++] = 'b';
+
+	if (g_key_file_get_boolean(gCfg, "Search", "IgnoreCase", NULL) == TRUE)
+		locate_opts[i++] = 'i';
+
+	if (g_key_file_get_boolean(gCfg, "Search", "Existing", NULL) == TRUE)
+		locate_opts[i++] = 'e';
+
+	if (g_key_file_get_boolean(gCfg, "Search", "NoFolow", NULL) == TRUE)
+		locate_opts[i++] = 'P';
+
+	startpath = g_key_file_get_boolean(gCfg, "Search", "StartPath", NULL);
+	dppath = g_key_file_get_string(gCfg, "DB", "Path", NULL);
+
+	i = 0;
+	argv[i++] = "locate";
+	argv[i++] = locate_opts;
+
+	if (dppath)
 	{
-		if (g_strrstr(pSearchRec->AttribStr, "d+") != NULL)
-			dirsonly = TRUE;
+		argv[i++] = "-d";
+		argv[i++] = dppath;
+	}
 
-		if (g_strrstr(pSearchRec->AttribStr, "d-") != NULL)
-			exludedirs = TRUE;
+	if (g_key_file_get_boolean(gCfg, "Search", "RegEx", NULL) == TRUE)
+		argv[i++] = "--regex";
 
-		command = g_strdup_printf("locate -e -i -b '%s'", pSearchRec->FileMask);
-		gUpdateStatus(PluginNr, command, 0);
+	argv[i++] = pSearchRec->FileMask;
+	argv[i++] = NULL;
 
-		gUpdateStatus(PluginNr, _("not found"), 0);
+	if (g_strrstr(pSearchRec->AttribStr, "d+") != NULL)
+		dirsonly = TRUE;
 
-		if (!g_shell_parse_argv(command, NULL, &argv, &err))
+	if (g_strrstr(pSearchRec->AttribStr, "d-") != NULL)
+		exludedirs = TRUE;
+
+	gUpdateStatus(PluginNr, _("not found"), 0);
+	gStop = FALSE;
+	i = 1;
+
+	if (!g_spawn_async_with_pipes(NULL, argv, NULL, flags, NULL, NULL, &pid, NULL, &fp, NULL, &err))
+
+		gUpdateStatus(PluginNr, g_strdup(err->message), 0);
+
+	else
+	{
+		GIOChannel *stdout = g_io_channel_unix_new(fp);
+
+		while (!gStop && (G_IO_STATUS_NORMAL == g_io_channel_read_line(stdout, &line, &len, &term, NULL)))
 		{
-			gUpdateStatus(PluginNr, g_strdup(err->message), 0);
-			gAddFileProc(PluginNr, "");
-		}
-		else
-		{
-			g_clear_error(&err);
-
-			if (!g_spawn_async_with_pipes(NULL, argv, NULL, flags, NULL, NULL, &pid, NULL, &fp, NULL, &err))
+			if (line)
 			{
-				gUpdateStatus(PluginNr, g_strdup(err->message), 0);
-				gAddFileProc(PluginNr, "");
-			}
-			else
-			{
-				GIOChannel *stdout = g_io_channel_unix_new(fp);
 
-				while (!gStop && (G_IO_STATUS_NORMAL == g_io_channel_read_line(stdout, &line, &len, &term, NULL)))
+				line[term] = '\0';
+
+				if (dirsonly || exludedirs)
 				{
-					if (line)
+					gboolean isdir = g_file_test(line, G_FILE_TEST_IS_DIR);
+
+					if ((dirsonly && isdir) || (exludedirs && !isdir))
 					{
-
-						line[term] = '\0';
-
-						if (dirsonly || exludedirs)
+						if (startpath)
 						{
-							gboolean isdir = g_file_test(line, G_FILE_TEST_IS_DIR);
-
-							if ((dirsonly && isdir) || (exludedirs && !isdir))
+							if (strncmp(line, pSearchRec->StartPath, strlen(pSearchRec->StartPath) - 1) == 0)
 								gAddFileProc(PluginNr, line);
 						}
 						else
 							gAddFileProc(PluginNr, line);
-
-						gUpdateStatus(PluginNr, line, i++);
-						g_free(line);
 					}
 				}
+				else if (startpath)
+				{
+					if (strncmp(line, pSearchRec->StartPath, strlen(pSearchRec->StartPath) - 1) == 0)
+						gAddFileProc(PluginNr, line);
+				}
+				else
+					gAddFileProc(PluginNr, line);
 
-				kill(pid, SIGTERM);
-				g_spawn_close_pid(pid);
-				g_io_channel_shutdown(stdout, TRUE, NULL);
-				g_io_channel_unref(stdout);
+				gUpdateStatus(PluginNr, line, i++);
+				g_free(line);
 			}
 		}
+
+		kill(pid, SIGTERM);
+		g_spawn_close_pid(pid);
+		g_io_channel_shutdown(stdout, TRUE, NULL);
+		g_io_channel_unref(stdout);
 	}
-	else
-		gUpdateStatus(PluginNr, _("failed to launch locate..."), 0);
 
-	if (command)
-		g_free(command);
-
-	if (argv)
-		g_strfreev(argv);
+	if (dppath)
+		g_free(dppath);
 
 	if (err)
 		g_error_free(err);
@@ -134,6 +174,12 @@ void DCPCALL StartSearch(int PluginNr, tDsxSearchRecord* pSearchRec)
 void DCPCALL StopSearch(int PluginNr)
 {
 	gStop = TRUE;
+
+	if (gCfg != NULL)
+	{
+		g_key_file_free(gCfg);
+		gCfg = NULL;
+	}
 }
 
 void DCPCALL Finalize(int PluginNr)
