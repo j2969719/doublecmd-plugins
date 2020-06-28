3c9258c19b0c54c994e91fa70b495d28137e2528
Skif-off <Skif-off@users.noreply.github.com>
2020-06-28 19:48:38 +0400

    FIX: gtkimgview: Memory leak https://doublecmd.sourceforge.io/forum/viewtopic.php?p=31214#p31214

--- plugins/wlx/gtkimgview/gtkimgview.c
+++ plugins/wlx/gtkimgview/gtkimgview.c
@@ -286,7 +286,9 @@ HWND DCPCALL ListLoad(HWND ParentWin, char* FileToLoad, int ShowFlags)
 
 void DCPCALL ListCloseWindow(HWND ListWin)
 {
+	GdkPixbuf *pixbuf = gtk_image_view_get_pixbuf(GTK_IMAGE_VIEW(find_child(ListWin, "imageview")));
 	gtk_widget_destroy(GTK_WIDGET(ListWin));
+	g_object_unref(pixbuf);
 }
 
 int DCPCALL ListSendCommand(HWND ListWin, int Command, int Parameter)
