0c713bbadacff907613fc4b2cef0100a682f2316
j2969719 <j172933@mvrht.net>
2021-01-28 20:57:56 +0300

    hold my beer

---
+++ plugins/wlx/qmediaplayer_qt5/readme.md
@@ -0,0 +1,11 @@
+qmediaplayer_qt5
+========
+Very primitive media player.
+
+![plug-preview](https://i.imgur.com/YI25AxF.png)
+
+## Compatibility
+- `doublecmd-qt5`
+
+## Dependencies
+![arch](https://wiki.archlinux.org/favicon.ico) `pacman -S qt5-multimedia`
--- plugins/wlx/qmediaplayer_qt5/src/plugin.cpp
+++ plugins/wlx/qmediaplayer_qt5/src/plugin.cpp
@@ -1,8 +1,10 @@
-#include <QStyle>
 #include <QtWidgets>
 #include <QVideoWidget>
 #include <QMediaPlayer>
 #include <QMimeDatabase>
+#include <QMediaMetaData>
+#include <QVideoSurfaceFormat>
+#include <QAbstractVideoSurface>
 #include "wlxplugin.h"
 
 Q_DECLARE_METATYPE(QMediaPlayer *)
@@ -25,11 +27,31 @@ HANDLE DCPCALL ListLoad(HANDLE ParentWin, char* FileToLoad, int ShowFlags)
 	QVBoxLayout *main = new QVBoxLayout(view);
 
 	QVideoWidget *video = new QVideoWidget(view);
-	player->setVideoOutput(video);
+	player->setVideoOutput(video->videoSurface());
+	main->addWidget(video);
+
+	QSlider *spos = new QSlider(Qt::Horizontal, view);
+	spos->setRange(0, player->duration() / 1000);
+
+	QObject::connect(spos, &QSlider::sliderMoved, [player](int x)
+	{
+		player->setPosition(x * 1000);
+	});
+
+	QObject::connect(player, &QMediaPlayer::durationChanged, [spos](qint64 x)
+	{
+		spos->setMaximum(x / 1000);
+	});
+
+	QObject::connect(player, &QMediaPlayer::positionChanged, [spos](qint64 x)
+	{
+		spos->setValue(x / 1000);
+	});
+
+	main->addWidget(spos);
 
 	QHBoxLayout *controls = new QHBoxLayout;
-	main->addWidget(video);
-	main->addLayout(controls);
+
 	QPushButton *bplay = new QPushButton(view);
 	bplay->setIcon(QApplication::style()->standardIcon(QStyle::SP_MediaPlay));
 	QObject::connect(bplay, SIGNAL(clicked()), player, SLOT(play()));
@@ -37,6 +59,18 @@ HANDLE DCPCALL ListLoad(HANDLE ParentWin, char* FileToLoad, int ShowFlags)
 	QPushButton *bpause = new QPushButton(view);
 	bpause->setIcon(QApplication::style()->standardIcon(QStyle::SP_MediaPause));
 	QObject::connect(bpause, SIGNAL(clicked()), player, SLOT(pause()));
+
+	QObject::connect(player, QOverload<const QString &, const QVariant &>::of(&QMediaObject::metaDataChanged), [video](const QString & key, const QVariant & value)
+	{
+		if (key == "CoverArtImage")
+		{
+			QImage image = value.value<QImage>().convertToFormat(QImage::Format_ARGB32);
+			QVideoSurfaceFormat format(image.size(), QVideoFrame::Format_ARGB32);
+			video->videoSurface()->start(format);
+			video->videoSurface()->present(image);
+		}
+	});
+
 
 	QSlider *svolume = new QSlider(Qt::Horizontal, view);
 	svolume->setRange(0, 100);
@@ -47,6 +81,7 @@ HANDLE DCPCALL ListLoad(HANDLE ParentWin, char* FileToLoad, int ShowFlags)
 	controls->addWidget(bpause);
 	controls->addStretch(1);
 	controls->addWidget(svolume);
+	main->addLayout(controls);
 
 	vplayer.setValue(player);
 	view->setProperty("player", vplayer);
