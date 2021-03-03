3e43c22f92d5893e088248ef6adb4ebd00805e19
j2969719 <j172933@mvrht.net>
2021-03-03 23:12:41 +0300

    helloworld

---
+++ plugins/wlx/csvviewer_gtk2/readme.md
@@ -0,0 +1,11 @@
+csvviewer_gtk2
+========
+Very primitive CSV viewer.
+
+![plug-preview](https://i.imgur.com/r36jIME.png)
+
+## Compatibility
+- `doublecmd-gtk2`
+
+## Dependencies
+![arch](https://wiki.archlinux.org/favicon.ico) `pacman -S enca`
---
+++ plugins/wlx/csvviewer_gtk2/src/plugin.c
@@ -0,0 +1,318 @@
+#include <gtk/gtk.h>
+#include <enca.h>
+#include <locale.h>
+#include <limits.h>
+#include <string.h>
+#include "wlxplugin.h"
+
+static char lang[3];
+gboolean use_iconv = TRUE;
+
+static gboolean quotes_not_closed(gchar *string)
+{
+	gint count = 0;
+
+	while (*string)
+		if (*string++ == '"')
+			count++;
+
+	if (count % 2 == 0)
+		return FALSE;
+
+	return TRUE;
+}
+
+static gchar *unqoute(gchar *string)
+{
+	gchar *target = NULL;
+
+	if (string[0] == '"' && string[strlen(string) - 1] == '"')
+		target = g_strndup(string + 1, strlen(string) - 2);
+	else
+		target = g_strdup(string);
+
+	gchar **split = g_strsplit(target, "\"\"", -1);
+	gchar *result = g_strjoinv("\"", split);
+
+	g_free(target);
+	g_strfreev(split);
+
+	return result;
+}
+
+static gchar *get_value(gchar ***p, gchar *separator)
+{
+	gchar *result = NULL;
+	gchar **itm = *p;
+
+	if (separator[0] != '\t' && *itm[0] == '"' && quotes_not_closed(*itm))
+	{
+		gchar **nitm = itm + 1;
+		gchar *cat = g_strdup(*itm);
+
+		while (*nitm != NULL)
+		{
+			gchar *tmp = g_strdup(cat);
+			cat = g_strjoin(separator, tmp, *nitm, NULL);
+			g_free(tmp);
+
+			if (cat && cat[strlen(cat) - 1] == '"' && !quotes_not_closed(cat))
+			{
+				result = unqoute(cat);
+				*p = nitm;
+				break;
+			}
+
+			nitm++;
+		}
+
+		g_free(cat);
+
+		if (*p != nitm)
+			result = g_strdup(*itm);
+
+	}
+	else if (separator[0] != '\t')
+	{
+		result = unqoute(*itm);
+	}
+	else
+		result = strdup(*itm);
+
+	return result;
+}
+
+HWND DCPCALL ListLoad(HWND ParentWin, char* FileToLoad, int ShowFlags)
+{
+	GtkWidget *gFix;
+	GtkWidget *scroll;
+	GtkWidget *list = NULL;
+	GtkCellRenderer *renderer;
+	GtkTreeViewColumn *column;
+	GtkListStore *store = NULL;
+	GtkTreeIter iter;
+	gsize columns = 0;
+	GType *types;
+	gchar *separator, *buffer, enc[256];
+	gchar *seps[] = { ",", ";", "\t", NULL };
+	gchar **lines, **row;
+	gsize bytes_read, len, i;
+	EncaAnalyser analyser;
+	EncaEncoding encoding;
+
+	if (!g_file_get_contents(FileToLoad, &buffer, &bytes_read, NULL))
+		return NULL;
+
+	if (use_iconv)
+	{
+		if (bytes_read == 0)
+		{
+			g_free(buffer);
+			return NULL;
+		}
+
+		analyser = enca_analyser_alloc(lang);
+
+		if (analyser)
+		{
+			enca_set_threshold(analyser, 1.38);
+			enca_set_multibyte(analyser, 1);
+			enca_set_ambiguity(analyser, 1);
+			enca_set_garbage_test(analyser, 1);
+			enca_set_filtering(analyser, 0);
+
+			encoding = enca_analyse(analyser, (unsigned char*)buffer, (size_t)bytes_read);
+			g_strlcpy(enc, enca_charset_name(encoding.charset, ENCA_NAME_STYLE_ICONV), 256);
+			enca_analyser_free(analyser);
+		}
+
+		gchar *converted = g_convert_with_fallback(buffer, bytes_read, "UTF-8", enc, NULL, NULL, &len, NULL);
+
+		if (converted != NULL)
+		{
+			g_free(buffer);
+			buffer = converted;
+		}
+	}
+
+	lines = g_regex_split_simple("[\r\n]+", buffer, 0, 0);
+
+	if (!lines)
+		return NULL;
+
+	for (gchar **chr = seps; *chr != NULL; chr++)
+	{
+		row = g_strsplit(*lines, *chr, -1);
+
+		if (row)
+			columns = g_strv_length(row);
+		else
+			continue;
+
+		if (columns > 1)
+		{
+			separator = *chr;
+			list = gtk_tree_view_new();
+			types = (GType*)g_malloc((columns + 1) * sizeof(GType));
+
+			for (i = 0; i <= columns; i++)
+				types[i] = G_TYPE_STRING;
+
+			store = gtk_list_store_newv(columns + 1, types);
+			g_free(types);
+
+			i = 0;
+
+			GValue val = G_VALUE_INIT;
+			g_value_init(&val, G_TYPE_BOOLEAN);
+			g_value_set_boolean(&val, TRUE);
+
+			for (gchar **itm = row; *itm != NULL; itm++)
+			{
+				renderer = gtk_cell_renderer_text_new();
+				g_object_set_property(G_OBJECT(renderer), "editable", &val);
+
+				gchar *header = get_value(&itm, separator);
+				column = gtk_tree_view_column_new_with_attributes(header, renderer, "text", i, NULL);
+				g_free(header);
+
+				gtk_tree_view_column_set_sort_column_id(column, i);
+				gtk_tree_view_append_column(GTK_TREE_VIEW(list), column);
+				i++;
+			}
+
+			renderer = gtk_cell_renderer_text_new();
+			column = gtk_tree_view_column_new_with_attributes("", renderer, "text", i, NULL);
+			gtk_tree_view_column_set_sort_column_id(column, i);
+			gtk_tree_view_append_column(GTK_TREE_VIEW(list), column);
+
+			g_value_unset(&val);
+
+			break;
+		}
+
+		g_strfreev(row);
+	}
+
+	if (columns < 2)
+	{
+		if (store)
+			gtk_list_store_clear(store);
+
+		if (list)
+			gtk_widget_destroy(list);
+
+		g_strfreev(lines);
+		return NULL;
+	}
+
+	for (gchar **line = lines + 1; *line != NULL; line++)
+	{
+		i = 0;
+
+		if (strlen(*line) < 1)
+			continue;
+
+		row = g_strsplit(*line, separator, -1);
+
+		if (row)
+		{
+			gtk_list_store_append(store, &iter);
+			GString *trash = g_string_new(NULL);
+
+			for (gchar **itm = row; *itm != NULL; itm++)
+			{
+				if (i >= columns)
+				{
+					trash = g_string_append(trash, ", ");
+					trash = g_string_append(trash, *itm);
+				}
+				else
+				{
+					gchar *value = get_value(&itm, separator);
+					gtk_list_store_set(store, &iter, i++, value, -1);
+					g_free(value);
+				}
+			}
+
+			gtk_list_store_set(store, &iter, i++, g_string_free(trash, FALSE), -1);
+			g_strfreev(row);
+		}
+	}
+
+	g_strfreev(lines);
+
+	gFix = gtk_vbox_new(FALSE, 5);
+	gtk_container_add(GTK_CONTAINER(GTK_WIDGET(ParentWin)), gFix);
+	scroll = gtk_scrolled_window_new(NULL, NULL);
+	gtk_container_add(GTK_CONTAINER(gFix), scroll);
+
+	gtk_tree_view_set_model(GTK_TREE_VIEW(list), GTK_TREE_MODEL(store));
+	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(scroll), GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
+	gtk_container_add(GTK_CONTAINER(scroll), list);
+
+	gtk_widget_show_all(gFix);
+	return gFix;
+
+}
+
+void DCPCALL ListCloseWindow(HWND ListWin)
+{
+	gtk_widget_destroy(GTK_WIDGET(ListWin));
+}
+
+void DCPCALL ListGetDetectString(char* DetectString, int maxlen)
+{
+	g_strlcpy(DetectString, "EXT=\"CSV\"|EXT=\"TSV\"", maxlen - 1);
+}
+
+int DCPCALL ListSearchDialog(HWND ListWin, int FindNext)
+{
+	return LISTPLUGIN_OK;
+}
+
+void DCPCALL ListSetDefaultParams(ListDefaultParamStruct* dps)
+{
+	GKeyFile *cfg;
+	char gCfgPath[PATH_MAX];
+
+	g_strlcpy(gCfgPath, dps->DefaultIniName, PATH_MAX);
+
+	char *pos = strrchr(gCfgPath, '/');
+
+	if (pos)
+		strcpy(pos + 1, "j2969719.ini");
+
+	cfg = g_key_file_new();
+
+	g_key_file_load_from_file(cfg, gCfgPath, G_KEY_FILE_KEEP_COMMENTS, NULL);
+
+	if (!g_key_file_has_key(cfg, "csvviewer", "enca", NULL))
+	{
+		g_key_file_set_boolean(cfg, "csvviewer", "enca", use_iconv);
+		g_key_file_save_to_file(cfg, gCfgPath, NULL);
+	}
+	else
+		use_iconv = g_key_file_get_boolean(cfg, "csvviewer", "enca", NULL);
+
+	if (!g_key_file_has_key(cfg, "csvviewer", "enca_lang", NULL))
+	{
+		snprintf(lang, 3, "%s", setlocale(LC_ALL, ""));
+		g_key_file_set_string(cfg, "csvviewer", "enca_lang", lang);
+		g_key_file_save_to_file(cfg, gCfgPath, NULL);
+	}
+	else
+	{
+		gchar *val = g_key_file_get_string(cfg, "csvviewer", "enca_lang", NULL);
+
+		if (val)
+		{
+			g_strlcpy(lang, val, 3);
+			g_free(val);
+		}
+		else
+			g_strlcpy(lang, "__", 3);
+	}
+
+	g_key_file_free(cfg);
+}
