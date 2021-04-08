8d1c4004f58fba724d0fbe8b406dad2014bdb7ed
j2969719 <j172933@mvrht.net>
2021-04-09 00:26:33 +0300

    https://www.youtube.com/watch?v=Z2NXAk-XkoQ

---
+++ plugins/wfx/wfx_scripts/scripts/aur_crap.lua
@@ -0,0 +1,66 @@
+#!/bin/env lua
+--[[
+⠀⠀⠀⠀⠀⠀⠀⣦⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣼⣿⣧⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣀⡻⣿⣿⣆⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣰⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀
+⠀⠀⠀⣰⣿⣿⡿⠿⢿⣿⣿⣆⠀⠀⠀
+⠀⠀⣰⣿⣿⣿⠀⠀⠈⣿⣿⣿⣆⠀⠀
+⠀⣰⣿⣿⠿⠟⠀⠀⠀⠻⠿⣿⣷⣄⠀
+⠰⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠆  btw i use arch®™
+]]
+
+args = {...}
+current_date = os.date("!%Y-%m-%dT%TZ")
+temp_file = "/tmp/doublecmd-aur.lst"
+
+function fs_init()
+    os.execute("curl https://aur.archlinux.org/pkgbase.gz | gzip -cd > " .. temp_file)
+    os.exit()
+end
+
+function fs_getlist(path)
+    if path == "/" then
+        for i = 65, 90 do
+            print("drwxr-xr-x " .. current_date .. " - " .. string.char(i))
+        end
+        print("drwxr-xr-x " .. current_date .. " - _other")
+    else
+        local file = io.open(temp_file, "r")
+        if not file then
+            os.exit(1)
+        end
+        local pattern = nil
+        if path:find("_other") then
+            pattern = "^%A"
+        else
+            local letter = path:match("%a/?$")
+            pattern = "^[" .. letter .. letter:lower() .. "]"
+        end
+        for line in file:lines() do
+            if line:find(pattern) and not line:find("# AUR package base list") then
+                print("-r--r--r-- " .. current_date .. " 404 " .. line .. ".tar.gz")
+            end
+        end
+        file:close()
+    end
+    os.exit()
+end
+
+function fs_getfile(file, target)
+    if file ~= nil and target ~= nil then
+        os.execute("curl https://aur.archlinux.org/cgit/aur.git/snapshot" .. file:match("/[^/]+$") .. " > " .. target)
+        os.exit()
+    end
+    os.exit(1)
+end
+
+if args[1] == "init" then
+    fs_init()
+elseif args[1] == "list" then
+    fs_getlist(args[2])
+elseif args[1] == "copyout" then
+    fs_getfile(args[2], args[3])
+end
+
+os.exit(1)
