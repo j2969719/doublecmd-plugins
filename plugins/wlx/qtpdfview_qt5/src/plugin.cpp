b0c848f9b8e62f177c4a2d05321b1ba3df160ea8
Rui Chen <crotoc@gmail.com>
2022-06-07 08:51:41 -0500

    Revision from upstream

--- plugins/wlx/qtpdfview_qt5/src/plugin.cpp
+++ plugins/wlx/qtpdfview_qt5/src/plugin.cpp
@@ -1,8 +1,14 @@
 #include <QtPdf>
-#include <QWidget>
 #include <QPdfView>
 #include <QtWidgets>
 #include "wlxplugin.h"
+
+static void update_pagecount(QLabel *label, QPdfView *view)
+{
+	int total = view->pageNavigation()->pageCount();
+	int page = view->pageNavigation()->currentPage() + 1;
+	label->setText(QString("%1/%2").arg(page).arg(total));
+}
 
 HANDLE DCPCALL ListLoad(HANDLE ParentWin, char* FileToLoad, int ShowFlags)
 {
@@ -11,57 +17,159 @@ HANDLE DCPCALL ListLoad(HANDLE ParentWin, char* FileToLoad, int ShowFlags)
 	if (document->load(QString(FileToLoad)) != QPdfDocument::NoError)
 	{
 		delete document;
-		return NULL;
+		return nullptr;
 	}
-	QFrame *view = new QFrame((QWidget*)ParentWin);
-	QFrame *frmControls = new QFrame(view);
-	view->setFrameStyle(QFrame::NoFrame);
-	frmControls->setFrameStyle(QFrame::NoFrame);
 
-		
+	QFrame *view = new QFrame((QWidget*)ParentWin);
+	view->setFrameStyle(QFrame::NoFrame);
 	QVBoxLayout *main = new QVBoxLayout(view);
-	QHBoxLayout *controls = new QHBoxLayout(frmControls);
-		
+	QToolBar *controls = new QToolBar(view);
+
 	QPdfView *pdfView = new QPdfView(view);
 	main->setSpacing(0);
-	main->setMargin(0);
-	main->setContentsMargins(1,1,1,1);
-	main->addWidget(pdfView,15);
-	main->addWidget(frmControls,1);
-	  
-	QPushButton *btnZoomIn = new QPushButton(view);
-	btnZoomIn->setIcon(QIcon::fromTheme("zoom-in"));
-	QObject::connect(btnZoomIn, &QPushButton::clicked, [pdfView]()
-	{
-	  pdfView->setZoomFactor(pdfView->zoomFactor() + 0.05);
-	});
-	btnZoomIn->setFocusPolicy(Qt::NoFocus);
-	controls->addWidget(btnZoomIn);
+	main->setContentsMargins(1, 1, 1, 1);
+	main->addWidget(controls);
+	main->addWidget(pdfView);
 
-	QPushButton *btnZoomOut = new QPushButton(view);
-	btnZoomOut->setIcon(QIcon::fromTheme("zoom-out"));
-	QObject::connect(btnZoomOut, &QPushButton::clicked, [pdfView]()
+	QAction *actFirst = new QAction(QIcon::fromTheme("go-first"), "First page", view);
+	QObject::connect(actFirst, &QAction::triggered, [pdfView]()
 	{
+		pdfView->pageNavigation()->setCurrentPage(0);
+	});
+	controls->addAction(actFirst);
+
+	QAction *actPrev = new QAction(QIcon::fromTheme("go-previous"), "Previous page", view);
+	QObject::connect(actPrev, &QAction::triggered, [pdfView]()
+	{
+		pdfView->pageNavigation()->goToPreviousPage();
+	});
+	controls->addAction(actPrev);
+
+	QAction *actNext = new QAction(QIcon::fromTheme("go-next"), "Next page", view);
+	QObject::connect(actNext, &QAction::triggered, [pdfView]()
+	{
+		pdfView->pageNavigation()->goToNextPage();
+	});
+	controls->addAction(actNext);
+
+	QAction *actLast = new QAction(QIcon::fromTheme("go-last"), "Last page", view);
+	QObject::connect(actLast, &QAction::triggered, [pdfView]()
+	{
+		int pages = pdfView->pageNavigation()->pageCount();
+
+		if (pages > 0)
+			pdfView->pageNavigation()->setCurrentPage(pages - 1);
+	});
+	controls->addAction(actLast);
+
+	controls->addSeparator();
+	QLabel *lblPages = new QLabel(view);
+	QObject::connect(pdfView->pageNavigation(), &QPdfPageNavigation::pageCountChanged, [lblPages, pdfView]()
+	{
+		update_pagecount(lblPages, pdfView);
+	});
+	QObject::connect(pdfView->pageNavigation(), &QPdfPageNavigation::currentPageChanged, [lblPages, pdfView]()
+	{
+		update_pagecount(lblPages, pdfView);
+	});
+	controls->addWidget(lblPages);
+	controls->addSeparator();
+
+	QAction *actZoomIn = new QAction(QIcon::fromTheme("zoom-in"), "Zoom In", view);
+	QObject::connect(actZoomIn, &QAction::triggered, [pdfView]()
+	{
+		if (pdfView->zoomMode() != QPdfView::CustomZoom)
+			pdfView->setZoomMode(QPdfView::CustomZoom);
+
+		pdfView->setZoomFactor(pdfView->zoomFactor() + 0.05);
+	});
+	controls->addAction(actZoomIn);
+
+	QAction *actZoomOut = new QAction(QIcon::fromTheme("zoom-out"), "Zoom Out", view);
+	QObject::connect(actZoomOut, &QAction::triggered, [pdfView]()
+	{
+		if (pdfView->zoomMode() != QPdfView::CustomZoom)
+			pdfView->setZoomMode(QPdfView::CustomZoom);
+
 		pdfView->setZoomFactor(pdfView->zoomFactor() - 0.05);
 	});
-	btnZoomOut->setFocusPolicy(Qt::NoFocus);
-	controls->addWidget(btnZoomOut);
+	controls->addAction(actZoomOut);
 
-	QPushButton *btnZoomOrg = new QPushButton(view);
-	btnZoomOrg->setIcon(QIcon::fromTheme("zoom-original"));
-	QObject::connect(btnZoomOrg, &QPushButton::clicked, [pdfView]()
+	QAction *actZoomOrg = new QAction(QIcon::fromTheme("zoom-original"), "Normal Size", view);
+	QObject::connect(actZoomOrg, &QAction::triggered, [pdfView]()
 	{
+		if (pdfView->zoomMode() != QPdfView::CustomZoom)
+			pdfView->setZoomMode(QPdfView::CustomZoom);
+
 		pdfView->setZoomFactor(1);
 	});
-	btnZoomOrg->setFocusPolicy(Qt::NoFocus);
-	controls->addWidget(btnZoomOrg);
-	controls->addSpacing(10);
+	controls->addAction(actZoomOrg);
 
+	controls->addSeparator();
+
+	QAction *actFit = new QAction(QIcon::fromTheme("zoom-fit-best"), "Fit", view);
+	actFit->setCheckable(true);
+	QObject::connect(actFit, &QAction::triggered, [pdfView]()
+	{
+		if (pdfView->zoomMode() == QPdfView::FitInView)
+			pdfView->setZoomMode(QPdfView::FitToWidth);
+		else
+			pdfView->setZoomMode(QPdfView::FitInView);
+	});
+	controls->addAction(actFit);
+
+	QAction *actPageMode = new QAction(QIcon::fromTheme("document-page-setup"), "Page Mode", view);
+	actPageMode->setCheckable(true);
+	QObject::connect(actPageMode, &QAction::triggered, [pdfView]()
+	{
+		if (pdfView->pageMode() != QPdfView::MultiPage)
+			pdfView->setPageMode(QPdfView::MultiPage);
+		else
+			pdfView->setPageMode(QPdfView::SinglePage);
+	});
+	controls->addAction(actPageMode);
+
+	controls->addSeparator();
+
+	QAction *actInfo = new QAction(QIcon::fromTheme("dialog-information"), "Info", view);
+	QObject::connect(actInfo, &QAction::triggered, [pdfView]()
+	{
+		QString info;
+		QMap<QString, QPdfDocument::MetaDataField> fields;
+		fields["Author"] = QPdfDocument::Author;
+		fields["Title"] = QPdfDocument::Title;
+		fields["Subject"] = QPdfDocument::Subject;
+		fields["Producer"] = QPdfDocument::Producer;
+		fields["Creator"] = QPdfDocument::Creator;
+		fields["Keywords"] = QPdfDocument::Keywords;
+		fields["Creation Date"] = QPdfDocument::CreationDate;
+		fields["Modification Date"] = QPdfDocument::ModificationDate;
+		QMapIterator<QString, QPdfDocument::MetaDataField> i(fields);
+
+		while (i.hasNext())
+		{
+			i.next();
+			QString value = pdfView->document()->metaData(i.value()).toString();
+
+			if (!value.isEmpty())
+			{
+				info.append(i.key());
+				info.append(": ");
+				info.append(value);
+				info.append("\n");
+			}
+		}
+
+		if (!info.isEmpty())
+			QMessageBox::information(pdfView, "", info);
+		else
+			QMessageBox::information(pdfView, "", "no suitable info available");
+	});
+	controls->addAction(actInfo);
 
 	pdfView->setDocument(document);
 	pdfView->setPageMode(QPdfView::MultiPage);
-	//pdfView->setZoomMode(QPdfView::FitToWidth);
-
+	pdfView->setObjectName("pdf_view");
 	view->show();
 
 	return view;
@@ -69,8 +177,9 @@ HANDLE DCPCALL ListLoad(HANDLE ParentWin, char* FileToLoad, int ShowFlags)
 
 int DCPCALL ListLoadNext(HWND ParentWin, HWND PluginWin, char* FileToLoad, int ShowFlags)
 {
-	QPdfView *view = (QPdfView*)PluginWin;
-	QPdfDocument *document = view->document();
+	QFrame *view = (QFrame*)ParentWin;
+	QPdfView *pdfView = view->findChild<QPdfView*>("pdf_view");
+	QPdfDocument *document = pdfView->document();
 
 	if (document != NULL)
 	{
@@ -87,8 +196,9 @@ int DCPCALL ListLoadNext(HWND ParentWin, HWND PluginWin, char* FileToLoad, int S
 
 void DCPCALL ListCloseWindow(HANDLE ListWin)
 {
-	QPdfView *view = (QPdfView*)ListWin;
-	QPdfDocument *document = view->document();
+	QFrame *view = (QFrame*)ListWin;
+	QPdfView *pdfView = view->findChild<QPdfView*>("pdf_view");
+	QPdfDocument *document = pdfView->document();
 
 	if (document != NULL)
 	{
@@ -96,12 +206,13 @@ void DCPCALL ListCloseWindow(HANDLE ListWin)
 		delete document;
 	}
 
+	delete pdfView;
 	delete view;
 }
 
 void DCPCALL ListGetDetectString(char* DetectString, int maxlen)
 {
-	strncpy(DetectString, "EXT=\"PDF\"", maxlen - 1);
+	snprintf(DetectString, maxlen - 1, "EXT=\"PDF\"");
 }
 
 int DCPCALL ListSearchDialog(HWND ListWin, int FindNext)
