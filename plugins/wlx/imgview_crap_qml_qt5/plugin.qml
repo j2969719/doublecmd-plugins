134df9a0334069823f49dfc52028c2f0e96ce24c
j2969719 <j172933@mvrht.net>
2022-06-08 19:18:02 +0300

    helloworld

--- plugins/wlx/imgview_crap_qml_qt5/plugin.qml
+++ plugins/wlx/imgview_crap_qml_qt5/plugin.qml
@@ -15,26 +15,123 @@ Rectangle
 		id: img
 		anchors.fill: parent
 		fillMode: Image.PreserveAspectFit
+		onStatusChanged: playing = (status == AnimatedImage.Ready)
 	}
 	function myListLoad(FileToLoad, ShowFlags)
 	{
-		// console.log("myListLoad, FileToLoad:", FileToLoad, ", ShowFlags:", ShowFlags)
+		// https://ghisler.github.io/WLX-SDK/listload.htm
+
+		/*
+		console.log("myListLoad, FileToLoad:", FileToLoad, ", ShowFlags:", ShowFlags)
+		console.log("Default INI: ", default_cfg)
+		console.log("Plugin Directory: ", plugin_dir)
+
+		if (ShowFlags & lcp_wraptext)
+			console.log ("lcp_wraptext")
+		if (ShowFlags & lcp_fittowindow)
+			console.log ("lcp_fittowindow")
+		if (ShowFlags & lcp_fitlargeronly)
+			console.log ("lcp_fitlargeronly")
+		if (ShowFlags & lcp_center)
+			console.log ("lcp_center")
+		if (ShowFlags & lcp_ansi)
+			console.log ("lcp_ansi")
+		if (ShowFlags & lcp_ascii)
+			console.log ("lcp_ascii")
+		if (ShowFlags & lcp_variable)
+			console.log ("lcp_variable")
+		if (ShowFlags & lcp_forceshow)
+			console.log ("lcp_forceshow")
+		*/
+
 		img.source = FileToLoad
 		if (img.status == Image.Error)
 			return false
 		return true
 	}
+	function myListLoadNext(FileToLoad, ShowFlags)
+	{
+		// https://ghisler.github.io/WLX-SDK/listloadnext.htm
+
+		//console.log("myListLoadNext, FileToLoad:", FileToLoad, ", ShowFlags:", ShowFlags)
+
+		return myListLoad(FileToLoad, ShowFlags)
+	}
 	function myListSendCommand(Command, Parameter)
 	{
-		// https://ghisler.github.io/WLX-SDK/listplug.h.htm
-		// console.log("myListSendCommand Command:", Command, ", Parameter:", Parameter)
+		// https://ghisler.github.io/WLX-SDK/listsendcommand.htm
+
+		/*
+		console.log("myListSendCommand Command:", Command, ", Parameter:", Parameter)
+		switch (Command)
+		{
+		case lc_copy:
+			console.log ("lc_copy")
+			break
+		case lc_selectall:
+			console.log ("lc_selectall")
+			break
+		case lc_setpercent:
+			console.log ("lc_setpercent")
+			break
+		case lc_newparams:
+			if (Parameter & lcp_wraptext)
+				console.log ("lcp_wraptext")
+			if (Parameter & lcp_fittowindow)
+				console.log ("lcp_fittowindow")
+			if (Parameter & lcp_ansi)
+				console.log ("lcp_ansi")
+			if (Parameter & lcp_ascii)
+				console.log ("lcp_ascii")
+			if (Parameter & lcp_variable)
+				console.log ("lcp_variable")
+			if (Parameter & lcp_fitlargeronly)
+				console.log ("lcp_fitlargeronly")
+			break
+		}
+		*/
+
+		return true
 	}
 	function myListSearchDialog(FindNext)
 	{
-		// console.log("myListSearchDialog, FindNext:", FindNext)
+		// https://ghisler.github.io/WLX-SDK/listsearchdialog.htm
+
+		//console.log("myListSearchDialog, FindNext:", FindNext)
+
+		return true
+	}
+	function myListSearchText(SearchString, SearchParameter)
+	{
+		// https://ghisler.github.io/WLX-SDK/listsearchtext.htm
+
+		/*
+		console.log("myListSearchDialog, SearchString:", SearchString, ", SearchParameter:", SearchParameter)
+
+		if (SearchParameter & lcs_findfirst)
+			console.log ("lcs_findfirst")
+		if (SearchParameter & lcs_matchcase)
+			console.log ("lcs_matchcase")
+		if (SearchParameter & lcs_wholewords)
+			console.log ("lcs_wholewords")
+		if (SearchParameter & lcs_backwards)
+			console.log ("lcs_backwards")
+		*/
+
+		return false
+	}
+	function myListPrint(FileToPrint, DefPrinter, PrintFlags, Margins)
+	{
+		// https://ghisler.github.io/WLX-SDK/listprint.htm
+
+		//console.log("myListPrint, FileToPrint:", FileToPrint, ", DefPrinter:", DefPrinter, ", PrintFlags:", PrintFlags, ", Margins:", Margins)
+
+		return false
 	}
 	function myListCloseWindow()
 	{
-		// console.log("myListCloseWindow")
+		// https://ghisler.github.io/WLX-SDK/listclosewindow.htm
+
+		//console.log("myListCloseWindow")
 	}
 }
--- plugins/wlx/imgview_crap_qml_qt5/src/plugin.cpp
+++ plugins/wlx/imgview_crap_qml_qt5/src/plugin.cpp
@@ -5,23 +5,47 @@
 #include <QQmlContext>
 #include "wlxplugin.h"
 
-QString gQmlFile;
+static QString gQmlFile;
+static QString gDefCfg;
+static QString gPlugDir;
 
 HANDLE DCPCALL ListLoad(HANDLE ParentWin, char* FileToLoad, int ShowFlags)
 {
 	QQuickWidget *view = new QQuickWidget((QWidget*)ParentWin);
 	QQmlContext *ctx = view->rootContext();
 	ctx->setContextProperty("parent", (QWidget*)ParentWin);
+	ctx->setContextProperty("default_cfg", gDefCfg);
+	ctx->setContextProperty("plugin_dir", gPlugDir);
+
+	ctx->setContextProperty("lc_copy", lc_copy);
+	ctx->setContextProperty("lc_newparams", lc_newparams);
+	ctx->setContextProperty("lc_selectall", lc_selectall);
+	ctx->setContextProperty("lc_setpercent", lc_setpercent);
+
+	ctx->setContextProperty("lcp_wraptext", lcp_wraptext);
+	ctx->setContextProperty("lcp_fittowindow", lcp_fittowindow);
+	ctx->setContextProperty("lcp_ansi", lcp_ansi);
+	ctx->setContextProperty("lcp_ascii", lcp_ascii);
+	ctx->setContextProperty("lcp_variable", lcp_variable);
+	ctx->setContextProperty("lcp_forceshow", lcp_forceshow);
+	ctx->setContextProperty("lcp_fitlargeronly", lcp_fitlargeronly);
+	ctx->setContextProperty("lcp_center", lcp_center);
+
+	ctx->setContextProperty("lcs_findfirst", lcs_findfirst);
+	ctx->setContextProperty("lcs_matchcase", lcs_matchcase);
+	ctx->setContextProperty("lcs_wholewords", lcs_wholewords);
+	ctx->setContextProperty("lcs_backwards", lcs_backwards);
+
 	view->setSource(QUrl::fromLocalFile(gQmlFile));
 	view->setResizeMode(QQuickWidget::SizeRootObjectToView);;
 	view->show();
 
 	QVariant ret;
 	QObject *object = qobject_cast<QObject *>(view->rootObject());
-	QMetaObject::invokeMethod(object, "myListLoad", 
-		Q_RETURN_ARG(QVariant, ret),
-		Q_ARG(QVariant, FileToLoad),
-		Q_ARG(QVariant, ShowFlags));
+	QMetaObject::invokeMethod(object, "myListLoad",
+	                          Q_RETURN_ARG(QVariant, ret),
+	                          Q_ARG(QVariant, FileToLoad),
+	                          Q_ARG(QVariant, ShowFlags));
 
 	if (!ret.toBool())
 	{
@@ -37,10 +61,10 @@ int DCPCALL ListLoadNext(HWND ParentWin, HWND PluginWin, char* FileToLoad, int S
 	QVariant ret;
 	QQuickWidget *view = (QQuickWidget*)PluginWin;
 	QObject *object = qobject_cast<QObject *>(view->rootObject());
-	QMetaObject::invokeMethod(object, "myListLoad", 
-		Q_RETURN_ARG(QVariant, ret),
-		Q_ARG(QVariant, FileToLoad),
-		Q_ARG(QVariant, ShowFlags));
+	QMetaObject::invokeMethod(object, "myListLoadNext",
+	                          Q_RETURN_ARG(QVariant, ret),
+	                          Q_ARG(QVariant, FileToLoad),
+	                          Q_ARG(QVariant, ShowFlags));
 
 	if (ret.toBool())
 		return LISTPLUGIN_OK;
@@ -55,6 +79,7 @@ void DCPCALL ListCloseWindow(HANDLE ListWin)
 	QObject *object = qobject_cast<QObject *>(view->rootObject());
 	QMetaObject::invokeMethod(object, "myListCloseWindow");
 
+	delete object;
 	delete view;
 }
 
@@ -63,12 +88,15 @@ int DCPCALL ListSendCommand(HWND ListWin, int Command, int Parameter)
 	QVariant ret;
 	QQuickWidget *view = (QQuickWidget*)ListWin;
 	QObject *object = qobject_cast<QObject *>(view->rootObject());
-	QMetaObject::invokeMethod(object, "myListSendCommand", 
-		Q_RETURN_ARG(QVariant, ret),
-		Q_ARG(QVariant, Command),
-		Q_ARG(QVariant, Parameter));
+	QMetaObject::invokeMethod(object, "myListSendCommand",
+	                          Q_RETURN_ARG(QVariant, ret),
+	                          Q_ARG(QVariant, Command),
+	                          Q_ARG(QVariant, Parameter));
 
-	return LISTPLUGIN_OK;
+	if (ret.toBool())
+		return LISTPLUGIN_OK;
+
+	return LISTPLUGIN_ERROR;
 }
 
 void DCPCALL ListGetDetectString(char* DetectString, int maxlen)
@@ -81,26 +109,75 @@ int DCPCALL ListSearchDialog(HWND ListWin, int FindNext)
 	QVariant ret;
 	QQuickWidget *view = (QQuickWidget*)ListWin;
 	QObject *object = qobject_cast<QObject *>(view->rootObject());
-	QMetaObject::invokeMethod(object, "myListSearchDialog", 
-		Q_RETURN_ARG(QVariant, ret),
-		Q_ARG(QVariant, FindNext));
+	QMetaObject::invokeMethod(object, "myListSearchDialog",
+	                          Q_RETURN_ARG(QVariant, ret),
+	                          Q_ARG(QVariant, FindNext));
 
-	return LISTPLUGIN_OK;
+	if (ret.toBool())
+		return LISTPLUGIN_OK;
+
+	return LISTPLUGIN_ERROR;
+}
+
+int DCPCALL ListSearchText(HWND ListWin, char* SearchString, int SearchParameter)
+{
+	QVariant ret;
+	QQuickWidget *view = (QQuickWidget*)ListWin;
+	QObject *object = qobject_cast<QObject *>(view->rootObject());
+	QMetaObject::invokeMethod(object, "myListSearchText",
+	                          Q_RETURN_ARG(QVariant, ret),
+	                          Q_ARG(QVariant, SearchString),
+	                          Q_ARG(QVariant, SearchParameter));
+
+	if (ret.toBool())
+		return LISTPLUGIN_OK;
+
+	return LISTPLUGIN_ERROR;
+}
+
+int DCPCALL ListPrint(HWND ListWin, char* FileToPrint, char* DefPrinter, int PrintFlags, RECT* Margins)
+{
+	QVariant ret;
+	QRect rect = QRect((int)Margins->left, (int)Margins->top,
+	                   (int)Margins->right, (int)Margins->bottom);
+	QQuickWidget *view = (QQuickWidget*)ListWin;
+	QObject *object = qobject_cast<QObject *>(view->rootObject());
+	QMetaObject::invokeMethod(object, "myListPrint",
+	                          Q_RETURN_ARG(QVariant, ret),
+	                          Q_ARG(QVariant, FileToPrint),
+	                          Q_ARG(QVariant, DefPrinter),
+	                          Q_ARG(QVariant, PrintFlags),
+	                          Q_ARG(QVariant, rect));
+
+	if (ret.toBool())
+		return LISTPLUGIN_OK;
+
+	return LISTPLUGIN_ERROR;
 }
 
 void DCPCALL ListSetDefaultParams(ListDefaultParamStruct* dps)
 {
-	Dl_info dlinfo;
-	static char path[PATH_MAX];
-	const char* qml = "plugin.qml";
-	memset(&dlinfo, 0, sizeof(dlinfo));
-	if (dladdr(path, &dlinfo) != 0)
+	if (gQmlFile.isEmpty() and gPlugDir.isEmpty())
 	{
-		strncpy(path, dlinfo.dli_fname, PATH_MAX);
-		char *pos = strrchr(path, '/');
+		Dl_info dlinfo;
+		static char path[PATH_MAX];
+		const char* qml = "plugin.qml";
+		memset(&dlinfo, 0, sizeof(dlinfo));
 
-		if (pos)
-			strcpy(pos + 1, qml);
-		gQmlFile = QString(path);
+		if (dladdr(path, &dlinfo) != 0)
+		{
+			strncpy(path, dlinfo.dli_fname, PATH_MAX);
+			char *pos = strrchr(path, '/');
+
+			if (pos)
+			{
+				strcpy(pos + 1, qml);
+				gQmlFile = QString(path);
+				*pos = '\0';
+				gPlugDir = QString(path);
+			}
+		}
 	}
+
+	gDefCfg = QString(dps->DefaultIniName);
 }
