3ee3c11e05abc8123a256d8b231197dd7fd2086e
j2969719 <j172933@mvrht.net>
2023-08-27 04:24:24 +0300

    helloworld

--- plugins/wdx/scripts/arcrootdirwdx.lua
+++ plugins/wdx/scripts/arcrootdirwdx.lua
@@ -1,5 +1,10 @@
+local cached_file = nil
+local root_dirs_count = 0
+local root_files_count = 0
+
+
 function ContentGetDetectString()
-    return 'EXT="7Z" | EXT="ZIP" | EXT="RAR" | EXT="XZ" | EXT="GZ" | EXT="Z" | EXT="LZMA" | EXT="BZ2" | EXT="TAR" | EXT="ZIPX"'
+    return 'EXT="7Z" | EXT="ZIP" | EXT="RAR" | EXT="XZ" | EXT="GZ" | EXT="Z" | EXT="LZMA" | EXT="BZ2" | EXT="TAR" | EXT="ZIPX" | EXT="ZST"'
 end
 
 function ContentGetSupportedField(FieldIndex)
@@ -17,39 +22,47 @@ function ContentGetValue(FileName, FieldIndex, UnitIndex, flags)
     if FieldIndex > 2 then
         return nil
     end
-    local arc_cmd = "7z l -slt"
-    local prefix = "^Path = "
-    if FileName:find("%.tar%.?") then
-        arc_cmd = "tar -tf"
-        prefix = '^'
-    end
-    local handle = io.popen(arc_cmd .. ' "' .. FileName:gsub('"', '\\"') .. '"', 'r');
-    local output = handle:read("*a");
-    handle:close();
-    if output == '' or output == nil then
-        return nil;
-    end
-    local root_dirs = {}
-    local root_files = {}
-    for line in output:gmatch("([^\n]*)\n?") do
-        local dir = line:match(prefix .. "([^/\\]+)[/\\][^/\\]+$")
-        if not dir then
-            local file = line:match(prefix .. "([^/\\]+)$")
-            if file ~= nil then
-                table.insert(root_files, file)
-            end
-        else
-            if not root_dirs[dir] then
-                root_dirs[dir] = true
+    if FileName ~= cached_file then
+        local arc_cmd = "7z l -slt"
+        local prefix = "^Path = "
+        if FileName:find("%.tar%.?") then
+            arc_cmd = "tar -tf"
+            prefix = '^'
+        end
+        local handle = io.popen(arc_cmd .. ' "' .. FileName:gsub('"', '\\"') .. '"', 'r');
+        local output = handle:read("*a");
+        handle:close();
+        if output == '' or output == nil then
+            return nil;
+        end
+        local root_dirs = {}
+        local root_files = {}
+        for line in output:gmatch("([^\n]*)\n?") do
+            local dir = line:match(prefix .. "([^/\\]+)[/\\].*$")
+            if not dir then
+                local file = line:match(prefix .. "([^/\\]+)$")
+                if file ~= nil and not root_dirs[file] then
+                    root_files[file] = true
+                end
+            else
+                if not root_dirs[dir] then
+                    root_dirs[dir] = true
+                    root_files[dir] = nil
+                end
             end
         end
-    end
-    local root_dirs_count = 0
-    for _ in pairs(root_dirs) do
-        root_dirs_count = root_dirs_count + 1
+        root_dirs_count = 0
+        for _ in pairs(root_dirs) do
+            root_dirs_count = root_dirs_count + 1
+        end
+        root_files_count = 0
+        for _ in pairs(root_files) do
+            root_files_count = root_files_count + 1
+        end
+        cached_file = FileName
     end
     if FieldIndex == 0 then
-        if root_dirs_count == 1 and #root_files == 0 then
+        if root_dirs_count == 1 and root_files_count == 0 then
             return true
         else
             return false
@@ -57,7 +70,7 @@ function ContentGetValue(FileName, FieldIndex, UnitIndex, flags)
     elseif FieldIndex == 1 then
         return root_dirs_count
     elseif FieldIndex == 2 then
-        return #root_files
+        return root_files_count
     end
     return nil
 end
