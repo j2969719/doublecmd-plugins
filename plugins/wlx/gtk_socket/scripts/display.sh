6ca112cb7b1a2561fa9b33792a9781283d0e5597
Skif-off <Skif-off@users.noreply.github.com>
2023-10-08 07:48:07 +0400

    UPD: display.sh (gtk_socket): Note about using in Debian/Ubuntu

--- plugins/wlx/gtk_socket/scripts/display.sh
+++ plugins/wlx/gtk_socket/scripts/display.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 xid=$1
 file=$2
+# Debian/Ubuntu:  Check the installed ImageMagick packages.
+# If the Q16HDRI version is installed, then add the "imagemagick-6.q16hdri"
+# package and use "display-im6.q16hdri" instead of "display". Otherwise, add
+# the "imagemagick-6.q16" package and use "display-im6.q16" instead of "display".
 display -backdrop -foreground white -window $xid -window-group $xid "$file"
