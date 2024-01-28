bd068baacb2bc86690ce036d327c6db04c27bb98
j2969719 <j172933@mvrht.net>
2024-01-28 14:42:42 +0300

    air quotes

--- plugins/make_all.sh
+++ plugins/make_all.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "build failed:" > dist/.build_fail.lst
-find $PWD -type f -name "Makefile" -not -path "*/third_party/*" -exec sh -c 'cd `dirname $0` && make' '{}' \;
+find "$PWD" -type f -name "Makefile" -not -path "*/third_party/*" -exec sh -c 'cd "`dirname "$0"`" && make' '{}' \;
 echo "END" >> dist/.build_fail.lst
--- plugins/run_ldd.sh
+++ plugins/run_ldd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo missing libs: > dist/.broken.log
-find $PWD -type f -name "*.[dw][cdfls]x" -not -path "*/third_party/*" -exec sh -c "ldd '{}' | grep 'not found' && echo '{}' >> $PWD/dist/.broken.log" \;
+find "$PWD" -type f -name "*.[dw][cdfls]x" -not -path "*/third_party/*" -exec sh -c "ldd '{}' | grep 'not found' && echo '{}' >> \"$PWD/dist/.broken.log\"" \;
 echo "END" >> dist/.broken.log
