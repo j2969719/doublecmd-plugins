9a92fe221935f64702e853b2057f8ded684cd823
j2969719 <j172933@mvrht.net>
2024-04-29 09:32:34 +0300

    helloworld

---
+++ plugins/wlx/mpv_alt/plugload.lua
@@ -0,0 +1,10 @@
+function on_fileload()
+  if mp.get_property("current-tracks/video") == nil then
+    mp.commandv("expand-properties", "show-text", "${filtered-metadata}", 9999)
+  end
+  mp.set_property_bool("pause", false)
+end
+
+
+mp.set_property_bool("loop", true)
+mp.register_event("file-loaded", on_fileload)
