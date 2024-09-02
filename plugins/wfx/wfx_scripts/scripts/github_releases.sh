cd72bc720b855a0c04dff2ef232d6ac1cab7ae13
j2969719 <j172933@mvrht.net>
2024-09-02 16:47:52 +0300

    helloworld

--- plugins/wfx/wfx_scripts/scripts/github_releases.sh
+++ plugins/wfx/wfx_scripts/scripts/github_releases.sh
@@ -9,12 +9,37 @@ init_fail()
     exit 1
 }
 
+cleanup()
+{
+    test -f "$DC_WFX_SCRIPT_JSON" && rm "$DC_WFX_SCRIPT_JSON"
+}
+
 get_json()
 {
     repo="$1"
+
+    [ -z "$DC_WFX_SCRIPT_JSON" ] || cleanup
+
     tmpfile=`mktemp /tmp/wfx_ghreleases.XXXXX`
     echo "Fs_Set_DC_WFX_SCRIPT_JSON $tmpfile"
-    curl "https://api.github.com/repos/$repo/releases" > "$tmpfile"
+    curl "https://api.github.com/repos/$repo/releases" > "$tmpfile" || init_fail WFX_SCRIPT_STR_ERR_CURL
+    err=`cat "$tmpfile" | jq -r '"\(.status): \(.message)"'`
+    [ -z "$err" ] || init_fail "$err"
+}
+
+get_body()
+{
+    ver="\"`dirname \"${1:1}\"`\""
+
+    [ -z "$DC_WFX_SCRIPT_TMP" ] && tmpdir=`mktemp -d /tmp/wfx_ghreleases.XXXXX` && echo "Fs_Set_DC_WFX_SCRIPT_TMP $tmpdir" ||\
+        tmpdir="$DC_WFX_SCRIPT_TMP"
+
+    tmpfile=`mktemp "$tmpdir/notes.XXXXX"`
+
+    [ "$ver" == "\".\"" ] && ver="\"`basename \"$1\"`\""
+
+    cat "$DC_WFX_SCRIPT_JSON" | jq -r --argjson ver "$ver" '.[] | select(.name == $ver) | .body' > "$tmpfile"
+    test -s "$tmpfile" && echo "Fs_ShowOutput cat \"$tmpfile\"" || echo "Fs_InfoMessage WFX_SCRIPT_STR_ERR_BODY"
 }
 
 vfs_init()
@@ -24,6 +49,8 @@ vfs_init()
     which wget >/dev/null 2>&1 || init_fail "\"wget\" WFX_SCRIPT_STR_ERR_NA"
     echo -e "Fs_PushValue WFX_SCRIPT_STR_REPO\tdoublecmd/doublecmd"
     echo "Fs_Request_Options"
+    echo "Fs_GetValues_Needed"
+    echo "Fs_DisableFakeDates"
     echo "WFX_SCRIPT_STR_REPO"
     exit $?
 }
@@ -35,6 +62,8 @@ vfs_setopt()
 
     case "$option" in
         "WFX_SCRIPT_STR_REPO") get_json "$value" ;;
+        "WFX_SCRIPT_STR_BODY") get_body "$value" ;;
+        "WFX_SCRIPT_STR_CHANGEREPO") echo -e "Fs_Request_Options\nWFX_SCRIPT_STR_REPO" ;;
         *) exit 1 ;;
     esac
 
@@ -51,9 +80,8 @@ vfs_list()
             jq '.[] | "dr-xr-xr-x \(.published_at) - \(.name)"' |\
             sed 's/"$//' | sed 's/^"//'
     else
-        cat "$DC_WFX_SCRIPT_JSON" | jq --argjson ver "\"${1:1}\"" \
-            '.[] | select(.name == $ver) | .assets[] | "0444 \(.updated_at) \(.size) \(.name)"' |\
-            sed 's/"$//' | sed 's/^"//'
+        cat "$DC_WFX_SCRIPT_JSON" | jq -r --argjson ver "\"${1:1}\"" \
+            '.[] | select(.name == $ver) | .assets[] | "0444 \(.updated_at) \(.size) \(.name)"'
     fi
     exit $?
 }
@@ -62,9 +90,8 @@ vfs_copyout()
 {
     ver="\"`dirname \"${1:1}\"`\""
     asset="\"${1#/*/}\""
-    url=`cat "$DC_WFX_SCRIPT_JSON" | jq --argjson ver "$ver" --argjson asset "$asset" \
-        '.[] | select(.name == $ver) | .assets[] | select(.name == $asset) | .browser_download_url'|\
-        sed 's/"$//' | sed 's/^"//'`
+    url=`cat "$DC_WFX_SCRIPT_JSON" | jq -r --argjson ver "$ver" --argjson asset "$asset" \
+        '.[] | select(.name == $ver) | .assets[] | select(.name == $asset) | .browser_download_url'`
     dst="$2"
 
     wget "$url" -O "$dst"
@@ -78,24 +105,45 @@ vfs_properties()
 
     if [ "$ver" != "\".\"" ] ; then
         asset="\"${1#/*/}\""
-        cat "$DC_WFX_SCRIPT_JSON" | jq --argjson ver "$ver" --argjson asset "$asset" \
+        cat "$DC_WFX_SCRIPT_JSON" | jq -r --argjson ver "$ver" --argjson asset "$asset" \
             '.[] | select(.name == $ver) | .assets[] | select(.name == $asset) | "WFX_SCRIPT_STR_AUTHOR|\(.uploader.login)", "content_type|\(.content_type)", "WFX_SCRIPT_STR_CTIME|\(.created_at)", "WFX_SCRIPT_STR_PTIME|\(.updated_at)", "WFX_SCRIPT_STR_DLOAD|\(.download_count)", "WFX_SCRIPT_STR_SIZE|\(.size)", "url|\(.browser_download_url)"'|\
-            sed 's/"$//' | sed 's/^"//' | tr '|' '\t'
+            tr '|' '\t'
     else
         ver="\"`basename \"$1\"`\""
-        cat "$DC_WFX_SCRIPT_JSON" | jq --argjson ver "$ver" \
+        cat "$DC_WFX_SCRIPT_JSON" | jq -r --argjson ver "$ver" \
             '.[] | select(.name == $ver) | "WFX_SCRIPT_STR_AUTHOR|\(.author.login)", "WFX_SCRIPT_STR_CTIME|\(.created_at)", "WFX_SCRIPT_STR_PTIME|\(.published_at)", "WFX_SCRIPT_STR_TARGET|\(.target_commitish)", "WFX_SCRIPT_STR_TAG|\(.tag_name)", "url|\(.html_url)"' |\
-            sed 's/"$//' | sed 's/^"//' | tr '|' '\t'
+            tr '|' '\t'
        pre=`cat "$DC_WFX_SCRIPT_JSON" | jq --argjson ver "$ver" '.[] | select(.name == $ver) | .prerelease'`
-       [ "$pre" == "null" ] && echo -e "filetype\tWFX_SCRIPT_STR_PRERELEASE" || echo -e "filetype\tWFX_SCRIPT_STR_RELEASE"
+       [ "$pre" == "true" ] && echo -e "filetype\tWFX_SCRIPT_STR_PRERELEASE" || echo -e "filetype\tWFX_SCRIPT_STR_RELEASE"
     fi
 
-    exit 0
+    echo -e "Fs_PropsActs WFX_SCRIPT_STR_BODY\tWFX_SCRIPT_STR_CHANGEREPO"
+
+    exit $?
+}
+
+vfs_getinfovalues()
+{
+    if [ "$1" != "/" ] ; then
+        ver="\"`basename \"$1\"`\""
+        cat "$DC_WFX_SCRIPT_JSON" | jq -r --argjson ver "$ver" \
+            '.[] | select(.name == $ver) | .assets[] | "\(.name)|⬇ \(.download_count)"' |\
+            tr '|' '\t'
+    else
+        cat "$DC_WFX_SCRIPT_JSON" | jq -r \
+            '.[] | "\(.name)|" + if .prerelease == true then "!" else "*" end + "   \(.tag_name)"' |\
+            tr '|' '\t'
+    fi
+
+    exit $?
 }
 
 vfs_deinit()
 {
-    test -f "$DC_WFX_SCRIPT_JSON" && rm "$DC_WFX_SCRIPT_JSON"
+    cleanup
+
+    [ -z "$DC_WFX_SCRIPT_TMP" ] || rm -rf "$DC_WFX_SCRIPT_TMP"
+
     exit 0
 }
 
@@ -106,6 +154,7 @@ case "$1" in
     list) vfs_list "$2";;
     copyout) vfs_copyout "$2" "$3";;
     properties) vfs_properties "$2";;
+    getvalues) vfs_getinfovalues "$2";;
     deinit) vfs_deinit;;
 esac
 exit 1
