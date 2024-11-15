683920efbefd5fc4da2335c93aefed016c23d5b6
Skif-off <Skif-off@users.noreply.github.com>
2024-11-15 09:17:14 +0400

    FIX: gtk_socket/scripts: ValueError.

--- plugins/wlx/gtk_socket/scripts/fb2info.py
+++ plugins/wlx/gtk_socket/scripts/fb2info.py
@@ -10,7 +10,10 @@ import xml.etree.ElementTree as ET
 
 import gi
 gi.require_version('Gtk', '3.0')
-gi.require_version('WebKit2', '4.0')
+try:
+    gi.require_version('WebKit2', '4.0')
+except ValueError:
+    gi.require_version('WebKit2', '4.1')
 from gi.repository import Gtk, GLib
 from gi.repository import WebKit2
 
--- plugins/wlx/gtk_socket/scripts/markdown_webkit.py
+++ plugins/wlx/gtk_socket/scripts/markdown_webkit.py
@@ -7,7 +7,10 @@ os.environ["GDK_CORE_DEVICE_EVENTS"] = "1"
 
 import gi
 gi.require_version('Gtk', '3.0')
-gi.require_version('WebKit2', '4.0')
+try:
+    gi.require_version('WebKit2', '4.0')
+except ValueError:
+    gi.require_version('WebKit2', '4.1')
 from gi.repository import Gtk, GLib
 from gi.repository import WebKit2
 
