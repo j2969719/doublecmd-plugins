b30c95aaf4a888c5f6e9d7607cbb1703660e3dc3
Skif-off <Skif-off@users.noreply.github.com>
2025-05-31 09:59:44 +0400

    UPD by j2969719: mediainfo (wdx): Additional fields and other, see diff.

--- plugins/wdx/mediainfo_crap/src/plugin.cpp
+++ plugins/wdx/mediainfo_crap/src/plugin.cpp
@@ -18,7 +18,6 @@ enum
 	PRE_FLOAT_BPS,
 	PRE_UNITS_BPS,
 	PRE_APROX_BPS,
-	PRE_CLEAR_KHZ,
 };
 
 typedef struct sfield
@@ -36,36 +35,39 @@ typedef struct saproxbps
 	const char *text;
 	double value;
 	double delta;
-	int pow;
+	int64_t unit;
 } taproxbps;
 
 #define ARRAY_SIZE(arr) (int)(sizeof(arr) / sizeof((arr)[0]))
 #define FIELDCOUNT ARRAY_SIZE(gFields)
+#define ONE_KILO 1000
+#define ONE_MEGA 1000000
+#define ONE_GIGA 1000000000 //CHAD
 
-taproxbps gBpsUnits[] =
+taproxbps gBpsChoices[] =
 {
-	// name,	value	+/-	0 = Bps, 1 = KBps, 2 = MBps, 3 = GBps  
-	{"16 KBps",	 16,	  2,	1},
-	{"32 KBps",	 32,	  2,	1},
-	{"96 KBps",	 96,	  2,	1},
-	{"128 KBps",	128,	  2,	1},
-	{"160 KBps",	160,	  2,	1},
-	{"192 KBps",	192,	  2,	1},
-	{"256 KBps",	256,	  2,	1},
-	{"320 KBps",	320,	  2,	1},
-	{"400 KBps",	320,	  2,	1},
-	{"750 KBps",	320,	  2,	1},
-	{"1 MBps",	  1,	0.5,	2},
-	{"2.5 MBps",	2.5,	0.5,	2},
-	{"3.8 MBps",	3.8,	0.5,	2},
-	{"4.5 MBps",	4.5,	0.5,	2},
-	{"6.8 MBps",	6.8,	0.5,	2},
-	{"9.8 MBps",	9.8,	0.5,	2},
-	{"18 MBps",	 18,	0.5,	2},
-	{"25 MBps",	 25,	  2,	2},
-	{"40 MBps",	 40,	  2,	2},
-	{"250 MBps",	250,	  2,	2},
-	{"1.4 GBps",	1.4,	0.5,	3},
+	// name,	value	+/-	unit
+	{"16 KBps",	 16,	  2,	ONE_KILO},
+	{"32 KBps",	 32,	  2,	ONE_KILO},
+	{"96 KBps",	 96,	  2,	ONE_KILO},
+	{"128 KBps",	128,	  2,	ONE_KILO},
+	{"160 KBps",	160,	  2,	ONE_KILO},
+	{"192 KBps",	192,	  2,	ONE_KILO},
+	{"256 KBps",	256,	  2,	ONE_KILO},
+	{"320 KBps",	320,	  2,	ONE_KILO},
+	{"400 KBps",	320,	  2,	ONE_KILO},
+	{"750 KBps",	320,	  2,	ONE_KILO},
+	{"1 MBps",	  1,	0.5,	ONE_MEGA},
+	{"2.5 MBps",	2.5,	0.5,	ONE_MEGA},
+	{"3.8 MBps",	3.8,	0.5,	ONE_MEGA},
+	{"4.5 MBps",	4.5,	0.5,	ONE_MEGA},
+	{"6.8 MBps",	6.8,	0.5,	ONE_MEGA},
+	{"9.8 MBps",	9.8,	0.5,	ONE_MEGA},
+	{"18 MBps",	 18,	0.5,	ONE_MEGA},
+	{"25 MBps",	 25,	  2,	ONE_MEGA},
+	{"40 MBps",	 40,	  2,	ONE_MEGA},
+	{"250 MBps",	250,	  2,	ONE_MEGA},
+	{"1.4 GBps",	1.4,	0.5,	ONE_GIGA},
 };
 
 tfield gFields[] =
@@ -73,10 +75,10 @@ tfield gFields[] =
 	// field name, 					  field type,			 units,  info,				    prepocess,	cache
 	{"General: Title",				  ft_string,			    "", "General;%Title%",		     PRE_NONE, nullptr},
 	{"General: Format",				  ft_string,			    "", "General;%Format%",		     PRE_NONE, nullptr},
-	{"General: Duration",				  ft_time,			    "", "General;%Duration%",		   PRE_TO_SEC, nullptr},
-	{"General: Duration, ms",			  ft_numeric_64,		    "", "General;%Duration%",		     PRE_NONE, nullptr},
+//	{"General: Duration, ms",			  ft_numeric_64,		    "", "General;%Duration%",		     PRE_NONE, nullptr},
 	{"General: Duration, s", 			  ft_numeric_32,		    "", "General;%Duration%",		   PRE_TO_SEC, nullptr},
-	{"General: Duration, HH:MM:SS",			  ft_string,			    "", "General;%Duration/String3%",  PRE_SHORT_TIME, nullptr},
+	{"General: Duration, HH:MM:SS",			  ft_time,			    "", "General;%Duration%",		   PRE_TO_SEC, nullptr},
+//	{"General: Duration, HH:MM:SS",			  ft_string,			    "", "General;%Duration/String3%",  PRE_SHORT_TIME, nullptr},
 	{"General: Duration, HH:MM:SS.MMM",		  ft_string,			    "", "General;%Duration/String3%",	     PRE_NONE, nullptr},
 	{"General: Overall bit rate",			  ft_numeric_floating, "Bps|KBps|MBps", "General;%OverallBitRate%",	PRE_UNITS_BPS, nullptr},
 	{"General: Overall bit rate, auto Bps/KBps/MBps", ft_string,			    "", "General;%OverallBitRate%",	PRE_FLOAT_BPS, nullptr},
@@ -86,6 +88,8 @@ tfield gFields[] =
 	{"General: Number of text streams",		  ft_numeric_64,		    "", "General;%TextCount%",		     PRE_NONE, nullptr},
 	{"General: Number of image streams",		  ft_numeric_64,		    "", "General;%ImageCount%",		     PRE_NONE, nullptr},
 	{"General: Number of menu streams",		  ft_numeric_64,		    "", "General;%MenuCount%",		     PRE_NONE, nullptr},
+	{"General: Encoded",				  ft_datetime,			    "", "General;%Encoded_Date%",	     PRE_NONE, nullptr},
+	{"General: Tagged",				  ft_datetime,			    "", "General;%Tagged_Date%",	     PRE_NONE, nullptr},
 	{"Video: Format",				  ft_string,			    "", "Video;%Format%",		     PRE_NONE, nullptr},
 	{"Video: Format profile",			  ft_string,			    "", "Video;%Format_Profile%",	     PRE_NONE, nullptr},
 	{"Video: Codec ID",				  ft_string,			    "", "Video;%CodecID%",		     PRE_NONE, nullptr},
@@ -104,6 +108,7 @@ tfield gFields[] =
 	{"Video: Number of frames",			  ft_numeric_64,		    "", "Video;%FrameCount%",		     PRE_NONE, nullptr},
 	{"Video: Bits/(Pixel*Frame)",			  ft_numeric_floating,		    "", "Video;%Bits-(Pixel*Frame)%",	     PRE_NONE, nullptr},
 	{"Video: Rotation",				  ft_numeric_64,		    "", "Video;%Rotation%",		     PRE_NONE, nullptr},
+	{"Video: Writing library",			  ft_string,			    "", "Video;%Encoded_Library/String%",    PRE_NONE, nullptr},
 	{"Audio: Format",				  ft_string,			    "", "Audio;%Format%",		     PRE_NONE, nullptr},
 	{"Audio: Format profile",			  ft_string,			    "", "Audio;%Format_Profile%",	     PRE_NONE, nullptr},
 	{"Audio: Codec ID",				  ft_string,			    "", "Audio;%CodecID%",		     PRE_NONE, nullptr},
@@ -113,8 +118,8 @@ tfield gFields[] =
 	{"Audio: Bit rate, aprox",			  ft_multiplechoice,		    "", "Audio;%BitRate%",		PRE_APROX_BPS, nullptr},
 	{"Audio: Bit rate mode",			  ft_string,			    "", "Audio;%BitRate_Mode%",		     PRE_NONE, nullptr},
 	{"Audio: Channel(s)",				  ft_numeric_64,		    "", "Audio;%Channel(s)%",		     PRE_NONE, nullptr},
-	{"Audio: Sampling rate, KHz",			  ft_string,			    "", "Audio;%SamplingRate/String%",	     PRE_NONE, nullptr},
-	{"Audio: Sampling rate, KHz (float)",		  ft_numeric_floating,		    "", "Audio;%SamplingRate/String%",	     PRE_CLEAR_KHZ, nullptr},
+	{"Audio: Sampling rate, KHz",			  ft_numeric_floating,		    "", "Audio;%SamplingRate/String%",	     PRE_NONE, nullptr},
+//	{"Audio: Sampling rate, KHz",			  ft_string,			    "", "Audio;%SamplingRate/String%",	     PRE_NONE, nullptr},
 	{"Audio: Bit depth",				  ft_numeric_64,		    "", "Audio;%BitDepth%",		     PRE_NONE, nullptr},
 	{"Audio: Delay in the stream",			  ft_numeric_64,		    "", "Audio;%Delaykfq%",		     PRE_NONE, nullptr},
 	{"Audio: Title",				  ft_string,			    "", "Audio;%Title%",		     PRE_NONE, nullptr},
@@ -123,6 +128,7 @@ tfield gFields[] =
 
 MediaInfo gMI;
 char gLastFile[PATH_MAX] = "";
+locale_t gNumericC;
 
 char* strlcpy(char* p, const char* p2, int maxlen)
 {
@@ -145,6 +151,24 @@ const wchar_t *toWChar(char *c_str)
 	return result;
 }
 
+bool toFileTime(char *string, LPFILETIME FileTime)
+{
+	struct tm t{0};
+
+	if (!strptime(string, "%Y-%m-%d %H:%M:%S", &t))
+		return false;
+
+	time_t unixtime = mktime(&t);
+
+	if (unixtime == -1)
+		return false;
+
+	unixtime = (unixtime * 10000000) + 0x019DB1DED53E8000;
+	FileTime->dwLowDateTime = (DWORD)unixtime;
+	FileTime->dwHighDateTime = unixtime >> 32;
+	return true;
+}
+
 int DCPCALL ContentGetSupportedField(int FieldIndex, char* FieldName, char* Units, int maxlen)
 {
 	if (FieldIndex < 0 || FieldIndex >= FIELDCOUNT)
@@ -156,12 +180,12 @@ int DCPCALL ContentGetSupportedField(int FieldIndex, char* FieldName, char* Unit
 	{
 		string units;
 
-		for (int i = 0; i < ARRAY_SIZE(gBpsUnits); i++)
+		for (int i = 0; i < ARRAY_SIZE(gBpsChoices); i++)
 		{
 			if (i > 0)
 				units.append("|");
 
-			units.append(gBpsUnits[i].text);
+			units.append(gBpsChoices[i].text);
 		}
 
 		strlcpy(Units, units.c_str(), maxlen - 1);
@@ -206,17 +230,16 @@ int DCPCALL ContentGetValue(char* FileName, int FieldIndex, int UnitIndex, void*
 					free(gFields[i].value);
 					gFields[i].value = nullptr;
 
-					if ((num / 1000000) < 1)
-					{
-						if ((num / 1000) < 1)
-						{
-							snprintf(bps, sizeof(bps), "%f Bps", num);
-						}
-						else
-							snprintf(bps, sizeof(bps), "%.2f KBps", num / 1000);
-					}
+					if (num >= ONE_GIGA)
+						snprintf(bps, sizeof(bps), "%.1f GBps", num / ONE_GIGA);
+					else if (num > ONE_MEGA)
+						snprintf(bps, sizeof(bps), "%.1f MBps", num / ONE_MEGA);
+					else if (num == ONE_MEGA)
+						snprintf(bps, sizeof(bps), "1 MBps");
+					else if (num >= ONE_KILO)
+						snprintf(bps, sizeof(bps), "%.0f KBps", num / ONE_KILO);
 					else
-						snprintf(bps, sizeof(bps), "%.2f MBps", num / 1000000);
+						snprintf(bps, sizeof(bps), "%.0f Bps", num);
 
 					gFields[i].value = strdup(bps);
 				}
@@ -234,38 +257,31 @@ int DCPCALL ContentGetValue(char* FileName, int FieldIndex, int UnitIndex, void*
 					free(gFields[i].value);
 					gFields[i].value = nullptr;
 
-					for (int u = 0; u < ARRAY_SIZE(gBpsUnits); u++)
+					for (int u = 0; u < ARRAY_SIZE(gBpsChoices); u++)
 					{
-						if (num / pow(1000, gBpsUnits[u].pow) > gBpsUnits[u].value - gBpsUnits[u].delta && \
-						                num / pow(1000, gBpsUnits[u].pow) < gBpsUnits[u].value + gBpsUnits[u].delta)
+						if (num / gBpsChoices[u].unit > gBpsChoices[u].value - gBpsChoices[u].delta && \
+						                num / gBpsChoices[u].unit < gBpsChoices[u].value + gBpsChoices[u].delta)
 						{
 
 							is_found = true;
-							gFields[i].value = strdup(gBpsUnits[u].text);
+							gFields[i].value = strdup(gBpsChoices[u].text);
 							break;
 						}
 					}
 
 					if (!is_found)
 					{
-						if (num >= pow(1000, 3))
-							snprintf(bps, sizeof(bps), "%.0f GBps", num / pow(1000, 3));
-						else if (num >= pow(1000, 2))
-							snprintf(bps, sizeof(bps), "%.0f MBps", num / pow(1000, 2));
-						else if (num >= 1000)
-							snprintf(bps, sizeof(bps), "%.0f KBps", num / 1000);
+						if (num >= ONE_GIGA)
+							snprintf(bps, sizeof(bps), "%.1f GBps", num / ONE_GIGA);
+						else if (num >= ONE_MEGA)
+							snprintf(bps, sizeof(bps), "%.1f MBps", num / ONE_MEGA);
+						else if (num >= ONE_KILO)
+							snprintf(bps, sizeof(bps), "%.0f KBps", num / ONE_KILO);
 						else
 							snprintf(bps, sizeof(bps), "%.0f Bps", num);
 
 						gFields[i].value = strdup(bps);
 					}
-				}
-				else if (gFields[i].pre == PRE_CLEAR_KHZ)
-				{
-					pos = strrchr(gFields[i].value, ' ');
-
-					if (pos)
-						*pos = '\0';
 				}
 			}
 		}
@@ -297,9 +313,9 @@ int DCPCALL ContentGetValue(char* FileName, int FieldIndex, int UnitIndex, void*
 			num = strtod(gFields[FieldIndex].value, &end);
 
 			if (UnitIndex == 1)
-				*(int*)FieldValue = (int)floor(num / 1000);
+				*(int *)FieldValue = (int)floor(num / ONE_KILO);
 			else if (UnitIndex == 2)
-				*(int*)FieldValue = (int)floor(num / 1000000);
+				*(int *)FieldValue = (int)floor(num / ONE_MEGA);
 		}
 		else
 			*(int*)FieldValue = atoi(gFields[FieldIndex].value);
@@ -307,14 +323,14 @@ int DCPCALL ContentGetValue(char* FileName, int FieldIndex, int UnitIndex, void*
 		break;
 
 	case ft_numeric_64:
-		num = strtod(gFields[FieldIndex].value, &end);
+		num = strtod_l(gFields[FieldIndex].value, &end, gNumericC);
 
 		if (gFields[FieldIndex].pre == PRE_UNITS_BPS && UnitIndex > 0)
 		{
 			if (UnitIndex == 1)
-				*(int64_t*)FieldValue = (int64_t)floor(num / 1000);
+				*(int64_t *)FieldValue = (int64_t)floor(num / ONE_KILO);
 			else if (UnitIndex == 2)
-				*(int64_t*)FieldValue = (int64_t)floor(num / 1000000);
+				*(int64_t *)FieldValue = (int64_t)floor(num / ONE_MEGA);
 		}
 		else
 			*(int64_t*)FieldValue = (int64_t)num;
@@ -322,14 +338,14 @@ int DCPCALL ContentGetValue(char* FileName, int FieldIndex, int UnitIndex, void*
 		break;
 
 	case ft_numeric_floating:
-		num = strtod(gFields[FieldIndex].value, &end);
+		num = strtod_l(gFields[FieldIndex].value, &end, gNumericC);
 
 		if (gFields[FieldIndex].pre == PRE_UNITS_BPS && UnitIndex > 0)
 		{
 			if (UnitIndex == 1)
-				*(double*)FieldValue = num / 1000;
+				*(double *)FieldValue = num / ONE_KILO;
 			else if (UnitIndex == 2)
-				*(double*)FieldValue = num / 1000000;
+				*(double *)FieldValue = num / ONE_MEGA;
 		}
 		else
 			*(double*)FieldValue = num;
@@ -372,6 +388,12 @@ int DCPCALL ContentGetValue(char* FileName, int FieldIndex, int UnitIndex, void*
 
 		return ft_fieldempty;
 
+	case ft_datetime:
+		if (!toFileTime(gFields[FieldIndex].value, (FILETIME*)FieldValue))
+			return ft_fieldempty;
+
+		break;
+
 	default:
 		return ft_fieldempty;
 	}
@@ -379,13 +401,24 @@ int DCPCALL ContentGetValue(char* FileName, int FieldIndex, int UnitIndex, void*
 	return gFields[FieldIndex].type;
 }
 
-int DCPCALL ContentGetDetectString(char* DetectString, int maxlen)
-{
-	snprintf(DetectString, maxlen - 1, "%s", DETECT_STRING);
-	return 0;
-}
-
 void DCPCALL ContentSetDefaultParams(ContentDefaultParamStruct* dps)
 {
-	setlocale(LC_NUMERIC, "C");
+	gNumericC = newlocale(LC_NUMERIC_MASK, "C", (locale_t)0);
+}
+
+void DCPCALL ContentPluginUnloading(void)
+{
+	for (int i = 0; i < FIELDCOUNT; i++)
+	{
+		free(gFields[i].value);
+		gFields[i].value = nullptr;
+	}
+
+	freelocale(gNumericC);
+}
+
+int DCPCALL ContentGetDetectString(char* DetectString, int maxlen)
+{
+	strlcpy(DetectString, DETECT_STRING, maxlen - 1);
+	return 0;
 }
