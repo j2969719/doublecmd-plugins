048132f710e43b8a75728879fba80099d9dae1eb
Skif-off <Skif-off@users.noreply.github.com>
2025-12-01 04:12:29 +0400

    UPD by j2969719: gotolinktarget.lua: Add 'newtab'.

--- scripts/lua/gotolinktarget.lua
+++ scripts/lua/gotolinktarget.lua
@@ -9,6 +9,7 @@ Params:
   %"0%p0
 
 Optional params:
+  newtab    - will open the target directory in a new tab.
   inactive  - will open the target directory in the inactive panel and make it active.
   onlydir   - if the target object is a file, only the parent directory will be opened.
   recursive - if the link points to a link then it's resolved recursively until a valid
@@ -31,79 +32,93 @@ DOUBLECMD#TOOLBAR#XMLDATA<?xml version="1.0" encoding="UTF-8"?>
 
 ]]
 
+local DIALOG_TITLE = "Go to link target"
+
+local MB_ICONERROR       = 0x0010
+local MB_ICONWARNING     = 0x0030
+local MB_ICONINFORMATION = 0x0040
+
 local params = {...}
 
 if #params == 0 then
-  Dialogs.MessageBox("Check parameters!", "Go to link target", 0x0030)
+  Dialogs.MessageBox("Required parameters are missing, please check the button settings.", DIALOG_TITLE, MB_ICONERROR)
   return
 end
 
-local iattr = SysUtils.FileGetAttr(params[1])
-if (iattr > 0) and (math.floor(iattr / 0x00000400) % 2 == 0) then return end
+local faSymLink   = 0x00000400
+local faDirectory = 0x00000010
 
-local scase, starget, stargetdir
-local bfile, bfull, binactive = true, true, false
-local sparams = ""
-
-if #params > 1 then sparams = table.concat(params, ";", 2) end
-
-if string.find(sparams, "recursive", 1, true) ~= nil then
-  starget = SysUtils.ReadSymbolicLink(params[1], true)
-  if starget == "" then
-    Dialogs.MessageBox("This link is broken or the target storage is currently unavailable.", "Go to link target", 0x0040)
-    return
-  end
-else
-  starget = SysUtils.ReadSymbolicLink(params[1], false)
+local function hasattr(fileattr, value)
+  return (fileattr > 0 and math.floor(fileattr / value) % 2 ~= 0)
 end
+
+local filename = params[1]
+local fileattr = SysUtils.FileGetAttr(filename)
+
+if not hasattr(fileattr, faSymLink) then
+  Dialogs.MessageBox('"' .. filename .. '" is not a symbolic link.', DIALOG_TITLE, MB_ICONERROR)
+  return
+end
+
+local is_newtab    = false
+local is_inactive  = false
+local is_onlydir   = false
+local is_recursive = false
+local is_case      = "on"
+
+if #params > 1 then
+  local user_options = table.concat(params, ";", 2)
+  is_newtab    = (user_options:find("newtab") ~= nil)
+  is_inactive  = (user_options:find("inactive") ~= nil)
+  is_onlydir   = (user_options:find("onlydir") ~= nil)
+  is_recursive = (user_options:find("recursive") ~= nil)
+end
+
+local link_target = SysUtils.ReadSymbolicLink(filename, is_recursive)
+
+if link_target == "" then
+  Dialogs.MessageBox("This link is broken or the target storage is currently unavailable.", DIALOG_TITLE, MB_ICONINFORMATION)
+  return
+end
+
+local is_absolute = false
 
 if SysUtils.PathDelim == "/" then
-  if string.sub(starget, 1, 1) ~= "/" then bfull = false end
-  scase = "on"
+  is_absolute = (link_target:sub(1, 1) == "/")
 else
-  if string.sub(starget, 2, 3) ~= ":\\" then bfull = false end
-  scase = "off"
+  is_absolute = (link_target:sub(2, 3) == ":\\")
+  is_case = "off"
 end
 
-if bfull == false then
-  starget = SysUtils.GetAbsolutePath(starget, SysUtils.ExtractFileDir(params[1]))
+if not is_absolute then
+  link_target = SysUtils.GetAbsolutePath(link_target, SysUtils.ExtractFileDir(filename))
 end
 
-iattr = SysUtils.FileGetAttr(starget)
-if iattr == -1 then
-  Dialogs.MessageBox('The target object\n"' ..  starget .. '"\nis currently unavailable.', "Go to link target", 0x0040)
-  return
-else
-  if (math.floor(iattr / 0x00000010) % 2 == 0) then
-    stargetdir = SysUtils.ExtractFileDir(starget)
-  else
-    stargetdir = starget
-    bfile = false
-  end
-end
+fileattr = SysUtils.FileGetAttr(link_target)
 
-if string.find(sparams, "inactive", 1, true) ~= nil then binactive = true end
-
-if string.find(sparams, "onlydir", 1, true) ~= nil then
-  if binactive == true then
-    DC.ExecuteCommand("cm_FocusSwap")
-  end
-  DC.ExecuteCommand("cm_ChangeDir", stargetdir)
+if fileattr == -1 then
+  Dialogs.MessageBox('The target object "' ..  link_target .. '" is currently unavailable.', DIALOG_TITLE, MB_ICONINFORMATION)
   return
 end
 
-if bfile == true then
-  if binactive == true then
-    DC.ExecuteCommand("cm_FocusSwap")
-    DC.ExecuteCommand("cm_ChangeDir", stargetdir)
-  else
-    if SysUtils.ExtractFileDir(params[1]) == stargetdir then
-      DC.ExecuteCommand("cm_QuickFilter", "search=off")
-    else
-      DC.ExecuteCommand("cm_ChangeDir", stargetdir)
-    end
+local is_link_to_dir = hasattr(fileattr, faDirectory)
+
+if is_inactive then
+  DC.ExecuteCommand("cm_FocusSwap")
+end
+
+if is_newtab then
+  DC.ExecuteCommand("cm_NewTab")
+end
+
+if is_link_to_dir then
+  DC.ExecuteCommand("cm_ChangeDir", link_target)
+elseif is_onlydir then
+  DC.ExecuteCommand("cm_ChangeDir", SysUtils.ExtractFileDir(link_target))
+else
+  if not pcall(DC.GoToFile, link_target, true) then
+    DC.ExecuteCommand("cm_ChangeDir", SysUtils.ExtractFileDir(link_target))
+    DC.ExecuteCommand("cm_QuickSearch", "search=on", "direction=first", "matchbeginning=off", "matchending=off", "casesensitive=" .. is_case, "files=on", "directories=off", "text=" .. SysUtils.ExtractFileName(link_target))
+    DC.ExecuteCommand("cm_QuickSearch", "search=off")
   end
-  starget = SysUtils.ExtractFileName(starget)
-  DC.ExecuteCommand("cm_QuickSearch", "search=on", "direction=first", "matchbeginning=off", "matchending=off", "casesensitive=" .. scase, "files=on", "directories=off", "text=" .. starget)
-  DC.ExecuteCommand("cm_QuickSearch", "search=off")
 end
