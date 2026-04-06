68a1d2c08af0c3405c2b23c25f8d7eeaf4312b4e
Skif-off <Skif-off@users.noreply.github.com>
2026-04-06 18:57:35 +0400

    UPD: CheckSum.lua: Add checksum32 (simple 32-bits checksum)

--- scripts/lua/CheckSum.lua
+++ scripts/lua/CheckSum.lua
@@ -1,5 +1,5 @@
 -- CheckSum.lua (cross-platform)
--- 2024.11.16
+-- 2025.04.06
 --
 -- Calculate or verify checksum (auto choose):
 -- if selected file is one of the DC supported (*.md5, *.sfv, *.sha and so on)
@@ -17,13 +17,13 @@ local params = {...}
 
 function chk(e)
   local l = {
-"blake2b", "blake2bp", "blake2s", "blake2sp", "blake3", "crc32", "haval",
-"md4", "md5", "ripemd128", "ripemd160", "sfv", "sha", "sha224", "sha256",
-"sha384", "sha512", "sha3", "tiger", "xxh128"
+"blake2b", "blake2bp", "blake2s", "blake2sp", "blake3", "checksum32", "crc32",
+"haval", "md4", "md5", "ripemd128", "ripemd160", "sfv", "sha", "sha224",
+"sha256", "sha3", "sha384", "sha512", "tiger", "xxh128"
 }
   e = string.lower(e)
   for i = 1, #l do
-    if l[i] == e then return 1 end;
+    if l[i] == e then return 1 end
   end
   return 0;
 end
@@ -31,7 +31,7 @@ end
 if #params == 2 then
   local c = "cm_CheckSumCalc"
   if params[2] == '' then
-    if chk(params[1]) == 1 then c = "cm_CheckSumVerify" end;
+    if chk(params[1]) == 1 then c = "cm_CheckSumVerify" end
   end
   DC.ExecuteCommand(c)
 end
