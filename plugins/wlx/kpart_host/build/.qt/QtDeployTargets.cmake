88be8763491e925adffb707573e7aefff89b9275
Peter P. Lupo <pplupo@gmail.com>
2026-04-17 10:11:51 -0400

    Cleanup.

--- plugins/wlx/kpart_host/build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_kpart_host_FILE /home/pplupo/repos/mpv_wayland/plugins/wlx/kpart_host/build/kpart_host.wlx)
-set(__QT_DEPLOY_TARGET_kpart_host_TYPE SHARED_LIBRARY)
--- plugins/wlx/kpart_host/build/CMakeFiles/kpart_host.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for kpart_host.
-# This may be replaced when dependencies are built.
