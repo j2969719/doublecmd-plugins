beb12411854d3ba68831df922d69e7e43394f723
j2969719 <j172933@mvrht.net>
2026-09-05 08:24:19 +0300

    0 days since an accident

--- plugins/wcx/bit7z/src/snatch.lua
+++
@@ -1,8 +0,0 @@
-#!/bin/env lua
-
-for line in io.lines("./third_party/bit7z/include/bit7z/bitformat.hpp") do
-  frmt, name = line:match("extern const BitIn%w+ (%w+);%s+///< (%w+)")
-  if frmt and name then
-    print('\t{"'..name..'",\tBitFormat::'..frmt..'},')
-  end
-end
